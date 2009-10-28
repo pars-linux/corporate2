5648e33d3c4800498c4726e37f580cbeb53b9f69
eki <eki@420bcd57-4a62-4fd6-832e-5ede16c90cc9>
2009-10-28 09:02:08 +0000

    add network/library/gnutls

---
+++ corporate2/devel/network/library/gnutls/actions.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2006-2009 TUBITAK/UEKAE
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import autotools
+
+def setup():
+    autotools.autoreconf("-fi")
+    autotools.configure("--disable-static \
+                         --disable-rpath \
+                         --disable-dependency-tracking \
+                         --enable-guile \
+                         --with-lzo \
+                         --with-zlib \
+                         --with-included-libcfg")
+
+def build():
+    autotools.make()
+
+def check():
+    autotools.make("check")
+
+def install():
+    autotools.install()
+
