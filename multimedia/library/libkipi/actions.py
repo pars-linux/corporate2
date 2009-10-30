dd6e0ba20ff40e747627e7385c41fd1713e998f6
Ozan Çağlayan <ozancaglayan@users.noreply.github.com>
2009-10-30 08:20:19 +0000

    Fix dependency names

--- multimedia/library/libkipi/actions.py
+++ multimedia/library/libkipi/actions.py
@@ -1,14 +1,18 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyright 2005,2006 TUBITAK/UEKAE
+# Copyright 2005-2009 TUBITAK/UEKAE
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
+from pisi.actionsapi import get
 from pisi.actionsapi import kde
+from pisi.actionsapi import shelltools
+
+shelltools.export("HOME", get.workDIR())
 
 def setup():
-    kde.configure()
+    kde.configure("--without-arts")
 
 def build():
     kde.make()
