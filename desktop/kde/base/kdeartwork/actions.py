1457974a22af6499d30ca5b1b6eda5690bb98ffe
Ozan Çağlayan <ozancaglayan@users.noreply.github.com>
2009-10-30 07:17:20 +0000

    Merge the latest versions from playground/ozan/kde3

---
+++ desktop/kde/base/kdeartwork/actions.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2005-2009 TUBITAK/UEKAE
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import kde
+from pisi.actionsapi import get
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import shelltools
+
+shelltools.export("HOME", get.workDIR())
+
+def setup():
+    kde.configure("--with-dpms \
+                   --with-libart \
+                   --with-opengl \
+                   --with-xscreensaver \
+                   --without-arts")
+
+def build():
+    kde.make()
+
+def install():
+    kde.install()
+
+    pisitools.removeDir("/usr/kde/3.5/share/sounds")
