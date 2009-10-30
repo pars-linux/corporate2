c392ac843f5ae66ac7a5a71f9e922dad1d28da8f
eki <eki@420bcd57-4a62-4fd6-832e-5ede16c90cc9>
2009-10-30 12:20:43 +0000

    add hal-info, dep of hal..

---
+++ hardware/info/hal-info/actions.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2005-2009 TUBITAK/UEKAE
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import shelltools
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+
+WorkDir = "hal-info-20090716"
+
+def setup():
+    autotools.configure("--enable-killswitch-dell-wlan=no \
+                         --enable-killswitch-dell-bluetooth=no \
+                         --enable-killswitch-dell-wwan=no")
+
+def build():
+    autotools.make()
+
+def install():
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+
+    pisitools.dodoc("AUTHORS", "COPYING")
