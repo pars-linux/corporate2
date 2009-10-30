cd6c113f14b72d0ed8dcdeef22f456967b266e76
ozan.caglayan <ozan.caglayan@420bcd57-4a62-4fd6-832e-5ede16c90cc9>
2009-10-30 07:17:26 +0000

    Merge the latest versions from playground/ozan/kde3

---
+++ corporate2/devel/desktop/kde/base/kdegraphics/actions.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2005-2009 TUBITAK/UEKAE
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import kde
+from pisi.actionsapi import get
+from pisi.actionsapi import shelltools
+
+shelltools.export("HOME", get.workDIR())
+
+def setup():
+    shelltools.export("DO_NOT_COMPILE", "kpovmodeler kmrml kview ksvg")
+    kde.configure("--with-poppler \
+                   --with-kamera \
+                   --without-arts")
+
+def build():
+    kde.make()
+
+def install():
+    kde.install()
