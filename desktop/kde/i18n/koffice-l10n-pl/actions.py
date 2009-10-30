a5c546b182371467c024266e94e393ed3322d951
Ozan Çağlayan <ozancaglayan@users.noreply.github.com>
2009-10-30 07:47:49 +0000

    Add i18n component

---
+++ desktop/kde/i18n/koffice-l10n-pl/actions.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2007 TUBITAK/UEKAE
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import kde
+
+def setup():
+    kde.configure()
+
+def build():
+    kde.make()
+
+def install():
+    kde.install()
