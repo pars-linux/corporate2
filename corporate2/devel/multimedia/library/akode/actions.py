412e83396108af70b5d2875bfaa41cb20c485944
eki <eki@420bcd57-4a62-4fd6-832e-5ede16c90cc9>
2009-10-30 10:05:29 +0000

    add multimedia.library component and akode

---
+++ corporate2/devel/multimedia/library/akode/actions.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2005-2009 TUBITAK/UEKAE
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import kde
+
+def setup():
+    kde.configure("--with-libsamplerate \
+                   --with-alsa \
+                   --without-jack \
+                   --with-flac \
+                   --with-libmad \
+                   --with-vorbis \
+                   --with-speex \
+                   --without-polypaudio")
+
+def build():
+    kde.make()
+
+def install():
+    kde.install()
+
+    pisitools.dodoc("AUTHORS", "NEWS", "README")
