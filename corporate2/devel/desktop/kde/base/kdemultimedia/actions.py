f8ab3dab240a03a200cb340cd11db904199b8204
ozan.caglayan <ozan.caglayan@420bcd57-4a62-4fd6-832e-5ede16c90cc9>
2009-10-30 07:17:29 +0000

    Merge the latest versions from playground/ozan/kde3

---
+++ corporate2/devel/desktop/kde/base/kdemultimedia/actions.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2005-2009 TUBITAK/UEKAE
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import kde
+from pisi.actionsapi import get
+from pisi.actionsapi import autotools
+from pisi.actionsapi import shelltools
+
+shelltools.export("HOME", get.workDIR())
+
+def setup():
+    shelltools.export("DO_NOT_COMPILE", "mpeglib mpeglib_artsplug kaboodle noatun")
+
+    autotools.make("-f Makefile.cvs")
+    kde.configure("--with-extra-includes=/usr/include/speex \
+                   --with-cdparanoia \
+                   --with-akode \
+                   --with-alsa \
+                   --with-vorbis \
+                   --with-lame \
+                   --with-flac \
+                   --with-speex \
+                   --with-libmad \
+                   --with-xine \
+                   --enable-cdparanoia \
+                   --without-arts-alsa \
+                   --without-arts \
+                   --without-jack \
+                   --with-musicbrainz")
+
+def build():
+    kde.make("-j1")
+
+def install():
+    kde.install()
