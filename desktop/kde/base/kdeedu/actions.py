20f0e6ec20aed14e9ce9d2bfba24014dd989222c
Ozan Çağlayan <ozancaglayan@users.noreply.github.com>
2009-10-30 07:17:24 +0000

    Merge the latest versions from playground/ozan/kde3

---
+++ desktop/kde/base/kdeedu/actions.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2005-2009 TUBITAK/UEKAE
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import kde
+from pisi.actionsapi import get
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import shelltools
+
+shelltools.export("HOME", get.workDIR())
+
+def setup():
+    shelltools.export("DO_NOT_COMPILE", "kvoctrain")
+    kde.make("-f Makefile.cvs")
+    kde.configure("--without-arts")
+
+def build():
+    kde.make()
+
+def install():
+    kde.install()
+
+    #remove desktop files, these come from kdebase now, #3665
+    pisitools.removeDir("/usr/kde/3.5/share/applnk")
