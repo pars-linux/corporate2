a9d07966f136d70ee7eaaa0d112426e456d947cb
eki <eki@420bcd57-4a62-4fd6-832e-5ede16c90cc9>
2009-11-07 20:48:25 +0000

    add programming/language/python/setuptools

---
+++ corporate2/devel/programming/language/python/setuptools/actions.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2006-2008 TUBITAK/UEKAE
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import pythonmodules
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+
+WorkDir="setuptools-%s" % get.srcVERSION().replace("_", "")
+
+def install():
+    pythonmodules.install()
+    pisitools.remove("/usr/lib/%s/site-packages/setuptools/*.exe" % get.curPYTHON())
