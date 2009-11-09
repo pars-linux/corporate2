faaaa0b9e9a42fcdbc3886a0e3e50181b2ae0ee2
eki <eki@420bcd57-4a62-4fd6-832e-5ede16c90cc9>
2009-11-09 13:48:23 +0000

    add tex/addon/texlive-latexextra

---
+++ tex/addon/texlive-latexextra/actions.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2009 TUBITAK/UEKAE
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import texlivemodules
+
+from pisi.actionsapi import get
+
+WorkDir = "%s-%s" % (get.srcNAME(), get.srcVERSION().split("_")[-1])
+
+def build():
+    texlivemodules.compile()
+
+def install():
+    texlivemodules.install()
+
+    # Install texmf bin scripts
+    bindocs=["perltex/perltex.pl","ppower4/pdfthumb.texlua", "ppower4/ppower4.texlua", "pst-pdf/ps4pdf", "vpe/vpe.pl"]
+    for i in bindocs:
+        binsplitslash=i.split("/")
+        binsplitpoint=binsplitslash[1].split(".")
+        print binsplitpoint[0]
+        pisitools.dosym("%s/%s/texmf-dist/scripts/%s" % (get.workDIR(), WorkDir, i), "/usr/bin/%s" % binsplitpoint[0])
+
