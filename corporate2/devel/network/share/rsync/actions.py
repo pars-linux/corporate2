a7822cb04bad9df1ce56842e9a69b24e4b2992a1
eki <eki@420bcd57-4a62-4fd6-832e-5ede16c90cc9>
2009-11-09 18:46:44 +0000

    add network/share/rsync

---
+++ corporate2/devel/network/share/rsync/actions.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2005-2009 TUBITAK/UEKAE
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+
+WorkDir="rsync-%s" % get.srcVERSION().replace('_','')
+
+def setup():
+    autotools.configure("--disable-debug \
+                         --with-rsyncd-conf=/etc/rsyncd.conf \
+                         --enable-acl-support \
+                         --enable-attr-support \
+                         --without-included-popt \
+                         --enable-ipv6")
+
+def build():
+    autotools.make()
+
+def install():
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+
+    pisitools.dodoc("NEWS","README","TODO")
