df5abf991490dafbf898940273f13421ffbb9947
Ozan Çağlayan <ozancaglayan@users.noreply.github.com>
2009-11-09 06:47:12 +0000

    Sync qscintilla from 2009

--- desktop/toolkit/qt3/qscintilla/actions.py
+++ desktop/toolkit/qt3/qscintilla/actions.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyright 2005-2009 TUBITAK/UEKAE
+# Copyright 2009 TUBITAK/UEKAE
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
@@ -10,7 +10,7 @@ from pisi.actionsapi import pisitools
 from pisi.actionsapi import shelltools
 from pisi.actionsapi import get
 
-WorkDir = "QScintilla1-1.71-gpl-1.7"
+WorkDir = "QScintilla-1.71-gpl-1.7.1"
 
 def setup():
     shelltools.cd("qt/")
@@ -18,12 +18,16 @@ def setup():
     pisitools.dosed("qscintilla.pro", "DESTDIR = \$\(QTDIR\)/lib", "DESTDIR = %s/%s/lib" % (get.installDIR(), get.qtDIR()))
     shelltools.system("qmake -o Makefile qscintilla.pro")
 
+    # Change C/XXFLAGS
+    pisitools.dosed("Makefile", "^CFLAGS   = -pipe -w -mcpu=i686 -O2 -pipe", "CFLAGS   = %s -w" % get.CFLAGS())
+    pisitools.dosed("Makefile", "^CXXFLAGS = -pipe -w -mcpu=i686 -O2 -pipe", "CXXFLAGS = %s -w" % get.CXXFLAGS())
+
 def install():
     pisitools.dodoc("ChangeLog", "LICENSE", "NEWS", "README")
 
     # execute install target of build system
     shelltools.cd("qt/")
-    autotools.make("-j1 all staticlib") # there is only an install target foolish qmake
+    autotools.make("all staticlib") # there is only an install target foolish qmake
 
     # installs not managed by the build system
     pisitools.insinto("%s/include" % get.qtDIR(), "qextscintilla*.h")
@@ -31,7 +35,7 @@ def install():
 
     shelltools.cd("../")
     pisitools.dohtml("doc/html/")
-    pisitools.insinto("/usr/share/doc/%s/Scintilla" % get.srcTAG(), "doc/Scintilla/*")
+    pisitools.insinto("/usr/share/doc/%s/Scintilla" % get.srcNAME(), "doc/Scintilla/*")
 
     # No static libs
     pisitools.remove("/usr/qt/3/lib/libqscintilla.a")
