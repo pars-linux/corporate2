8a4bfc3f0482c54a146f64ac3b4daabf62a3ee7d
eki <eki@420bcd57-4a62-4fd6-832e-5ede16c90cc9>
2009-11-10 18:17:53 +0000

    add desktop/gnome/libgnomecanvas

---
+++ corporate2/devel/desktop/gnome/libgnomecanvas/actions.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 2
+# See the file http://www.gnu.org/copyleft/gpl.txt
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+
+def setup():
+    autotools.autoreconf("-fi")
+    autotools.configure("--enable-static=no")
+
+    pisitools.dosed("libgnomecanvas/Makefile", "^LIBS = (.*)$", "LIBS = \\1 -lm")
+
+def build():
+    autotools.make()
+
+def install():
+    autotools.install()
+
+    pisitools.dodoc("README", "NEWS", "ChangeLog", "AUTHORS")
