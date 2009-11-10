9394e5051628dfc429fcbc7fc48c50c3a38d1a6d
eki <eki@420bcd57-4a62-4fd6-832e-5ede16c90cc9>
2009-11-10 17:30:51 +0000

    add office/docbook/SGMLSpm

---
+++ corporate2/devel/office/docbook/SGMLSpm/actions.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2007-2009 TUBITAK/UEKAE
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+
+WorkDir="SGMLSpm"
+
+def install():
+    pisitools.chmod("*",0644)
+
+    pisitools.insinto("/usr/lib/perl5/vendor_perl/%s/SGMLS" % get.curPERL(), "Refs.pm")
+    pisitools.insinto("/usr/lib/perl5/vendor_perl/%s/SGMLS" % get.curPERL(), "Output.pm")
+    pisitools.insinto("/usr/lib/perl5/vendor_perl/%s" % get.curPERL(), "SGMLS.pm")
+
+    pisitools.chmod("sgmlspl.pl")
+    pisitools.insinto("/usr/bin","sgmlspl.pl","sgmlspl")
+
+    pisitools.dodoc("ChangeLog","README","COPYING")
