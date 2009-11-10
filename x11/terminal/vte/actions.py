eaf1dba49c22b14dc23f758ab2ad6536c42f02c4
Fatih Arslan <ftharsln@gmail.com>
2009-11-10 07:47:54 +0000

    Fix build with new automake

--- x11/terminal/vte/actions.py
+++ x11/terminal/vte/actions.py
@@ -10,7 +10,7 @@ from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 
 def setup():
-    autotools.automake()
+    autotools.autoreconf("-vif")
     autotools.configure("--disable-static \
                          --enable-python \
                          --with-xft2 \
