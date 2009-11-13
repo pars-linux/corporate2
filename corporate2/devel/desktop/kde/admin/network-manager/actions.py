be5d57ba4e433dd6838e496fa09c9c2ef87216c6
eki <eki@420bcd57-4a62-4fd6-832e-5ede16c90cc9>
2009-11-13 13:31:51 +0000

    cleanup package for corporate2

---
+++ corporate2/devel/desktop/kde/admin/network-manager/actions.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2005-2009 TUBITAK/UEKAE
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+#
+
+from pisi.actionsapi import pythonmodules
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import shelltools
+from pisi.actionsapi import get
+
+shelltools.export('HOME', get.workDIR())
+def install():
+    pythonmodules.install()
+    binpath = "%s/bin/network-manager" % get.kdeDIR()
+    pisitools.remove(binpath)
+    pisitools.dosym("%s/share/apps/network-manager/network-manager.py" % get.kdeDIR(), binpath)
+    binpath = "%s/bin/network-applet" % get.kdeDIR()
+    pisitools.remove(binpath)
+    pisitools.dosym("%s/share/apps/network-manager/network-applet.py" % get.kdeDIR(), binpath)
