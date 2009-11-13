cf7500a69cc56380c2de0f63212227d9afde1d2f
ozan.caglayan <ozan.caglayan@420bcd57-4a62-4fd6-832e-5ede16c90cc9>
2009-11-13 10:12:48 +0000

    Switch to 5.x

--- corporate2/devel/system/base/dhcpcd/actions.py
+++ corporate2/devel/system/base/dhcpcd/actions.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyright 2005-2008 TUBITAK/UEKAE
+# Copyright 2005-2009 TUBITAK/UEKAE
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
@@ -10,13 +10,19 @@ from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 
 def setup():
-    pisitools.dosed("config.h", "^#define NTPDRIFTFILE.*$", '#define NTPDRIFTFILE "/var/lib/ntp/drift/ntp.drift"')
-    pisitools.dosed("config.h", "^#define ENABLE_IPV4LL$", "#undef ENABLE_IPV4LL")
+    autotools.configure("--libexecdir=/lib/dhcpcd \
+                         --dbdir=/var/lib/dhcpcd \
+                         --sbindir=/sbin \
+                         --localstatedir=/var")
 
 def build():
-    autotools.make('CC="%s" CFLAGS="%s" LDFLAGS="%s"' % (get.CC(), get.CFLAGS(), get.LDFLAGS()))
+    autotools.make()
 
 def install():
-    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+    autotools.rawInstall("DBDIR=/var/lib/dhcpcd LIBEXECDIR=/lib/dhcpcd DESTDIR=%s" % get.installDIR())
 
-    pisitools.dodoc ("README")
+    # Remove hooks install the compat one
+    pisitools.remove("/lib/dhcpcd/dhcpcd-hooks/*")
+    pisitools.insinto("/lib/dhcpcd/dhcpcd-hooks", "dhcpcd-hooks/50-dhcpcd-compat")
+
+    pisitools.dodoc("README")
