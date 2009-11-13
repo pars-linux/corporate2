eac13554a2566e83018e652e5c923e5a8afee184
eki <eki@420bcd57-4a62-4fd6-832e-5ede16c90cc9>
2009-11-13 14:08:17 +0000

    add Display Manager, cleanup package for corporate2

---
+++ corporate2/devel/desktop/kde/admin/display-manager/actions.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2008 TUBITAK/UEKAE
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+#
+
+from pisi.actionsapi import get
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import shelltools
+from pisi.actionsapi import pythonmodules
+
+shelltools.export('HOME', get.workDIR())
+
+def install():
+    pythonmodules.install()
+
+    binpath = "%s/bin/display-manager" % get.kdeDIR()
+    pisitools.remove(binpath)
+    pisitools.dosym("%s/share/apps/display-manager/display-manager.py" % get.kdeDIR(), binpath)
