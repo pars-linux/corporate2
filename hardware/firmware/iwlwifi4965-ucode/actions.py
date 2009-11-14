e4042e559b5d9817aea9bc5b0bc21798d862b371
eki <eki@420bcd57-4a62-4fd6-832e-5ede16c90cc9>
2009-11-14 15:48:30 +0000

    add /home/ekin/svn/pardus/2009/devel/hardware/firmware/iwlwifi4965-ucode

---
+++ hardware/firmware/iwlwifi4965-ucode/actions.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2008-2009 TUBITAK/UEKAE
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+
+WorkDir = "iwlwifi-4965-ucode-%s" % get.srcVERSION()
+
+def install():
+    pisitools.insinto("/lib/firmware", "*.ucode")
