4e97336acb06414394b8900be53b5a045dbdbaac
eki <eki@420bcd57-4a62-4fd6-832e-5ede16c90cc9>
2009-11-14 11:05:30 +0000

    add hardware/disk/gptsync

---
+++ hardware/disk/gptsync/actions.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2008 TUBITAK/UEKAE
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/copyleft/gpl.txt.
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+
+def build():
+    autotools.make("-j1")
+
+def install():
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+    pisitools.dodoc("README")
+
