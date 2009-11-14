06eb16b1fc67b367885153b23c4b85ea62d7839e
eki <eki@420bcd57-4a62-4fd6-832e-5ede16c90cc9>
2009-11-14 14:15:25 +0000

    add programming/language/python/PyQt * Bump package to 4.6.1 - needed by our new sip * Cleanup package for Corporate2

---
+++ corporate2/devel/programming/language/python/PyQt/actions.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2006-2009 TUBITAK/UEKAE
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import shelltools
+from pisi.actionsapi import pythonmodules
+from pisi.actionsapi import get
+
+WorkDir="%s-x11-gpl-%s" % (get.srcNAME(), get.srcVERSION())
+
+def setup():
+    pisitools.dosed("configure.py", "  check_license()", "# check_license()")
+    pythonmodules.run("configure.py -q /usr/bin/qmake-qt4")
+
+def build():
+    autotools.make()
+
+def install():
+    autotools.rawInstall("DESTDIR=%(DESTDIR)s INSTALL_ROOT=%(DESTDIR)s" % {'DESTDIR':get.installDIR()})
+
+    pisitools.dodir("/usr/qt/4/doc/PyQt4")
+    shelltools.copy("doc/html/*","%s/usr/qt/4/doc/PyQt4" % get.installDIR())
+    pisitools.dosym("/usr/qt/4/doc/PyQt4/classes.html","/usr/qt/4/doc/PyQt4/index.html")
+
+    pisitools.dodoc("ChangeLog", "NEWS", "README", "THANKS", "LICENSE.GPL2", "LICENSE.GPL3")
+
