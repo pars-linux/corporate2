cd9ea182e018cb1e527e5c1a03a84a692d27143f
eki <eki@420bcd57-4a62-4fd6-832e-5ede16c90cc9>
2009-11-14 12:08:22 +0000

    add gfxboot and gfxtheme-pardus

---
+++ corporate2/devel/system/boot/gfxtheme-pardus/actions.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2008-2009 TUBITAK/UEKAE
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/copyleft/gpl.txt.
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+
+WorkDir = "pardus"
+datadir = "/usr/share/gfxtheme/pardus"
+
+def build():
+    autotools.make('PRODUCT="Pardus 2009"')
+
+def install():
+    pisitools.insinto(datadir, "bootlogo.dir", "install")
+    pisitools.insinto(datadir, "message.dir", "boot")
+
