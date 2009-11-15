d04997e8d3e98a20e340523fc41310fb21a3e796
eki <eki@420bcd57-4a62-4fd6-832e-5ede16c90cc9>
2009-11-15 17:27:33 +0000

    add sysinfo : - Cleanup package for Corporate2 - Update tarball with a few swedish translations - Fix automake version detection

---
+++ desktop/kde/addon/sysinfo/actions.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2005-2009 TUBITAK/UEKAE
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import kde
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import shelltools
+from pisi.actionsapi import get
+
+KeepSpecial=["libtool"]
+shelltools.export('HOME', get.workDIR())
+
+def setup():
+    kde.make("-f admin/Makefile.common")
+    kde.configure("--without-arts")
+
+def build():
+    kde.make()
+
+def install():
+    kde.install()
+
+    pisitools.remove("/usr/kde/3.5/lib/kde3/kio_sysinfo.a")
+    pisitools.remove("/usr/kde/3.5/lib/kde3/libksysinfopart.a")
