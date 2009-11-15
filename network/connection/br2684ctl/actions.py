46e999357e0e09ca1bd8ea622b1794896b623cc2
eki <eki@420bcd57-4a62-4fd6-832e-5ede16c90cc9>
2009-11-15 21:42:57 +0000

    add network/connection/br2684ctl

---
+++ network/connection/br2684ctl/actions.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2006 TUBITAK/UEKAE
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+
+def build():
+    autotools.make()
+
+def install():
+    pisitools.dosbin("br2684ctl")
+
+    pisitools.doman("br2684ctl.8")
+    pisitools.dodoc("COPYING")
