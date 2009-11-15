0e5521113898d5aa47b12f5f1d924467e0ba4f87
eki <eki@420bcd57-4a62-4fd6-832e-5ede16c90cc9>
2009-11-15 21:43:02 +0000

    add network/connection/bridge-utils

---
+++ network/connection/bridge-utils/actions.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2006 TUBITAK/UEKAE
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+
+def setup():
+    autotools.autoconf()
+    autotools.configure()
+
+def build():
+    autotools.make()
+
+def install():
+    autotools.install()
+
+    pisitools.dodoc("AUTHORS", "COPYING", "ChangeLog", "README", "TODO", "doc/FAQ","doc/HOWTO","doc/SMPNOTES","doc/PROJECTS","doc/FIREWALL","doc/WISHLIST")
