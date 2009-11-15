d84ff1f7c8b1ddef3336718a0d237786f7d2d74d
eki <eki@420bcd57-4a62-4fd6-832e-5ede16c90cc9>
2009-11-15 14:56:40 +0000

    add firewall-config

---
+++ desktop/kde/admin/firewall-config/actions.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2005-2006 TUBITAK/UEKAE
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import get
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import pythonmodules
+from pisi.actionsapi import shelltools
+
+shelltools.export('HOME', get.workDIR())
+
+def install():
+    pythonmodules.install()
+    binpath = "%s/bin/firewall-config" % get.kdeDIR()
+    pisitools.remove(binpath)
+    pisitools.dosym("%s/share/apps/firewall-config/firewall-config.py" % get.kdeDIR(), binpath)
+
+    shelltools.chmod("%s/var/lib/iptables/pardus" % get.installDIR(), 0600)
