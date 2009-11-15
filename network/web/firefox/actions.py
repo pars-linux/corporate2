d66120227cb6c95b4be5082dd63c937c0100164c
eki <eki@420bcd57-4a62-4fd6-832e-5ede16c90cc9>
2009-11-15 17:50:16 +0000

    add firefox.. yay \o/

---
+++ network/web/firefox/actions.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2005-2009 TUBITAK/UEKAE
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import shelltools
+from pisi.actionsapi import get
+
+WorkDir = "mozilla"
+
+def setup():
+    shelltools.makedirs("objdir")
+    shelltools.cd("objdir")
+    #this dummy configure is needed to build locales.
+    shelltools.system("../configure --prefix=/usr --libdir=/usr/lib --disable-strip --disable-install-strip")
+
+    #now we have Makefiles needed to build locales (like toolkit/Makefile)
+    #since we need debug symbols in dbginfo packages, we shouldn't strip binaries
+    shelltools.system("../configure --prefix=/usr --libdir=/usr/lib --with-libxul-sdk=/usr/lib/xulrunner-devel-1.9 --disable-strip --disable-install-strip")
+
+def build():
+    shelltools.cd("objdir")
+
+    autotools.make()
+
+    locales = ["be", "ca", "de", "es-AR", "es-ES", "fr", "it", "nl", "pl", "pt-BR", "sv-SE", "tr"]
+
+    for locale in locales:
+        autotools.make("-j1 -C browser/locales libs-%s" % locale)
+        pisitools.copy("dist/xpi-stage/locale-%s/chrome/%s.jar" % (locale, locale), "dist/bin/chrome/")
+        pisitools.copy("dist/xpi-stage/locale-%s/chrome/%s.manifest" % (locale, locale), "dist/bin/chrome/")
+
+def install():
+    shelltools.cd("objdir")
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+    #pisitools.domove("/usr/lib/%s-%s" % (get.srcNAME(), get.srcVERSION()), "/usr/lib", "MozillaFirefox")
+    olddir = shelltools.ls("%s/usr/lib/firefox*" % get.installDIR())[0].replace(get.installDIR(), "")
+    pisitools.domove(olddir, "/usr/lib", "MozillaFirefox")
+
+    pisitools.remove("/usr/bin/firefox")
+
+    #install locales
+    locales = ["be", "ca", "de", "es-AR", "es-ES", "fr", "it", "nl", "pl", "pt-BR", "sv-SE", "tr"]
+    for locale in locales:
+        pisitools.insinto("/usr/lib/MozillaFirefox/chrome", "dist/bin/chrome/%s.*" % locale)
+
+    # Remove these
+    #pisitools.remove("/usr/lib/MozillaFirefox/defaults/profile/mimeTypes.rdf")
+    pisitools.remove("/usr/lib/MozillaFirefox/defaults/profile/bookmarks.html")
+    pisitools.remove("/usr/lib/MozillaFirefox/.autoreg")
+
+    shelltools.cd("..")
+
+    # Install branding icon
+    pisitools.insinto("/usr/share/pixmaps", "other-licenses/branding/firefox/default256.png", "firefox.png")
+
+    # Install docs
+    pisitools.dodoc("LEGAL", "LICENSE")
