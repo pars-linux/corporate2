874b063579f5f4bed7d2b6af00ce8eccc285fc33
eki <eki@420bcd57-4a62-4fd6-832e-5ede16c90cc9>
2009-11-17 06:34:33 +0000

    add kaptan

---
+++ desktop/kde/addon/kaptan/actions.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2005,2006 TUBITAK/UEKAE
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import get
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import shelltools
+from pisi.actionsapi import pythonmodules
+
+shelltools.export('HOME', get.work.DIR())
+
+def install():
+    pythonmodules.install()
+
+    pythonmodules.fixCompiledPy("/usr/kde/3.5/share/apps/kaptan")
+
+    pisitools.remove("%s/bin/kaptan" % get.kdeDIR())
+    pisitools.dosym("%s/share/apps/kaptan/kaptan.py" % get.kdeDIR(), "%s/bin/kaptan" % get.kdeDIR())
