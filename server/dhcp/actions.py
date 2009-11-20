67962030ce9379b0bb51c1052607f4a8f7194bc4
Ozan Çağlayan <ozancaglayan@users.noreply.github.com>
2009-11-20 18:06:51 +0000

    Don't include files in /var, touch the leases db on first install in postInstall

--- server/dhcp/actions.py
+++ server/dhcp/actions.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyright 2005-2007 TUBITAK/UEKAE
+# Copyright 2005-2009 TUBITAK/UEKAE
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
@@ -13,21 +13,22 @@ from pisi.actionsapi import get
 WorkDir="dhcp-4.1.0p1"
 
 def setup():
+    shelltools.export("CFLAGS", "%s -D_GNU_SOURCE" % get.CFLAGS())
     pisitools.dosed("client/scripts/linux", "/etc/dhclient-exit-hooks", "/etc/dhcp/dhclient-exit-hooks")
     pisitools.dosed("client/scripts/linux", "/etc/dhclient-enter-hooks", "/etc/dhcp/dhclient-enter-hooks")
 
-def build():
-    shelltools.export("CFLAGS","%s -D_GNU_SOURCE" % get.CFLAGS())
     autotools.configure()
+
+def build():
     autotools.make()
 
 def install():
     autotools.rawInstall("DESTDIR=%s" % get.installDIR())
     pisitools.remove("/etc/dhcpd.conf")
     pisitools.remove("/etc/dhclient.conf")
-    pisitools.dodir("/var/run")
+
+    # Create directory hierarchy in /var
+    pisitools.dodir("/var/run/dhcp")
     pisitools.dodir("/var/lib/dhcp")
-    shelltools.touch("%s/var/lib/dhcp/dhcpd.leases" % get.installDIR())
-    shelltools.touch("%s/var/lib/dhcp/dhcpd.leases~" % get.installDIR())
-    shelltools.touch("%s/var/run/dhcpd.pid" % get.installDIR())
+
     pisitools.dodoc("README", "RELNOTES")
