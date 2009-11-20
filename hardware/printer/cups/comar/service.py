2c17d01fe42c88835437f9fb9ca541ff8ede2f3b
Ozan Çağlayan <ozancaglayan@users.noreply.github.com>
2009-11-20 18:09:20 +0000

    Merge from 2009: Fix several XSS flaws in CUPS

--- hardware/printer/cups/comar/service.py
+++ hardware/printer/cups/comar/service.py
@@ -20,6 +20,18 @@ def start():
     startService(command="/usr/sbin/cupsd",
                  donotify=True)
 
+    # Tell udevd to replay printer events
+    # FIXME: This will be enabled after switching to the new system-config-printer
+    """
+    # One for low-level usb
+    os.system("udevadm trigger --subsystem-match=usb \
+                               --attr-match=bInterfaceClass=07 \
+                               --attr-match=bInterfaceSubClass=01")
+    # One for usblp backend
+    os.system("udevadm trigger --subsystem-match=usb \
+                               --property-match=DEVNAME=\"/dev/usb/lp*\"")
+    """
+
 @synchronized
 def stop():
     stopService(pidfile="/var/run/cups/cupsd.pid",
