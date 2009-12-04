f7e774e96a52b51c5caf5fbc564732d4e2fe9be7
Fatih Arslan <ftharsln@gmail.com>
2009-12-04 08:22:41 +0000

    New stable release (0.6c11)
    Merged from 2009/devel.

--- programming/language/python/setuptools/actions.py
+++ programming/language/python/setuptools/actions.py
@@ -1,7 +1,5 @@
-#!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyright 2006-2008 TUBITAK/UEKAE
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
@@ -9,7 +7,7 @@ from pisi.actionsapi import pythonmodules
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 
-WorkDir="setuptools-%s" % get.srcVERSION().replace("_", "")
+WorkDir="setuptools-%s" % get.srcVERSION().replace("_", "c")
 
 def install():
     pythonmodules.install()
