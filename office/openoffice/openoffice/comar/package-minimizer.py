a5c59e0e1fc2604a95206616d136e6e64c810850
Gökçen Eraslan <gokcen.eraslan@gmail.com>
2009-12-14 10:39:06 +0000

    Here comes OpenOffice for Corporate2

---
+++ office/openoffice/openoffice/comar/package-minimizer.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+
+import os
+
+unopkg = "/opt/OpenOffice.org/bin/unopkg"
+extPath = "/opt/OpenOffice.org/lib/ooo-3.1/share/extension/install"
+extName = "sun-presentation-minimizer"
+extID = "com.sun.star.PresentationMinimizer-linux_x86"
+
+def postInstall(fromVersion, fromRelease, toVersion, toRelease):
+    os.environ["JAVA_HOME"] = "/opt/sun-jre"
+    os.system("%s add --shared --force %s/%s.oxt" % (unopkg, extPath, extName))
+
+def preRemove():
+    os.environ["JAVA_HOME"] = "/opt/sun-jre"
+    os.system("%s remove --shared %s" % (unopkg, extID))
