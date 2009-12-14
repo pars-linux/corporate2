1899c22c49518491a22be333471935e5ef1400ae
Ozan Çağlayan <ozancaglayan@users.noreply.github.com>
2009-12-14 08:37:35 +0000

    Add aria2

---
+++ network/download/aria2/actions.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import get
+
+def setup():
+    autotools.configure("--enable-bittorrent \
+                         --enable-metalink \
+                         --enable-epoll \
+                         --enable-nls \
+                         --disable-rpath \
+                         --with-gnutls \
+                         --with-openssl \
+                         --with-sqlite3 \
+                         --with-libxml2 \
+                         --with-libcares \
+                         --with-libz")
+
+def build():
+    autotools.make()
+
+def install():
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
