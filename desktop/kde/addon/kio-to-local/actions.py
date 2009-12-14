1eec263b3a7cccc03da13e1ad8815135d10846d9
Gökçen Eraslan <gokcen.eraslan@gmail.com>
2009-12-14 09:57:27 +0000

    Merge kio-to-local for OpenOffice

---
+++ desktop/kde/addon/kio-to-local/actions.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2006 TUBITAK/UEKAE
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import cmaketools
+from pisi.actionsapi import get
+
+def setup():
+    cmaketools.configure()
+
+def install():
+    cmaketools.install("DESTDIR=%s" % get.installDIR())
