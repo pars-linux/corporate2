d7d9301adb7c30ed5e93745236413c5aebca6ee8
ozan.caglayan <ozan.caglayan@420bcd57-4a62-4fd6-832e-5ede16c90cc9>
2009-12-14 18:03:19 +0000

    Add aufs-progs

---
+++ corporate2/devel/hardware/disk/aufs-progs/actions.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2009 TUBITAK/UEKAE
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import shelltools
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+
+WorkDir = "%s-%s" % (get.srcNAME(), get.srcVERSION().split("_")[1])
+
+def setup():
+    pisitools.dosed("Makefile", "-O -Wall", get.CFLAGS())
+    pisitools.dosed("Makefile", "^KDIR = .*$", "KDIR = /lib/modules/%s/build" % get.curKERNEL())
+
+def build():
+    autotools.make()
+
+def install():
+    pisitools.dodir("usr/bin")
+    pisitools.dodir("sbin")
+    pisitools.dodir("usr/share/aufs-progs")
+
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+
+    pisitools.dodoc("README", "COPYING")
