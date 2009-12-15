5432dc41c513ef10622bcb05dda24e29f7fb928c
gokcen.eraslan <gokcen.eraslan@420bcd57-4a62-4fd6-832e-5ede16c90cc9>
2009-12-15 07:53:06 +0000

    Merge Thunderbird

---
+++ corporate2/devel/network/mail/thunderbird/actions.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2005-2009 TUBITAK/UEKAE
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import get
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import shelltools
+
+WorkDir = "thunderbird"
+
+shelltools.export("CFLAGS", "%s -Os -fno-strict-aliasing" % get.CFLAGS())
+
+def setup():
+    shelltools.system("./configure --prefix=/usr --libdir=/usr/lib")
+
+def build():
+    autotools.make()
+
+    #compile language files
+    locales = ["be", "ca", "de", "es-AR", "es-ES", "fr", "it", "nl", "pl", "pt-BR", "sv-SE", "tr"]
+
+    for locale in locales:
+        autotools.make("-C mail/locales libs-%s" % locale)
+        pisitools.copy("mozilla/dist/xpi-stage/locale-%s/chrome/%s.jar" % (locale, locale), "mozilla/dist/bin/chrome/")
+        pisitools.copy("mozilla/dist/xpi-stage/locale-%s/chrome/%s.manifest" % (locale, locale), "mozilla/dist/bin/chrome/")
+
+def install():
+    pisitools.insinto("/usr/lib/", "mozilla/dist/bin", "MozillaThunderbird", sym=False)
+
+    # Fake symlinks to get Turkish spell check support working
+    pisitools.dosym("/usr/lib/MozillaThunderbird/dictionaries/en-US.aff","/usr/lib/MozillaThunderbird/dictionaries/tr-TR.aff")
+    pisitools.dosym("/usr/lib/MozillaThunderbird/dictionaries/en-US.dic","/usr/lib/MozillaThunderbird/dictionaries/tr-TR.dic")
+
+    # Install icon
+    pisitools.insinto("/usr/share/pixmaps", "other-licenses/branding/thunderbird/mailicon256.png", "thunderbird.png")
+
+    # Install docs
+    pisitools.dodoc("mozilla/LEGAL", "mozilla/LICENSE")
---
+++ corporate2/devel/network/mail/thunderbird/files/default-prefs.js
@@ -0,0 +1,47 @@
+pref("network.protocol-handler.app.ftp","firefox");
+pref("network.protocol-handler.app.http","firefox");
+pref("network.protocol-handler.app.https","firefox");
+pref("mail.content_disposition_type", 1);
+pref("mailnews.reply_header_type", 3);
+pref("mailnews.reply_header_authorwrote", "%s şöyle yazmıştı:");
+pref("mailnews.reply_header_ondate", "on %s");
+pref("mailnews.reply_header_separator", " ");
+pref("mailnews.reply_header_colon", ":");
+pref("mailnews.default_sort_type", 2);
+pref("mailnews.default_sort_order", 22);
+
+pref("mail.compose.max_recycled_windows", 1);
+pref("browser.display.show_image_placeholders", false);
+pref("mailnews.customHeaders", "List-Id");
+
+pref("general.useragent.vendor", "Pardus");
+pref("general.useragent.vendorSub", "Corporate2");
+
+pref("font.name.monospace.null", "DejaVu Sans Mono");
+pref("font.name.monospace.tr", "DejaVu Sans Mono");
+pref("font.name.monospace.x-central-euro", "DejaVu Sans Mono");
+pref("font.name.monospace.x-unicode", "DejaVu Sans Mono");
+pref("font.name.monospace.x-user-def", "DejaVu Sans Mono");
+pref("font.name.monospace.x-western", "DejaVu Sans Mono");
+pref("font.name.sans-serif.null", "DejaVu Sans");
+pref("font.name.sans-serif.tr", "DejaVu Sans");
+pref("font.name.sans-serif.x-central-euro", "DejaVu Sans");
+pref("font.name.sans-serif.x-unicode", "DejaVu Sans");
+pref("font.name.sans-serif.x-user-def", "DejaVu Sans");
+pref("font.name.sans-serif.x-western", "DejaVu Sans");
+pref("font.name.serif.tr", "DejaVu Sans");
+pref("font.name.serif.x-central-euro", "DejaVu Sans");
+pref("font.name.serif.x-unicode", "DejaVu Sans");
+pref("font.name.serif.x-user-def", "DejaVu Sans");
+pref("font.name.serif.x-western", "DejaVu Sans");
+pref("font.minimum-size.null", 12);
+pref("font.minimum-size.x-central-euro", 12);
+pref("font.minimum-size.x-unicode", 12);
+pref("font.minimum-size.x-user-def", 12);
+pref("font.minimum-size.x-western", 12);
+pref("font.minimum-size.tr", 12);
+pref("font.size.fixed.null", 12);
+pref("font.size.variable.x-unicode", 12);
+pref("font.size.variable.null", 12);
+pref("font.size.variable.x-western", 12);
+pref("font.size.variable.tr", 12);
