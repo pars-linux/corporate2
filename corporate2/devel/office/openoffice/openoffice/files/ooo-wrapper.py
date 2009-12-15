ba5b1d0f5fbd2d284f08804de126407ba859966a
gokcen.eraslan <gokcen.eraslan@420bcd57-4a62-4fd6-832e-5ede16c90cc9>
2009-12-15 08:46:10 +0000

    Make ootool and ooconfig work.

--- corporate2/devel/office/openoffice/openoffice/files/ooo-wrapper.py
+++ corporate2/devel/office/openoffice/openoffice/files/ooo-wrapper.py
@@ -5,14 +5,16 @@ import os
 import sys
 from subprocess import *
 
-ooo_base_path = "/opt/OpenOffice.org/lib/ooo-3.1/";
-ooo_program_path = ooo_base_path + "program/"
-program=ooo_program_path+"s"+sys.argv[0].split('/')[-1][2:] # This transforms oowriter into swriter, oobase into sbase etc.
+ooo_bin_path= "/opt/OpenOffice.org/bin/";
+program=ooo_bin_path + os.path.basename(sys.argv[0])
+
+#skip wrapper for those
+blacklist = ['ooconfig', 'ootool', 'unopkg']
 
 files = sys.argv[1:]
-if files:
+if files and not os.path.basename(program) in blacklist:
     for file in files:
-        process = Popen(['kio-to-local',program,file])
+        process = Popen(['kio-to-local', program, file])
 else:
     process = Popen([program])
 
