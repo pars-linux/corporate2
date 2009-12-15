621fa06bd991120c98f57e646f40ff92debfb13a
ozan.caglayan <ozan.caglayan@420bcd57-4a62-4fd6-832e-5ede16c90cc9>
2009-12-15 08:03:59 +0000

    Add physfs

---
+++ corporate2/devel/programming/library/physfs/actions.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2006-2009 TUBITAK/UEKAE
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import cmaketools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+
+
+def setup():
+    cmaketools.configure("-DPHYSFS_BUILD_STATIC=0")
+
+def build():
+    cmaketools.make()
+
+def install():
+    cmaketools.rawInstall('DESTDIR="%s"' % get.installDIR())
+
+    pisitools.dodoc("CHANGELOG.txt", "CREDITS.txt", "TODO.txt")
