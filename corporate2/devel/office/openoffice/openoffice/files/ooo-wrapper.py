f06cda4c10f341d4aebd68e6cc5a14cfe9f1de70
gokcen.eraslan <gokcen.eraslan@420bcd57-4a62-4fd6-832e-5ede16c90cc9>
2009-12-15 08:48:42 +0000

    We cannot use command line paramters of OO programs now, this is because of kio-to-local

--- corporate2/devel/office/openoffice/openoffice/files/ooo-wrapper.py
+++ corporate2/devel/office/openoffice/openoffice/files/ooo-wrapper.py
@@ -11,6 +11,9 @@ program=ooo_bin_path + os.path.basename(sys.argv[0])
 #skip wrapper for those
 blacklist = ['ooconfig', 'ootool', 'unopkg']
 
+#TODO: Now we couldn't use command line parameters of soffice, this is because of kio-to-local
+#For example 'ooimpress -h' doesn't work now :(
+
 files = sys.argv[1:]
 if files and not os.path.basename(program) in blacklist:
     for file in files:
