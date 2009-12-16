ec3c8ec67dae0e3fc9c7269aa3be66fbc3d016ba
Fatih Arslan <ftharsln@gmail.com>
2009-12-16 06:39:48 +0000

    New stable release (1.0.3)

--- system/base/libXdmcp/actions.py
+++ system/base/libXdmcp/actions.py
@@ -1,12 +1,9 @@
-#!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyright 2006 TUBITAK/UEKAE
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
 from pisi.actionsapi import autotools
-from pisi.actionsapi import get
 
 def setup():
     autotools.configure("--disable-static")
@@ -15,5 +12,4 @@ def build():
     autotools.make()
 
 def install():
-    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
-
+    autotools.install()
