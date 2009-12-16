5a0af70173520a26b2a5768c58e41de43bee532f
Fatih Arslan <ftharsln@gmail.com>
2009-12-16 06:40:20 +0000

    New stable release (1.0.5)

--- x11/library/libXv/actions.py
+++ x11/library/libXv/actions.py
@@ -1,7 +1,5 @@
-#!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyright 2006 TUBITAK/UEKAE
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
@@ -16,4 +14,3 @@ def build():
 
 def install():
     autotools.rawInstall("DESTDIR=%s" % get.installDIR())
-
