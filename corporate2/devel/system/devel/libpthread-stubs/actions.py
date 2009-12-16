d708bbbbdbf5fb380fa29c4901f8c36ed3c516df
fatih <fatih@420bcd57-4a62-4fd6-832e-5ede16c90cc9>
2009-12-16 06:39:41 +0000

    New release (0.2)

--- corporate2/devel/system/devel/libpthread-stubs/actions.py
+++ corporate2/devel/system/devel/libpthread-stubs/actions.py
@@ -1,7 +1,5 @@
-#!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyright 2006 TUBITAK/UEKAE
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
@@ -9,7 +7,7 @@ from pisi.actionsapi import autotools
 from pisi.actionsapi import get
 
 def setup():
-    autotools.configure()
+    autotools.configure("--disable-static")
 
 def build():
     autotools.make()
