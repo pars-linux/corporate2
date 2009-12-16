19f84cf71b739e0c41246745a49110ea4f1a7337
Fatih Arslan <ftharsln@gmail.com>
2009-12-16 06:40:35 +0000

    New stable release (1.1.0)

--- x11/misc/xbitmaps/actions.py
+++ x11/misc/xbitmaps/actions.py
@@ -1,12 +1,10 @@
-#!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyright 2006 TUBITAK/UEKAE
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
 from pisi.actionsapi import autotools
-from pisi.actionsapi import get
+from pisi.actionsapi import pisitools
 
 def setup():
     autotools.configure()
@@ -15,5 +13,8 @@ def build():
     autotools.make()
 
 def install():
-    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+    autotools.install()
 
+    # Fix conflict with xorg-app
+    pisitools.remove("/usr/include/X11/bitmaps/black6")
+    pisitools.remove("/usr/include/X11/bitmaps/box6")
