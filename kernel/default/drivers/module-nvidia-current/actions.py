f6957201ffe1e7525ebcba6a69ae3348766d12cd
Fatih Arslan <ftharsln@gmail.com>
2009-12-17 12:11:21 +0000

    - Depend on new libvdpau library - Create symlinks for important libraries first - Remove trace driver as it comes with libvdpau
    Merged from 2009/devel.

--- kernel/default/drivers/module-nvidia-current/actions.py
+++ kernel/default/drivers/module-nvidia-current/actions.py
@@ -21,6 +21,7 @@ def setup():
     # Remove VDPAU headers and wrapper library
     shelltools.unlinkDir("usr/include/vdpau")
     shelltools.unlink("usr/lib/libvdpau.so.%s" % get.srcVERSION())
+    shelltools.unlink("usr/lib/vdpau/libvdpau_trace.so.%s" % get.srcVERSION())
 
 def build():
     shelltools.export("SYSSRC", "/lib/modules/%s/build" % KDIR)
