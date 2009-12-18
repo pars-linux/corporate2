233fe372555c1cf1795cbd618897191b82a222cd
Ozan Çağlayan <ozancaglayan@users.noreply.github.com>
2009-12-18 14:58:51 +0000

    Map some old icons into the new ones

--- desktop/lookandfeel/bctango-icon-theme/actions.py
+++ desktop/lookandfeel/bctango-icon-theme/actions.py
@@ -23,3 +23,15 @@ def install():
         pisitools.remove("/usr/share/icons/bctangokde/%s" % d)
 
     pisitools.rename("/usr/share/icons/bctangokde", "BCTango")
+
+    # Symlinks
+    mapping = {"button_cancel.png": "actions/cancel.png",
+               "kmenu.png": "apps/panel.png",
+               "kmenu.png": "apps/panel_settings.png"}
+
+    for size in (16, 22, 32, 48, 64, 128):
+        for k in mapping.keys():
+            try:
+                pisitools.dosym(k, "usr/share/icons/BCTango/%dx%d/%s" % (size, size, mapping[k]))
+            except:
+                pass
