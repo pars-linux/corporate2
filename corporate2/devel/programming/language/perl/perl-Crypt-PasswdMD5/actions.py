b072805cd8e5e1f3869e8accbeddc665b6d3cc65
ozan.caglayan <ozan.caglayan@420bcd57-4a62-4fd6-832e-5ede16c90cc9>
2009-12-20 13:32:28 +0000

    Merge from devel/programming/language/perl/perl-Crypt-PasswdMD5/:
    rev. 80949, by faik on 2009-11-24
    Add perl deps
    
    rev. 81933, by onur on 2009-12-04
    Depend on new perl

--- corporate2/devel/programming/language/perl/perl-Crypt-PasswdMD5/actions.py
+++ corporate2/devel/programming/language/perl/perl-Crypt-PasswdMD5/actions.py
@@ -23,5 +23,5 @@ def check():
 def install():
     perlmodules.install()
 
-    pisitools.removeDir("/usr/lib/perl5/%s" % get.curPERL())
+    pisitools.removeDir("/usr/lib/perl5/vendor_perl/%s/i686-linux-thread-multi/" % get.curPERL())
     pisitools.dodoc("README")
