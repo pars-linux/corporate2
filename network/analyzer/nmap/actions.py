ffcf735b70ca0e5d8d6bc477db28b2d69772062f
Ozan Çağlayan <ozancaglayan@users.noreply.github.com>
2009-12-21 09:05:41 +0000

    Ready for 2009

---
+++ network/analyzer/nmap/actions.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2005-2009 TUBITAK/UEKAE
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+
+WorkDir = "nmap-%s" % get.srcVERSION().replace("_", "").upper()
+
+def setup():
+    autotools.autoconf()
+    autotools.configure("--without-zenmap")
+
+def build():
+    autotools.make()
+
+def install():
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+
+    pisitools.dodoc("docs/README", "HACKING", "CHANGELOG", "docs/*.txt")
