d75a8ba15a9facbf6282c131cf1ce479387f28e9
ozan.caglayan <ozan.caglayan@420bcd57-4a62-4fd6-832e-5ede16c90cc9>
2009-12-21 14:40:21 +0000

    Fix KDE Path

--- corporate2/devel/desktop/misc/shared-mime-info/comar/package.py
+++ corporate2/devel/desktop/misc/shared-mime-info/comar/package.py
@@ -4,5 +4,5 @@ import os
 
 # Update global mime databases, mime database format may change (0.70)
 def postInstall(fromVersion, fromRelease, toVersion, toRelease):
-    for p in ["/usr/kde/4/share/mime", "/usr/share/mime"]:
+    for p in ["/usr/kde/3.5/share/mime", "/usr/share/mime"]:
         os.system("/usr/bin/update-mime-database %s" % p)
