e1eb7cbfddabe2da1c6acb0826adde60409e0812
ozan.caglayan <ozan.caglayan@420bcd57-4a62-4fd6-832e-5ede16c90cc9>
2009-12-21 09:01:44 +0000

    Ready for 2009

---
+++ corporate2/devel/hardware/info/lshw/actions.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*- 
+#
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+
+WorkDir = "lshw-B.%s" % get.srcVERSION()
+
+def build():
+    autotools.make()
+    autotools.make("gui")
+
+def install():
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR(), "install-gui")
+
+    pisitools.dodoc("docs/*", "COPYING", "README")
