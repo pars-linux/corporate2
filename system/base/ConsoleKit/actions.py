c581ff2c0cd75ced927949859ba0b324d92b30d7
Ozan Çağlayan <ozancaglayan@users.noreply.github.com>
2009-12-21 08:51:12 +0000

    Enable PolicyKit support, don't remove scripts

--- system/base/ConsoleKit/actions.py
+++ system/base/ConsoleKit/actions.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyright 2007,2008 TUBITAK/UEKAE
+# Copyright 2007-2009 TUBITAK/UEKAE
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
@@ -12,7 +12,6 @@ from pisi.actionsapi import get
 def setup():
     autotools.autoreconf("-fi")
     autotools.configure("--disable-static \
-                         --disable-policy-kit \
                          --enable-pam-module \
                          --localstatedir=/var \
                          --with-pid-file=/var/run/ConsoleKit/pid")
@@ -23,9 +22,8 @@ def build():
 def install():
     autotools.rawInstall("DESTDIR=%s/" % get.installDIR())
 
-    pisitools.removeDir("/usr/lib/ConsoleKit/scripts/")
-
     pisitools.dodir("/var/run/ConsoleKit")
+
     # pam_console-compat
     pisitools.dodir("/var/run/console")
 
