5714353e2aa5e8dd4f018323fcf6dda3c1df4278
Ozan Çağlayan <ozancaglayan@users.noreply.github.com>
2009-12-21 14:46:36 +0000

    Disable gtk-doc

--- system/base/PolicyKit/actions.py
+++ system/base/PolicyKit/actions.py
@@ -16,6 +16,7 @@ def setup():
                          --with-polkit-user=polkit \
                          --with-polkit-group=polkit \
                          --disable-man-pages \
+                         --disable-gtk-doc \
                          --localstatedir=/var \
                          --disable-static")
 
