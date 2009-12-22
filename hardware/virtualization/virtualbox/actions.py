0b7279f638391fb872f0a48568fb5c9d66e8df75
Fatih Arslan <ftharsln@gmail.com>
2009-12-22 12:31:39 +0000

    VirtualBox 3.1.2
    - Bump to latest stable release
    - Use drivers for xorg server 1.7
    - Refactor actions.py
    - Install python bindings

--- hardware/virtualization/virtualbox/actions.py
+++ hardware/virtualization/virtualbox/actions.py
@@ -1,4 +1,3 @@
-#!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
 # Licensed under the GNU General Public License, version 2.
@@ -7,72 +6,82 @@
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import shelltools
+from pisi.actionsapi import pythonmodules
 from pisi.actionsapi import get
-
-import os
 
 WorkDir = "VirtualBox-%s_OSE" % get.srcVERSION()
 
-installDir = "/usr/lib/virtualbox"
-XorgVersion = "16"
+VBoxLibDir = "/usr/lib/virtualbox"
+VBoxDataDir = "/usr/share/virtualbox"
+XorgVersion = "17"
 
 def setup():
-    shelltools.makedirs("build")
-    shelltools.sym("build/linux.x86/release/bin", "out")
+    pisitools.dosed("LocalConfig.kmk", "__VBOXLIBDIR__", VBoxLibDir)
+    pisitools.dosed("LocalConfig.kmk", "__VBOXDATADIR__", VBoxDataDir)
+
+    shelltools.echo("vbox.cfg", "INSTALL_DIR=%s" % VBoxLibDir)
 
     # TODO: Enable web service when we have soapcpp2
     autotools.rawConfigure("--disable-kmods \
                             --enable-hardening \
                             --with-gcc=%s \
                             --with-g++=%s \
-                            --with-qt-dir=/usr/qt/4 \
-                            --out-path=build" % (get.CC(), get.CXX()))
+                            --with-qt-dir=/usr/qt/4" % (get.CC(), get.CXX()))
 
 def build():
-    shelltools.system("source build/env.sh && kmk")
+    shelltools.system("source env.sh && kmk")
 
 def install():
-    exclude = ("additions", "sdk", "src", "SUP", "testcase", "vboxkeyboard", "tst", "xpidl")
+    pisitools.insinto("/etc/vbox", "vbox.cfg")
+    pisitools.insinto("/etc/X11/Xsession.d", "src/VBox/Additions/x11/Installer/98vboxadd-xclient", "98-vboxclient.sh")
+    pisitools.insinto("/usr/bin", "src/VBox/Additions/x11/Installer/VBoxRandR.sh", "VBoxRandR")
+    pisitools.insinto("/usr/share/X11/pci", "src/VBox/Additions/x11/Installer/vboxvideo.ids")
+    pisitools.insinto("/usr/share/hal/fdi/policy/20thirdparty", "src/VBox/Additions/linux/installer/90-vboxguest.fdi")
 
-    for _file in shelltools.ls("out"):
+    shelltools.cd("out/linux.x86/release/bin")
+
+    exclude = ("additions", "nls", "sdk", "src", "SUP", "vboxkeyboard",
+               "VBox.sh", "VBoxSysInfo.sh", "VBoxTunctl", "testcase", "tst", "xpidl")
+
+    for _file in shelltools.ls("."):
         if _file.startswith(exclude):
             continue
 
         print "Installing %s ..." % _file
-        pisitools.insinto(installDir, "out/%s" % _file)
+        pisitools.insinto(VBoxLibDir, _file)
+
+    pisitools.dobin("VBoxTunctl")
+
+    pisitools.dobin("VBox*.sh", VBoxDataDir)
+    pisitools.insinto(VBoxDataDir, "nls")
 
     # Symlinks
     # TODO: Add vboxwebsrv when ready
     apps = ("VBoxHeadless", "VBoxManage", "VBoxSDL", "VBoxVRDP", "VirtualBox")
-    relativeInstallDir = os.path.relpath(installDir, "/usr/bin")
     for link in apps:
-        pisitools.dosym("%s/VBox.sh" % relativeInstallDir, "/usr/bin/%s" % link)
+        pisitools.dosym("../share/virtualbox/VBox.sh", "/usr/bin/%s" % link)
 
-    pisitools.dosym("%s/VBoxTunctl" % relativeInstallDir, "/usr/bin/VBoxTunctl")
-
-    # Configuration file
-    pisitools.dodir("/etc/vbox")
-    shelltools.echo("%s/etc/vbox/vbox.cfg" % get.installDIR(), "INSTALL_DIR=%s" % installDir)
-
-    # Desktop file
-    pisitools.domove("%s/*.desktop" % installDir, "/usr/share/applications")
-    pisitools.domove("%s/*.png" % installDir, "/usr/share/pixmaps")
+    # Desktop file and icon
+    pisitools.domove("%s/*.desktop" % VBoxLibDir, "/usr/share/applications")
+    pisitools.domove("%s/*.png" % VBoxLibDir, "/usr/share/pixmaps")
 
     # Guest additions
-    pisitools.dobin("out/additions/VBoxClient")
-    pisitools.dosbin("out/additions/VBoxControl")
-    pisitools.dosbin("out/additions/VBoxService")
+    pisitools.dobin("additions/VBoxClient")
+    pisitools.dobin("additions/VBoxControl")
 
-    pisitools.insinto("/sbin", "out/additions/mountvboxsf", "mount.vboxsf")
+    pisitools.dosbin("additions/VBoxService")
+    pisitools.dosbin("additions/mount.vboxsf")
 
-    pisitools.insinto("/etc/X11/Xsession.d", "src/VBox/Additions/x11/Installer/98vboxadd-xclient", "98-vboxclient.sh")
-    pisitools.insinto("/usr/bin", "src/VBox/Additions/x11/Installer/VBoxRandR.sh", "VBoxRandR")
-
-    pisitools.dolib("out/additions/VBoxOGL*")
+    pisitools.dolib("additions/VBoxOGL*")
     pisitools.dosym("../../../VBoxOGL.so", "/usr/lib/xorg/modules/dri/vboxvideo_dri.so")
 
-    pisitools.insinto("/usr/share/X11/pci", "src/VBox/Additions/x11/Installer/vboxvideo.ids")
-    pisitools.insinto("/usr/lib/xorg/modules/drivers", "out/additions/vboxvideo_drv_%s.so" % XorgVersion, "vboxvideo_drv.so")
-    pisitools.insinto("/usr/lib/xorg/modules/input",   "out/additions/vboxmouse_drv_%s.so" % XorgVersion, "vboxmouse_drv.so")
+    pisitools.insinto("/usr/lib/xorg/modules/drivers", "additions/vboxvideo_drv_%s.so" % XorgVersion, "vboxvideo_drv.so")
+    pisitools.insinto("/usr/lib/xorg/modules/input",   "additions/vboxmouse_drv_%s.so" % XorgVersion, "vboxmouse_drv.so")
 
-    pisitools.insinto("/usr/share/hal/fdi/policy/10osvendor", "src/VBox/Additions/linux/installer/90-vboxguest.fdi")
+    # Python bindings
+    pisitools.insinto("%s/sdk/bindings/xpcom" % VBoxLibDir, "sdk/bindings/xpcom/python")
+
+    shelltools.cd("sdk/installer")
+    shelltools.copy("vboxapisetup.py", "setup.py")
+    shelltools.export("VBOX_INSTALL_PATH", VBoxLibDir)
+    pythonmodules.install()
