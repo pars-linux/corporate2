68da22de4114cb25218f6a566dfb6e5333a7be3a
Fatih Arslan <ftharsln@gmail.com>
2009-12-24 08:39:32 +0000

    Add cogito package

---
+++ programming/vcs/cogito/actions.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2007 TUBITAK/UEKAE
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+
+def build():
+    autotools.make()
+
+def install():
+    autotools.rawInstall("DESTDIR=%s prefix=/usr" % get.installDIR())
+    pisitools.dodoc("Documentation/*.txt", "Documentation/technical/*.txt", "Documentation/tutorial-script/*.txt")
+
