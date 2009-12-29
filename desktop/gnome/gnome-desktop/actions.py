57dfd616f1559b20fcf863136d3734412be8186f
Ozan Çağlayan <ozancaglayan@users.noreply.github.com>
2009-12-29 16:59:27 +0000

    Add padevchooser dependencies.

---
+++ desktop/gnome/gnome-desktop/actions.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+
+def setup():
+    autotools.autoreconf("-vfi")
+    autotools.configure("--disable-static \
+                         --disable-scrollkeeper \
+                         --enable-gtk-doc \
+                         --with-gnome-distributor=\"Pardus\"")
+
+def build():
+    autotools.make("LIBS=-lm")
+
+def install():
+    autotools.install()
+
+    pisitools.dodoc("AUTHORS", "Change*", "COPY*", "HACKING", "MAINTAINERS", "NEWS", "README")
