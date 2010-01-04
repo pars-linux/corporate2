2660a46c295b8e98031ac3014a0d3f4ed88f8cb1
Ozan Çağlayan <ozancaglayan@users.noreply.github.com>
2010-01-04 10:19:32 +0000

    blueman is ready.

---
+++ hardware/bluetooth/blueman/actions.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2009 TUBITAK/UEKAE
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import shelltools
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+
+def setup():
+    open("py-compile", "w").write("#!/bin/bash\n")
+    autotools.configure("--disable-static \
+                         --disable-polkit \
+                         --enable-hal")
+
+def build():
+    autotools.make()
+
+def install():
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+
+    pisitools.removeDir("/usr/share/polkit-1")
+
+    pisitools.dodoc("AUTHORS", "ChangeLog", "COPYING")
