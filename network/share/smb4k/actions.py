71953c3579a6d8e78a4aa31a5143c50281e23550
Ozan Çağlayan <ozancaglayan@users.noreply.github.com>
2010-01-04 07:10:58 +0000

    Ready.

--- network/share/smb4k/actions.py
+++ network/share/smb4k/actions.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyright 2006,2007 TUBITAK/UEKAE
+# Copyright 2006-2009 TUBITAK/UEKAE
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
 from pisi.actionsapi import kde
-from pisi.actionsapi import shelltools
 from pisi.actionsapi import get
+from pisi.actionsapi import shelltools
+
+KeepSpecial = ["libtool"]
 
 def setup():
     kde.configure()
