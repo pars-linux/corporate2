166186a4214723487d5633abb95c53b00c429cf0
Ozan Çağlayan <ozancaglayan@users.noreply.github.com>
2010-01-06 07:22:52 +0000

    Merge from devel/tex/base/texlive-latex:
    rev. 83141, by semen.cirit on 2009-12-18
    Pisi check problems fixed for texlive-latex.
    
    rev. 83184, by semen.cirit on 2009-12-19
    pdftex and luatex files shoud not be removed from the system.

--- tex/base/texlive-latex/actions.py
+++ tex/base/texlive-latex/actions.py
@@ -6,7 +6,7 @@
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
 from pisi.actionsapi import texlivemodules
-
+from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 
 WorkDir = "%s-%s" % (get.srcNAME(), get.srcVERSION().split("_")[-1])
@@ -17,3 +17,12 @@ def build():
 def install():
     texlivemodules.install()
 
+    pisitools.remove("/var/lib/texmf/web2c/luatex/pdfluatex.log")
+    pisitools.remove("/var/lib/texmf/web2c/pdftex/etex.log")
+    pisitools.remove("/var/lib/texmf/web2c/luatex/lualatex.log")
+    pisitools.remove("/var/lib/texmf/web2c/pdftex/pdftex.log")
+    pisitools.remove("/var/lib/texmf/web2c/luatex/luatex.log")
+    pisitools.remove("/var/lib/texmf/web2c/pdftex/latex.log")
+    pisitools.remove("/var/lib/texmf/web2c/luatex/pdflualatex.log")
+    pisitools.remove("/var/lib/texmf/web2c/pdftex/pdfetex.log")
+    pisitools.remove("/var/lib/texmf/web2c/pdftex/pdflatex.log")
