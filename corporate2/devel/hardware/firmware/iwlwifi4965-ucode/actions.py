d5fceab11ebb27a122d061afeece5ec77b79b925
ozan.caglayan <ozan.caglayan@420bcd57-4a62-4fd6-832e-5ede16c90cc9>
2010-01-06 07:21:36 +0000

    Merge from devel/hardware/firmware/iwlwifi4965-ucode/:
    rev. 84120, by ozan.caglayan on 2010-01-06
    Fix file permission

--- corporate2/devel/hardware/firmware/iwlwifi4965-ucode/actions.py
+++ corporate2/devel/hardware/firmware/iwlwifi4965-ucode/actions.py
@@ -5,6 +5,7 @@
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
+from pisi.actionsapi import shelltools
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 
@@ -12,3 +13,4 @@ WorkDir = "iwlwifi-4965-ucode-%s" % get.srcVERSION()
 
 def install():
     pisitools.insinto("/lib/firmware", "*.ucode")
+    shelltools.chmod("%s/lib/firmware/*.ucode" % get.installDIR(), 0644)
