fa64aad68a8bfd81ad25c6b0a1f3e93b8cd2b4dd
Ozan Çağlayan <ozancaglayan@users.noreply.github.com>
2010-01-07 07:14:02 +0000

    Bump to 0.7.6, split packages

--- util/crypt/pinentry/actions.py
+++ util/crypt/pinentry/actions.py
@@ -1,33 +1,40 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyright 2005-2008 TUBITAK/UEKAE
+# Copyright 2005-2010 TUBITAK/UEKAE
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
-from pisi.actionsapi import kde
-from pisi.actionsapi import pisitools
 from pisi.actionsapi import shelltools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import autotools
 from pisi.actionsapi import get
 
 def setup():
-    shelltools.export("LDFLAGS", "%s -Wl,-z,now" % get.LDFLAGS())
+    autotools.configure("--disable-dependency-tracking \
+                         --disable-rpath \
+                         --disable-pinentry-gtk \
+                         --enable-pinentry-qt4 \
+                         --enable-pinentry-curses \
+                         --enable-pinentry-qt \
+                         --infodir=/usr/share/info")
 
-    shelltools.system("./autogen.sh")
-    kde.configure("--disable-dependency-tracking \
-                   --enable-maintainer-mode \
-                   --disable-pinentry-gtk \
-                   --disable-pinentry-gtk2 \
-                   --enable-pinentry-qt \
-                   --enable-pinentry-curses \
-                   --enable-fallback-curses \
-                   --infodir=/usr/share/info")
+    # Modify binary names for Qt3 and Qt4
+    pisitools.dosed("qt/Makefile.in", "pinentry-qt", "pinentry-qt3")
+    pisitools.dosed("qt4/Makefile.in", "pinentry-qt4", "pinentry-qt")
+
+    # Fix MOC files
+    shelltools.cd("qt4")
+    shelltools.system("moc-qt4 pinentrydialog.h > pinentrydialog.moc")
+    shelltools.system("moc-qt4 qsecurelineedit.h > qsecurelineedit.moc")
 
 def build():
-    kde.make()
+    autotools.make()
 
 def install():
-    kde.install()
+    autotools.install()
 
-    pisitools.remove("/usr/share/info/dir")
+    pisitools.dosym("pinentry-gtk-2", "/usr/bin/pinentry-gtk")
+    pisitools.remove("/usr/bin/pinentry")
+
     pisitools.dodoc("AUTHORS", "ChangeLog", "NEWS", "README", "THANKS")
