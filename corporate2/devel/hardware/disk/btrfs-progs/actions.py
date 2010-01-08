9711defafd55d1c8c2d3a21e9c55b157144d6ea0
ozan.caglayan <ozan.caglayan@420bcd57-4a62-4fd6-832e-5ede16c90cc9>
2010-01-08 10:23:12 +0000

    Bump to 0.19 for format change in 2.6.31

--- corporate2/devel/hardware/disk/btrfs-progs/actions.py
+++ corporate2/devel/hardware/disk/btrfs-progs/actions.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyright 2009 TUBITAK/UEKAE
+# Copyright 2009-2010 TUBITAK/UEKAE
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
@@ -10,8 +10,8 @@ from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 
 def build():
-    autotools.make("CFLAGS=\"%s\"" % get.CFLAGS())
-    autotools.make("convert")
+    autotools.make("CC=%s CFLAGS=\"%s\"" % (get.CC(), get.CFLAGS()))
+    autotools.make("CC=%s CFLAGS=\"%s\" convert" % (get.CC(), get.CFLAGS()))
 
 def install():
     pisitools.dosed("man/Makefile", "^prefix \?= .*$", "prefix = /usr/share")
