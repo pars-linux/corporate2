ffcc2a285235f45cfa6e2f003974fbed099c64a8
ozan.caglayan <ozan.caglayan@420bcd57-4a62-4fd6-832e-5ede16c90cc9>
2010-01-14 19:05:41 +0000

    Add ktorrent

---
+++ corporate2/devel/network/p2p/ktorrent/actions.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2005-2008 TUBITAK/UEKAE
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import kde
+from pisi.actionsapi import pisitools
+
+KeepSpecial = ["libtool"]
+
+def setup():
+    # needed by networkstatus.patch
+    kde.make("-f admin/Makefile.common")
+    kde.configure()
+
+def build():
+    kde.make()
+
+def install():
+    kde.install()
