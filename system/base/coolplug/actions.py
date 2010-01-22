380aabcfc293c57882dff087a5f5edee1cb9b409
Ozan Çağlayan <ozancaglayan@users.noreply.github.com>
2010-01-22 14:08:58 +0000

    Revert to 0.4

--- system/base/coolplug/actions.py
+++ system/base/coolplug/actions.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyright 2005-2009 TUBITAK/UEKAE
+# Copyright 2005-2010 TUBITAK/UEKAE
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
@@ -9,6 +9,7 @@ from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
 
 NoStrip = ["/"]
+WorkDir = "coolplug"
 
 def build():
     autotools.make("CC=dietlibc-gcc")
