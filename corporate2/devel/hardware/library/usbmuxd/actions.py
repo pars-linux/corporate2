2959d3bd0f10546384e730e77fdd26dbf4da3f52
ozan.caglayan <ozan.caglayan@420bcd57-4a62-4fd6-832e-5ede16c90cc9>
2010-01-23 09:10:14 +0000

    Ready for stable

---
+++ corporate2/devel/hardware/library/usbmuxd/actions.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2009 TUBITAK/UEKAE
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import cmaketools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+
+WorkDir = "%s-%s" % (get.srcNAME(), get.srcVERSION().replace("_", "-"))
+
+def setup():
+    pisitools.dosed("udev/85-usbmuxd.rules.in", "-u -U", "-u")
+
+    cmaketools.configure("-DCMAKE_SKIP_RPATH=ON")
+
+def build():
+    cmaketools.make()
+
+def install():
+    cmaketools.rawInstall("DESTDIR=%s" % get.installDIR())
+
+    pisitools.dodoc("AUTHORS", "COPYING*", "README", "README.devel")
