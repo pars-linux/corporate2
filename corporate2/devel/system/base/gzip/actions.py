dcdd11b3292e859291b60ff60c7df1e7f9201047
ozan.caglayan <ozan.caglayan@420bcd57-4a62-4fd6-832e-5ede16c90cc9>
2010-01-24 09:36:52 +0000

    Version bump

--- corporate2/devel/system/base/gzip/actions.py
+++ corporate2/devel/system/base/gzip/actions.py
@@ -1,19 +1,18 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyright 2005-2008 TUBITAK/UEKAE
+# Copyright 2005-2010 TUBITAK/UEKAE
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
+from pisi.actionsapi import shelltools
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
-from pisi.actionsapi import shelltools
 from pisi.actionsapi import get
 
 def setup():
-    shelltools.export("DEFS","NO_ASM")
-    autotools.configure("--exec-prefix=/ \
-                         --enable-nls")
+    shelltools.export("DEFS", "NO_ASM")
+    autotools.configure("--exec-prefix=/")
 
 def build():
     autotools.make()
