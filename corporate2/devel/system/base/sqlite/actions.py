0c7c0e4c8072ccb8e2560a6116fa6c134aa29322
erenturkay <erenturkay@420bcd57-4a62-4fd6-832e-5ede16c90cc9>
2010-01-25 09:46:30 +0000

    Merge from devel/system/base/sqlite:
    rev. 85346, by erenturkay on 2010-01-25
    * Add SQLITE_SECURE_DELETE compile-time option. Even if the data is deleted with sqlite query, the traces of the deleted data still remains in the file but cannot be seen with sqlite query. However, it can be seen by opening the file with a text editor. SQLITE_SECURE_DELETE overwrites written data with zeros.
    
    S2: Less Critical
    Q4: Stable update but better to go to testing repo.
    
    BUG:COMMENT:12137

--- corporate2/devel/system/base/sqlite/actions.py
+++ corporate2/devel/system/base/sqlite/actions.py
@@ -11,7 +11,16 @@ from pisi.actionsapi import shelltools
 from pisi.actionsapi import get
 
 def setup():
-    shelltools.export("CFLAGS", "%s -DSQLITE_ENABLE_COLUMN_METADATA=1 -DSQLITE_DISABLE_DIRSYNC=1 -DSQLITE_ENABLE_FTS3=3 -DSQLITE_ENABLE_RTREE=1 -fno-strict-aliasing" % get.CFLAGS())
+    # Use secure delete. Even if the data is deleted with sqlite query, the traces of the deleted data still remains in the file
+    # but cannot be seen with sqlite query. However, it can be seen by opening the file with a text editor.
+    # SQLITE_SECURE_DELETE overwrites written data with zeros.
+    shelltools.export("CFLAGS", "%s \
+                       -DSQLITE_SECURE_DELETE=1 \
+                       -DSQLITE_ENABLE_COLUMN_METADATA=1 \
+                       -DSQLITE_DISABLE_DIRSYNC=1 \
+                       -DSQLITE_ENABLE_FTS3=3 \
+                       -DSQLITE_ENABLE_RTREE=1 \
+                       -fno-strict-aliasing" % get.CFLAGS())
     autotools.configure("--disable-static")
 
 def build():
