dfdd2af97f1b3905bf1e0c853ba21ac4c128cc05
ozan.caglayan <ozan.caglayan@420bcd57-4a62-4fd6-832e-5ede16c90cc9>
2010-01-28 10:04:56 +0000

    Fix automake and Python detection

--- corporate2/devel/desktop/kde/base/kdegames/actions.py
+++ corporate2/devel/desktop/kde/base/kdegames/actions.py
@@ -1,21 +1,26 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyright 2005-2009 TUBITAK/UEKAE
+# Copyright 2005-2010 TUBITAK/UEKAE
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
 from pisi.actionsapi import kde
 from pisi.actionsapi import get
+from pisi.actionsapi import pisitools
 from pisi.actionsapi import shelltools
 
 KeepSpecial=["libtool"]
 shelltools.export("HOME", get.workDIR())
 
 def setup():
+    # Fix automake and python detection
+    pisitools.dosed("admin/cvs.sh", "automake\*1\.10\*", "automake*1.1[0-5]*")
+    pisitools.dosed("admin/acinclude.m4.in", "KDE_CHECK_PYTHON_INTERN\(\"2.5", "KDE_CHECK_PYTHON_INTERN(\"%s" % get.curPYTHON().split("python")[1])
+    kde.make("-f admin/Makefile.common")
+
     shelltools.export("DO_NOT_COMPILE", "atlantik")
-    kde.configure("--disable-setgid \
-                   --without-arts")
+    kde.configure("--disable-setgid")
 
 def build():
     kde.make()
