27e058afdf0b671033bdee7941b7dcac80f01e66
ozan.caglayan <ozan.caglayan@420bcd57-4a62-4fd6-832e-5ede16c90cc9>
2010-01-28 10:14:17 +0000

    Fix automake and Python detection

--- corporate2/devel/desktop/kde/base/kdenetwork/actions.py
+++ corporate2/devel/desktop/kde/base/kdenetwork/actions.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyright 2005-2009 TUBITAK/UEKAE
+# Copyright 2005-2010 TUBITAK/UEKAE
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
 from pisi.actionsapi import kde
 from pisi.actionsapi import get
-from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import shelltools
 
@@ -15,14 +14,16 @@ KeepSpecial=["libtool"]
 shelltools.export("HOME", get.workDIR())
 
 def setup():
-    shelltools.export("DO_NOT_COMPILE", "ksirc wifi lanbrowsing")
+    # Fix automake and python detection
+    pisitools.dosed("admin/cvs.sh", "automake\*1\.10\*", "automake*1.1[0-5]*")
+    pisitools.dosed("admin/acinclude.m4.in", "KDE_CHECK_PYTHON_INTERN\(\"2.5", "KDE_CHECK_PYTHON_INTERN(\"%s" % get.curPYTHON().split("python")[1])
+    kde.make("-f admin/Makefile.common")
 
-    kde.make("-f Makefile.cvs")
+    shelltools.export("DO_NOT_COMPILE", "ksirc wifi lanbrowsing")
     kde.configure("--with-slp \
                    --with-wifi \
                    --disable-sametime-plugin \
                    --without-xmms \
-                   --without-arts \
                    --without-external-libgadu")
 
 def build():
