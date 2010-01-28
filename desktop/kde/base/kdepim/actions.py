c82a051c87de0b37b0d5321ea78d28a99e6d4e8b
Ozan Çağlayan <ozancaglayan@users.noreply.github.com>
2010-01-28 10:16:47 +0000

    Fix automake and Python detection

--- desktop/kde/base/kdepim/actions.py
+++ desktop/kde/base/kdepim/actions.py
@@ -1,24 +1,26 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyright 2005-2009 TUBITAK/UEKAE
+# Copyright 2005-2010 TUBITAK/UEKAE
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
 from pisi.actionsapi import kde
 from pisi.actionsapi import get
-from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
 from pisi.actionsapi import shelltools
 
 KeepSpecial=["libtool"]
 shelltools.export("HOME", get.workDIR())
 
 def setup():
-    shelltools.export("DO_NOT_COMPILE", "kandy korn networkstatus")
+    # Fix automake and python detection
+    pisitools.dosed("admin/cvs.sh", "automake\*1\.10\*", "automake*1.1[0-5]*")
+    pisitools.dosed("admin/acinclude.m4.in", "KDE_CHECK_PYTHON_INTERN\(\"2.5", "KDE_CHECK_PYTHON_INTERN(\"%s" % get.curPYTHON().split("python")[1])
+    kde.make("-f admin/Makefile.common")
 
-    autotools.make("-f admin/Makefile.common")
-    kde.configure("--with-sasl \
-                   --without-arts")
+    shelltools.export("DO_NOT_COMPILE", "kandy korn networkstatus")
+    kde.configure("--with-sasl")
 
 def build():
     kde.make("-j1")
