622a525e63140ebc0c9703d8e8154172916e19f6
ozan.caglayan <ozan.caglayan@420bcd57-4a62-4fd6-832e-5ede16c90cc9>
2010-01-28 10:06:50 +0000

    Fix automake and Python detection

--- corporate2/devel/desktop/kde/base/kdelibs/actions.py
+++ corporate2/devel/desktop/kde/base/kdelibs/actions.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyright 2005-2009 TUBITAK/UEKAE
+# Copyright 2005-2010 TUBITAK/UEKAE
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
@@ -15,7 +15,11 @@ KeepSpecial=["libtool"]
 shelltools.export("HOME", get.workDIR())
 
 def setup():
+    # Fix automake and python detection
+    pisitools.dosed("admin/cvs.sh", "automake\*1\.10\*", "automake*1.1[0-5]*")
+    pisitools.dosed("admin/acinclude.m4.in", "KDE_CHECK_PYTHON_INTERN\(\"2.5", "KDE_CHECK_PYTHON_INTERN(\"%s" % get.curPYTHON().split("python")[1])
     kde.make("-f admin/Makefile.common")
+
     kde.configure('--with-distribution="Pardus" \
                    --disable-libfam \
                    --enable-inotify \
@@ -33,7 +37,6 @@ def setup():
                    --enable-gcc-hidden-visibility \
                    --with-aspell \
                    --with-acl \
-                   --without-arts \
                    --without-lua \
                    --without-hspell')
 
