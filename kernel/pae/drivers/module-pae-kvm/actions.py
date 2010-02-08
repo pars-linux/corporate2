20c2b46c9b43201f8bf249bc59f06c1970d3f284
Ozan Çağlayan <ozancaglayan@users.noreply.github.com>
2010-02-08 10:34:49 +0000

    Bump

--- kernel/pae/drivers/module-pae-kvm/actions.py
+++ kernel/pae/drivers/module-pae-kvm/actions.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyright 2007-2009 TUBITAK/UEKAE
+# Copyright 2007-2010 TUBITAK/UEKAE
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
@@ -14,11 +14,9 @@ NoStrip=["/usr/share/kvm"]
 WorkDir="kvm-kmod-%s" % get.srcVERSION()
 
 def setup():
-    # Don't run depmod
-    pisitools.dosed("Makefile","/sbin/depmod", "/bin/true")
-
-    autotools.rawConfigure('--arch=x86 \
-                            --kerneldir=/lib/modules/%s/build' % kerneltools.getKernelVersion())
+    autotools.rawConfigure('--arch=%s \
+                            --kerneldir=/lib/modules/%s/build' % (get.ARCH().replace("i686", "x86"),
+                                                                  kerneltools.getKernelVersion()))
 
 def build():
     autotools.make()
@@ -28,4 +26,4 @@ def install():
 
     # FIXME: Remove internal rules for now, they're more bleeding-edge oriented
     pisitools.removeDir("/etc/udev/rules.d")
-    pisitools.removeDir("/usr/local")
+    pisitools.removeDir("/usr")
