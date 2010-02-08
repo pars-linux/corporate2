188bc9a8fd0d9c42635f356dacabdb355f508741
Ozan Çağlayan <ozancaglayan@users.noreply.github.com>
2010-02-08 10:20:14 +0000

    Merge from devel/programming/language/python/geoip-python/:
    rev. 86052, by cihan on 2010-02-04
    version bump
    add description tag to pspec.xml
    add translations.xml

--- programming/language/python/geoip-python/actions.py
+++ programming/language/python/geoip-python/actions.py
@@ -1,14 +1,17 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*- 
 #
-# Copyright 2006 TUBITAK/UEKAE
+# Copyright 2006-2010 TUBITAK/UEKAE
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import pythonmodules
 
-WorkDir="GeoIP-Python-1.2.1"
+WorkDir="GeoIP-Python-1.2.4"
+
+def build():
+    pythonmodules.compile()
 
 def install():
     pythonmodules.install()
