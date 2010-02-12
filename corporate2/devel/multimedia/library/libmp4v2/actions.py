2b155268232abfc77426cbbf7e9ba6770cf5f43d
onur <onur@420bcd57-4a62-4fd6-832e-5ede16c90cc9>
2010-02-12 07:19:45 +0000

    switch to maintained version from code.google.com

--- corporate2/devel/multimedia/library/libmp4v2/actions.py
+++ corporate2/devel/multimedia/library/libmp4v2/actions.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyright 2006-2009 TUBITAK/UEKAE
+# Copyright 2006-2010 TUBITAK/UEKAE
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
@@ -11,48 +11,23 @@ from pisi.actionsapi import shelltools
 from pisi.actionsapi import libtools
 from pisi.actionsapi import get
 
-WorkDir = "mpeg4ip-%s" % get.srcVERSION()
+WorkDir = "mp4v2-%s" % get.srcVERSION()
 
-srclib = "lib/mp4v2"
-targetinc = "/usr/include/libmp4v2"
+# autoreconf breaks sandbox with subversion
+shelltools.export("HOME", get.workDIR())
 
 def setup():
-    shelltools.touch("bootstrapped")
-    autotools.configure("--disable-warns-as-err \
-                         --disable-server \
-                         --disable-player \
-                         --disable-mp4live \
-                         --disable-id3tags \
-                         --disable-xvid \
-                         --disable-a52dec \
-                         --disable-mad \
-                         --disable-mpeg2dec \
-                         --disable-srtp \
-                         --disable-mp3lame \
-                         --disable-faac \
-                         --disable-ffmpeg \
-                         --disable-x264 \
-                         --disable-static")
-
-    pisitools.dosed("%s/Makefile" % srclib, "SUBDIRS = . test util", "SUBDIRS = .")
+    autotools.autoreconf("-vfi")
+    autotools.configure("--disable-dependency-tracking \
+                         --disable-static \
+                         --enable-util \
+                         --disable-gch")
 
 def build():
-    shelltools.cd(srclib)
     autotools.make()
 
 def install():
-    pisitools.dodir(targetinc)
-
-    shelltools.cd(srclib)
     autotools.rawInstall("DESTDIR=%s" % get.installDIR())
-    pisitools.dodoc("README", "INTERNALS", "API_CHANGES", "TODO")
 
-    shelltools.cd("../..")
-    for f in ["include/mpeg4ip_version.h", "include/mpeg4ip.h", "mpeg4ip_config.h"]:
-        pisitools.insinto(targetinc, f)
-
-    pisitools.dosed("%s/usr/include/mp4.h" % get.installDIR(), '"mpeg4ip.h"', '<libmp4v2/mpeg4ip.h>')
-    pisitools.dosed("%s/%s/mpeg4ip.h" % (get.installDIR(), targetinc), "mpeg4ip_config.h", "libmp4v2/mpeg4ip_config.h")
-    pisitools.dosed("%s/%s/mpeg4ip.h" % (get.installDIR(), targetinc), '"mpeg4ip_version.h"', '<libmp4v2/mpeg4ip_version.h>')
-    pisitools.dosed("%s/%s/mpeg4ip.h" % (get.installDIR(), targetinc), "#include <systems.h>", "#include <sys/types.h>")
+    pisitools.dodoc("README", "doc/*.txt")
 
