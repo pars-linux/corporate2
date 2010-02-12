4426c5f0735f90aced522425d6beee71daa97da4
onur <onur@420bcd57-4a62-4fd6-832e-5ede16c90cc9>
2010-02-12 07:19:20 +0000

    version bump to cleaner version, required by new packages yet to be added to repo, breaks abi

--- corporate2/devel/multimedia/sound/faad2/actions.py
+++ corporate2/devel/multimedia/sound/faad2/actions.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyright 2005-2008 TUBITAK/UEKAE
+# Copyright 2005-2010 TUBITAK/UEKAE
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
@@ -9,12 +9,13 @@ from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 
-WorkDir = "faad2"
 
 def setup():
-    autotools.autoreconf("-fi")
-    autotools.configure("--with-mp4v2 \
-                         --without-xmms \
+    pisitools.dosed("libfaad/Makefile.am", "iquote ", "I")
+    autotools.autoreconf("-vfi")
+
+    autotools.configure("--without-xmms \
+                         --with-drm \
                          --disable-static")
 
 def build():
@@ -22,6 +23,5 @@ def build():
 
 def install():
     autotools.rawInstall("DESTDIR=%s" % get.installDIR())
-    pisitools.dosed("%s/usr/include/mp4ff.h" % get.installDIR(), '"mp4ff_int_types.h"', '<stdint.h>')
 
     pisitools.dodoc("AUTHORS", "ChangeLog", "NEWS", "README", "README.linux", "TODO")
