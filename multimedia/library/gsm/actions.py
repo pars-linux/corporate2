0968fb4129ee62b7d720cdfb0fa96772e176439d
Onur Küçük <zaburt@users.noreply.github.com>
2010-02-15 13:29:58 +0000

    version bump, tidy up package

--- multimedia/library/gsm/actions.py
+++ multimedia/library/gsm/actions.py
@@ -8,7 +8,11 @@ from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 
-WorkDir="gsm-1.0-pl12"
+WorkDir="gsm-1.0-pl13"
+
+def setup():
+    pisitools.dosed("Makefile", "pardusCC", get.CC())
+    pisitools.dosed("Makefile", "pardusCFLAGS", get.CFLAGS())
 
 def build():
     autotools.make()
@@ -18,11 +22,11 @@ def install():
 
     for bin in ["tcat","untoast"]:
         pisitools.remove("/usr/bin/%s" % bin)
-        pisitools.dosym("/usr/bin/toast", "/usr/bin/%s" % bin)
+        pisitools.dosym("toast", "/usr/bin/%s" % bin)
 
     # Move gsm.h out of gsm subdir
-    pisitools.insinto("/usr/include","inc/gsm.h")
-    pisitools.removeDir("/usr/include/gsm")
+    # pisitools.insinto("/usr/include","inc/gsm.h")
+    # pisitools.removeDir("/usr/include/gsm")
 
     # No static libs
     pisitools.remove("/usr/lib/libgsm.a")
