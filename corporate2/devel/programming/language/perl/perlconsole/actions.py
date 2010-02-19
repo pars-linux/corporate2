920810e5aa40134371fa136b08a460cc11d06bd7
ozan.caglayan <ozan.caglayan@420bcd57-4a62-4fd6-832e-5ede16c90cc9>
2010-02-19 11:09:56 +0000

    Use get.ARCH() in actions.py

--- corporate2/devel/programming/language/perl/perlconsole/actions.py
+++ corporate2/devel/programming/language/perl/perlconsole/actions.py
@@ -20,5 +20,5 @@ def build():
 def install():
     perlmodules.install()
 
-    pisitools.removeDir("/usr/lib/perl5/vendor_perl/%s/i686-linux-thread-multi/" % get.curPERL())
+    pisitools.removeDir("/usr/lib/perl5/vendor_perl/%s/%s-linux-thread-multi/" % (get.curPERL(), get.ARCH()))
     pisitools.dodoc("CHANGES", "README", "COPYING", "AUTHORS")
