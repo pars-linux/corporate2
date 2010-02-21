e29dc20f6de7830085e5ed30b1b193a2b8b0af86
ozan.caglayan <ozan.caglayan@420bcd57-4a62-4fd6-832e-5ede16c90cc9>
2010-02-21 13:29:07 +0000

    Ready for stable

---
+++ corporate2/devel/network/connection/ModemManager/comar/service.py
@@ -0,0 +1,21 @@
+from comar.service import *
+import os
+
+serviceType = "local"
+serviceDefault = "on"
+serviceDesc = _({"en": "Modem Manager",
+                 "tr": "Modem Yöneticisi"})
+
+@synchronized
+def start():
+    startService(command="/usr/sbin/modem-manager",
+                 donotify=True,
+                 detach=True)
+
+@synchronized
+def stop():
+    stopService(command="/usr/sbin/modem-manager",
+                donotify=True)
+
+def status():
+    return isServiceRunning(command="/usr/sbin/modem-manager")
