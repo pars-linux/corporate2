ea696bec0d4705f9e8675b3ddccaaf0f2eefe686
fatih <fatih@420bcd57-4a62-4fd6-832e-5ede16c90cc9>
2010-02-22 10:03:37 +0000

    Add config files as additional files. Package is ready for build on x86_64.

--- corporate2/devel/system/base/pisi/actions.py
+++ corporate2/devel/system/base/pisi/actions.py
@@ -1,7 +1,5 @@
-#!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyright 2005 TUBITAK/UEKAE
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
@@ -16,3 +14,5 @@ def install():
     pisitools.domove("/usr/lib/%s/site-packages/pisi/" % get.curPYTHON(), "/usr/lib/pardus/", "pisi")
 
     pisitools.dosym("/usr/bin/pisi-cli", "/usr/bin/pisi")
+
+    pisitools.insinto("/etc/pisi", "pisi.conf-%s" % get.ARCH(), "pisi.conf")
