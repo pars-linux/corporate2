952cbacdb0f1a85b5a64ea88db1b981f813c6456
Ozan Çağlayan <ozancaglayan@users.noreply.github.com>
2010-02-22 12:43:25 +0000

    Add fedora patches and vpnc-script, create some needed files in postInstall

--- network/connection/vpnc/actions.py
+++ network/connection/vpnc/actions.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyright 2006-2008 TUBITAK/UEKAE
+# Copyright 2006-2010 TUBITAK/UEKAE
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
@@ -10,18 +10,13 @@ from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 
 def build():
-    autotools.make('CFLAGS="%s -fPIE" LDFLAGS="%s -pie -lgcrypt"' % (get.CFLAGS(),get.LDFLAGS()))
+    autotools.make('CC="%s" CFLAGS="%s -fPIE" LDFLAGS="%s -pie -lgcrypt"' % (get.CC(),
+                                                                             get.CFLAGS(),
+                                                                             get.LDFLAGS()))
 
 def install():
-    # bins.
-    pisitools.dobin("vpnc")
-    pisitools.dobin("vpnc-disconnect")
-    pisitools.dobin("pcf2vpnc")
-    pisitools.doexe("vpnc-script", "/etc/vpnc")
+    autotools.rawInstall("DESTDIR=%s PREFIX=/usr" % get.installDIR())
 
-    # docs.
+    pisitools.dodir("/var/run/vpnc")
+
     pisitools.dodoc("README")
-    pisitools.doman("vpnc.8")
-
-    # conf.
-    pisitools.insinto("/etc", "vpnc.conf")
