16aff7a239113e95bf7e5c4d8f4319a31354713e
Ozan Çağlayan <ozancaglayan@users.noreply.github.com>
2010-02-23 20:12:44 +0000

    Version bump

--- system/base/attr/actions.py
+++ system/base/attr/actions.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyright 2005-2007 TUBITAK/UEKAE
+# Copyright 2005-2010 TUBITAK/UEKAE
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
@@ -11,11 +11,6 @@ from pisi.actionsapi import shelltools
 from pisi.actionsapi import get
 
 def setup():
-    shelltools.export("OPTIMIZER", get.CFLAGS())
-    shelltools.export("DEBUG", "-DNDEBUG")
-    # shelltools.export("CC", "gcc")
-
-    autotools.autoconf()
     autotools.rawConfigure("--libdir=/lib \
                             --mandir=/usr/share/man \
                             --libexecdir=/lib \
@@ -24,4 +19,7 @@ def build():
     autotools.make()
 
 def install():
-    autotools.make("DIST_ROOT=%s install install-lib install-dev" % get.installDIR())
+    autotools.make("DESTDIR=%s install install-lib install-dev" % get.installDIR())
+
+    pisitools.remove("/lib/*.a")
+    shelltools.chmod("%s/lib/libattr.so.*.*.*" % get.installDIR(), 0755)
