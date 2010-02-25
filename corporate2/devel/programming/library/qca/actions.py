bf8351bcc250e95fe780268784ab0397aa842d27
fatih <fatih@420bcd57-4a62-4fd6-832e-5ede16c90cc9>
2010-02-25 10:36:48 +0000

    Use -fPIC

--- corporate2/devel/programming/library/qca/actions.py
+++ corporate2/devel/programming/library/qca/actions.py
@@ -1,19 +1,17 @@
-#!/usr/bin/python
 # -*- coding: utf-8 -*-
 
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 
-WorkDir = "qca-1.0"
+WorkDir = "qca-%s" % get.srcVERSION()
 
 def setup():
     autotools.rawConfigure("--prefix=/usr/qt/3")
-    pisitools.dosed("Makefile", "(?m)^(CFLAGS.*)",  "CFLAGS =" + get.CFLAGS())
-    pisitools.dosed("Makefile", "(?m)^(CXXFLAGS.*)", "CXXFLAGS =" + get.CXXFLAGS())
+    pisitools.dosed("Makefile", "^(CXXFLAGS .*)$", r"\1 -fPIC ")
 
 def build():
-    autotools.make()
+    autotools.make("CXX=%s LINK=%s" % (get.CXX(), get.CXX()))
 
 def install():
     autotools.rawInstall("INSTALL_ROOT=%s" % get.installDIR())
