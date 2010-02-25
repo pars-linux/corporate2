593a8057f854a470d90c3daf50fe698ab9064f34
Fatih Arslan <ftharsln@gmail.com>
2010-02-25 10:15:56 +0000

    Use Gentoo patches to use autotools. This also fixes build on x86_64.

--- programming/library/metis/actions.py
+++ programming/library/metis/actions.py
@@ -1,4 +1,3 @@
-#!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
 # Copyright 2006-2009 TUBITAK/UEKAE
@@ -7,19 +6,17 @@
 
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
-from pisi.actionsapi import shelltools
-from pisi.actionsapi import get
 
 WorkDir="metis-4.0"
 
+def setup():
+    autotools.autoreconf("-vif")
+    autotools.configure("--disable-static")
+
 def build():
-    autotools.make("CC=%s" % get.CC())
+    autotools.make()
 
 def install():
-    pisitools.insinto("/usr/lib", "libmetis.so*")
-
-    for header in ["metis.h","defs.h","struct.h","macros.h","rename.h","proto.h"]:
-        shelltools.chmod("Lib/%s" % header, 0644)
-        pisitools.insinto("/usr/include/metis","Lib/%s" % header)
+    autotools.install()
 
     pisitools.dodoc("CHANGES")
