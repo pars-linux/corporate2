8aad2c5902743870c6cb3f8e58e9b09d01a231e1
ozan.caglayan <ozan.caglayan@420bcd57-4a62-4fd6-832e-5ede16c90cc9>
2010-03-01 12:34:22 +0000

    bring kdevelop

---
+++ corporate2/devel/programming/environment/kdevelop/actions.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2005-2010 TUBITAK/UEKAE
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import kde
+from pisi.actionsapi import get
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import shelltools
+
+def setup():
+    # Update the admin dir used in KDE template projects.
+    for i in shelltools.ls("admin/*"):
+        shelltools.copy(i, "parts/appwizard/common/admin/")
+
+    # Fix automake and python detection
+    pisitools.dosed("admin/cvs.sh", "automake\*1\.10\*", "automake*1.1[0-5]*")
+    pisitools.dosed("admin/acinclude.m4.in", "KDE_CHECK_PYTHON_INTERN\(\"2.5", "KDE_CHECK_PYTHON_INTERN(\"%s" % get.curPYTHON().split("python")[1])
+    kde.make("-f admin/Makefile.common")
+
+    kde.configure("--with-kdelibsdoxy-dir=%s/share/doc/HTML/en/kdelibs-apidocs \
+                   --enable-java \
+                   --enable-python \
+                   --enable-ruby \
+                   --enable-ada \
+                   --enable-fortran \
+                   --enable-haskell \
+                   --enable-pascal \
+                   --enable-perl \
+                   --enable-php \
+                   --enable-sql \
+                   --enable-antproject \
+                   --enable-subversion" % get.kdeDIR())
+
+def build():
+    kde.make()
+
+def install():
+    kde.install()
