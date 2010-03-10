992e67f610eb46e7cc41b99bd5ad7e94600230da
ozan.caglayan <ozan.caglayan@420bcd57-4a62-4fd6-832e-5ede16c90cc9>
2010-03-10 09:16:05 +0000

    Version bump, disable PolicyKit as ACL management is now done within udev and CK.

--- corporate2/devel/hardware/info/hal/actions.py
+++ corporate2/devel/hardware/info/hal/actions.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyright 2005-2009 TUBITAK/UEKAE
+# Copyright 2005-2010 TUBITAK/UEKAE
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
+from pisi.actionsapi import shelltools
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
-from pisi.actionsapi import libtools
-from pisi.actionsapi import shelltools
 from pisi.actionsapi import get
 
 def setup():
+    autotools.autoreconf("-fi")
     autotools.configure("--exec-prefix=/usr \
                          --localstatedir=/var \
                          --disable-static \
@@ -19,15 +19,19 @@ def setup():
                          --disable-gtk-doc \
                          --disable-dependency-tracking \
                          --disable-smbios \
+                         --disable-console-kit \
+                         --disable-policy-kit \
+                         --disable-acl-management \
+                         --disable-acpi-ibm \
+                         --disable-parted \
+                         --disable-sonypic \
+                         --without-dell-backlight \
+                         --without-deprecated-keys \
+                         --enable-acpi-acpid \
+                         --enable-acpi-proc \
                          --enable-man-pages \
-                         --enable-console-kit \
-                         --enable-policy-kit \
-                         --enable-acl-management \
-                         --enable-acpi-ibm \
-                         --enable-sonypic \
                          --enable-umount-helper \
                          --with-eject=/usr/bin/eject \
-                         --without-dell-backlight \
                          --with-hal-user=hal \
                          --with-hal-group=hal \
                          --with-udev-prefix=/lib")
