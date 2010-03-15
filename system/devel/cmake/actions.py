e2c4d3083667bf9d9c79e3992bb54cbbc4ddfa78
Fatih Arslan <ftharsln@gmail.com>
2010-03-15 14:23:38 +0000

    Bump to new stable release
    Merged from 2009/devel.

--- system/devel/cmake/actions.py
+++ system/devel/cmake/actions.py
@@ -1,15 +1,10 @@
-#!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyright 2006-2009 TUBITAK/UEKAE
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
 from pisi.actionsapi import autotools
-from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
-
-majorVersion = ".".join(get.srcVERSION().split(".")[:2])
 
 def setup():
     autotools.rawConfigure("--prefix=/usr \
@@ -22,6 +17,3 @@ def build():
 
 def install():
     autotools.rawInstall("DESTDIR=%s" % get.installDIR())
-
-    pisitools.dosed("%s/usr/share/man/man1/*" % get.installDIR(), get.curDIR(), "/usr/share/cmake-%s" % majorVersion)
-    pisitools.dosed("%s/usr/share/doc/cmake/*" % get.installDIR(), get.curDIR(), "/usr/share/cmake-%s" % majorVersion)
