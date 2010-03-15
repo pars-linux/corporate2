d29a5eda61238a39759458fa13ae8a14b7a1f552
ozan.caglayan <ozan.caglayan@420bcd57-4a62-4fd6-832e-5ede16c90cc9>
2010-03-15 07:03:54 +0000

    Version bump

--- corporate2/devel/server/library/libtdb/actions.py
+++ corporate2/devel/server/library/libtdb/actions.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyright 2009 TUBITAK/UEKAE
+# Copyright 2009-2010 TUBITAK/UEKAE
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
@@ -22,9 +22,5 @@ def install():
     autotools.install()
 
     pisitools.remove("/usr/lib/*.a")
-
-    # Create symlinks for so file
-    pisitools.dosym("libtdb.so.%s" % get.srcVERSION(), "/usr/lib/libtdb.so.%s" % get.srcVERSION().split(".")[0])
-    pisitools.dosym("libtdb.so.%s" % get.srcVERSION(), "/usr/lib/libtdb.so")
 
     pisitools.dodoc("docs/README")
