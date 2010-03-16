670a8d05def4c7eeb834bbc47a984fd526c40220
Ozan Çağlayan <ozancaglayan@users.noreply.github.com>
2010-03-16 10:08:09 +0000

    Merge from devel/hardware/library/ifuse/:
    rev. 86190, by inanc on 2010-02-07
    Update new libimobildevice
    
    rev. 86193, by inanc on 2010-02-07
    fix asneeded patch

--- hardware/library/ifuse/actions.py
+++ hardware/library/ifuse/actions.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyright 2009 TUBITAK/UEKAE
+# Copyright 2009-2010 TUBITAK/UEKAE
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
@@ -14,7 +14,7 @@ def setup():
     autotools.configure()
 
 def build():
-    autotools.make()
+    autotools.make("-j1")
 
 def install():
     autotools.rawInstall("DESTDIR=%s" % get.installDIR())
