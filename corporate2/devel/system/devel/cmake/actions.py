cd5d024f9a7f9e1890079c37ffaca0783e274517
fatih <fatih@420bcd57-4a62-4fd6-832e-5ede16c90cc9>
2010-03-16 08:05:53 +0000

    - Fix sandbox violation - Strip cmake major version from the name of data directory - Enable parallel build when bootstrapping

--- corporate2/devel/system/devel/cmake/actions.py
+++ corporate2/devel/system/devel/cmake/actions.py
@@ -4,13 +4,20 @@
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
 from pisi.actionsapi import autotools
+from pisi.actionsapi import shelltools
 from pisi.actionsapi import get
 
 def setup():
-    autotools.rawConfigure("--prefix=/usr \
+    # Fix sandbox violation
+    shelltools.export("BZR_HOME", get.workDIR())
+
+    autotools.rawConfigure("--parallel=%s \
                             --system-libs \
+                            --no-qt-gui \
+                            --prefix=/usr \
+                            --datadir=/share/cmake \
                             --docdir=/share/doc/cmake \
-                            --mandir=/share/man")
+                            --mandir=/share/man" % get.makeJOBS().replace("-j", ""))
 
 def build():
     autotools.make()
