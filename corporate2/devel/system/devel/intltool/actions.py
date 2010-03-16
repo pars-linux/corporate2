ca77929cd836ae9e02af3cea09f9156cc40a7e03
ozan.caglayan <ozan.caglayan@420bcd57-4a62-4fd6-832e-5ede16c90cc9>
2010-03-16 09:51:03 +0000

    Merge from devel/system/devel/intltool/:
    rev. 88522, by ozan.caglayan on 2010-03-16
    Version bump

--- corporate2/devel/system/devel/intltool/actions.py
+++ corporate2/devel/system/devel/intltool/actions.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyright 2005 TUBITAK/UEKAE
+# Copyright 2005-2010 TUBITAK/UEKAE
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
@@ -18,4 +18,4 @@ def build():
 def install():
     autotools.rawInstall("DESTDIR=%s" % get.installDIR())
 
-    pisitools.dodoc("AUTHORS", "ChangeLog", "NEWS", "README", "TODO", "doc/I18N-HOWTO")
+    pisitools.dodoc("AUTHORS", "README", "TODO", "COPYING", "doc/I18N-HOWTO")
