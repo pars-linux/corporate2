b6bde77ea395cf47e2642563d66f888fe0f4ea5d
Fatih Arslan <ftharsln@gmail.com>
2010-03-16 20:40:20 +0000

    New stable release (2.6.1)

--- system/devel/patch/actions.py
+++ system/devel/patch/actions.py
@@ -1,16 +1,13 @@
-#!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyright 2005-2009 TUBITAK/UEKAE
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
-from pisi.actionsapi import shelltools
 from pisi.actionsapi import get
 
-WorkDir = "patch-2.5.9-120-g62d03ac"
+#WorkDir = "patch-2.5.9-120-g62d03ac"
 
 def setup():
     autotools.configure()
