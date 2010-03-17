b9792cc78275f5e6fecadfa508735635c47c5d60
sdalgic <sdalgic@420bcd57-4a62-4fd6-832e-5ede16c90cc9>
2010-03-17 08:27:02 +0000

    Merge from devel/programming/language/python/Pygments:
    rev. 88621, by sdalgic on 2010-03-17
    - Version bump with many bug-fixes, new features and enhancements.
    - Add missing setuptools runtime dependency.

--- corporate2/devel/programming/language/python/Pygments/actions.py
+++ corporate2/devel/programming/language/python/Pygments/actions.py
@@ -8,6 +8,9 @@ from pisi.actionsapi import pisitools
 from pisi.actionsapi import pythonmodules
 from pisi.actionsapi import get
 
+def build():
+    pythonmodules.compile()
+
 def install():
     pythonmodules.install()
 
