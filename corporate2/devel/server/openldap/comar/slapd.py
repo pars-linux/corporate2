694b24801701b70190f2cad6aa38c93c6278ac09
bahadir.kandemir <bahadir.kandemir@420bcd57-4a62-4fd6-832e-5ede16c90cc9>
2010-03-21 20:50:27 +0000

    Fixed service script not accepting multiple values in SERVE configuration

--- corporate2/devel/server/openldap/comar/slapd.py
+++ corporate2/devel/server/openldap/comar/slapd.py
@@ -1,17 +1,21 @@
-from comar.service import *
-
 serviceType = "server"
 serviceDesc = _({"en": "OpenLDAP Server",
                  "tr": "OpenLDAP Sunucusu"})
-serviceConf = "slapd"
+serviceConf = "slapd.conf"
+
+from comar.service import *
 
 def start():
     import os
     os.environ["LC_ALL"] = "C"
     os.environ["LANG"] = "C"
 
+    args = ["-u", "ldap", "-g", "ldap", "-h"]
+    args.extend(config.get("SERVE", "ldap://").split())
+    args.extend(config.get("OPTS", "").split())
+
     startService(command="/usr/libexec/slapd",
-                 args="-u ldap -g ldap -h %s %s" % (config.get("SERVE", "ldap://"), config.get("OPTS", "")),
+                 args=args,
                  pidfile="/var/run/openldap/slapd.pid",
                  donotify=True)
 
