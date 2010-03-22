ce7a6431f68d0ec246eb36df1b6c13739b1a14e7
fatih <fatih@420bcd57-4a62-4fd6-832e-5ede16c90cc9>
2010-03-22 13:13:29 +0000

    New stable release (1.6.3)

--- corporate2/devel/office/misc/doxygen/actions.py
+++ corporate2/devel/office/misc/doxygen/actions.py
@@ -1,7 +1,5 @@
-#!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyright 2005-2009 TUBITAK/UEKAE
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
@@ -20,7 +18,7 @@ def setup():
                             --prefix /usr")
 
 def build():
-    autotools.make()
+    autotools.make("QMAKE=%s/bin/qmake" % get.qtDIR())
 
 def install():
     autotools.rawInstall("DESTDIR=%s" % get.installDIR())
