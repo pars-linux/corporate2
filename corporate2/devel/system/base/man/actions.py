28470b894b16d1a08e3c4e4e5ed0e9adf7852542
ozan.caglayan <ozan.caglayan@420bcd57-4a62-4fd6-832e-5ede16c90cc9>
2010-03-23 14:02:34 +0000

    Merge from devel/system/base/man:
    rev. 88893, by ozan.caglayan on 2010-03-22
    Version bump to support manxx directories

--- corporate2/devel/system/base/man/actions.py
+++ corporate2/devel/system/base/man/actions.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyright 2005-2008 TUBITAK/UEKAE
+# Copyright 2005-2010 TUBITAK/UEKAE
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
@@ -12,17 +12,16 @@ from pisi.actionsapi import get
 WorkDir="man-db-%s" % get.srcVERSION()
 
 def setup():
-     autotools.configure("--enable-dups \
-                          --disable-setuid \
-                          --with-device=utf8 \
-                          --with-zio \
-                          --with-gnu-ld \
-                          --enable-mb-groff \
-                          --with-db=gdbm \
-                          --enable-nls \
-                          --with-config-file=/etc/man.conf \
+     autotools.configure("--disable-setuid \
                           --disable-rpath \
-                          --without-included-gettext")
+                          --docdir=/%s/%s \
+                          --with-device=utf8 \
+                          --with-gnu-ld \
+                          --with-config-file=/etc/man.conf \
+                          --with-db=gdbm \
+                          --enable-mb-groff \
+                          --enable-nls \
+                          --without-included-gettext" % (get.docDIR(), get.srcNAME()))
 
 def build():
     autotools.make("nls=all")
