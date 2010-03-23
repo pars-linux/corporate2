3013cb1a4ccfb9faf0a1299fe023c08cbb234d93
Ozan Çağlayan <ozancaglayan@users.noreply.github.com>
2010-03-23 14:02:45 +0000

    Merge from devel/system/base/man-pages/:
    rev. 88894, by ozan.caglayan on 2010-03-22
    Version bump, add POSIX and some more additional man pages.
    
    rev. 88895, by ozan.caglayan on 2010-03-22
    Fix typo

--- system/base/man-pages/actions.py
+++ system/base/man-pages/actions.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyright 2005-2008 TUBITAK/UEKAE
+# Copyright 2005-2010 TUBITAK/UEKAE
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
@@ -11,6 +11,7 @@ from pisi.actionsapi import get
 
 def install():
     autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+    autotools.rawInstall("DESTDIR=%s -C ../man-pages-posix-2003-a" % get.installDIR())
 
     # These come from attr
     pisitools.remove("/usr/share/man/man2/flistxattr.2")
