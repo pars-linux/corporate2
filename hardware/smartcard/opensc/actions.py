fae2479aca925348ea9fb3b1d86ba2ce701c4a3c
Ozan Çağlayan <ozancaglayan@users.noreply.github.com>
2010-03-24 08:18:50 +0000

    Depend on the library instead of the service

--- hardware/smartcard/opensc/actions.py
+++ hardware/smartcard/opensc/actions.py
@@ -11,17 +11,18 @@ from pisi.actionsapi import shelltools
 from pisi.actionsapi import get
 
 def setup():
+    pisitools.dosed("Makefile.am", "win32 ", "")
     autotools.autoreconf("-fi")
     autotools.configure("--disable-static \
-                         --enable-nsplugin \
-                         --enable-openct \
                          --enable-pcsc \
+                         --enable-openct \
+                         --enable-nsplugin \
                          --with-pinentry=/usr/bin/pinentry \
                          --with-pcsc-provider=/usr/lib/libpcsclite.so.1 \
                          --with-plugindir=/usr/lib/nsbrowser/plugins/")
 
 def build():
-    autotools.make("-j1")
+    autotools.make()
 
 def install():
     autotools.rawInstall("DESTDIR=%s" % get.installDIR())
