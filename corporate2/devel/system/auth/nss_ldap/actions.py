b02ac11fc23a19af50b1dc114c6d6a58b50165ea
ozan.caglayan <ozan.caglayan@420bcd57-4a62-4fd6-832e-5ede16c90cc9>
2010-03-24 13:26:06 +0000

    - Version bump to 265, - Install ldap.conf into /etc, - Enable configurable krb5 gssapi.

--- corporate2/devel/system/auth/nss_ldap/actions.py
+++ corporate2/devel/system/auth/nss_ldap/actions.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyright 2007-2009 TUBITAK/UEKAE
+# Copyright 2007-2010 TUBITAK/UEKAE
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
@@ -13,13 +13,16 @@ from pisi.actionsapi import get
 def setup():
     shelltools.export("CFLAGS","%s -fPIC" % get.CFLAGS())
 
-    autotools.autoreconf()
+    # Change search bases to something generic instead of padl
+    pisitools.dosed("ldap.conf", "dc=padl", "dc=example")
+
+    autotools.autoreconf("-fi")
     autotools.configure("--with-ldap-lib=openldap \
-                         --with-ldap-conf-file=/etc/security/ldap.conf \
-                         --with-ldap-secret-file=/etc/security/ldap.secret \
                          --libdir=/lib \
+                         --disable-dependency-tracking \
                          --enable-sasl \
                          --enable-rfc2307bis \
+                         --enable-configurable-krb5-ccname-gssapi \
                          --enable-paged-results")
 
 def build():
@@ -28,5 +31,8 @@ def build():
 def install():
     pisitools.dodir("/etc")
     autotools.rawInstall("DESTDIR=%s libdir=/lib" % get.installDIR())
+
+    # Move the nsswitch.conf tweaked for ldap stuff into the docs
+    pisitools.domove("/etc/nsswitch.ldap", "/%s/%s" % (get.docDIR(), get.srcNAME()))
 
     pisitools.dodoc("ChangeLog", "ANNOUNCE", "NEWS", "README", "AUTHORS", "ldap.conf")
