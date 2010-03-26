917e577fb13df0be607168254838b618967cd0fc
sdalgic <sdalgic@420bcd57-4a62-4fd6-832e-5ede16c90cc9>
2010-03-26 07:01:43 +0000

    Merge from devel/programming/language/python/imdbpy:
    rev. 88831, by igungor on 2010-03-19
    First, sorry for the mass commit. i had to do this operation sooner or later. Second, please, put a period in Description.
    
    rev. 89083, by sdalgic on 2010-03-26
    Version bump to latest stable version.

--- corporate2/devel/programming/language/python/imdbpy/actions.py
+++ corporate2/devel/programming/language/python/imdbpy/actions.py
@@ -16,5 +16,7 @@ def setup():
 def install():
     pythonmodules.install()
 
+    pisitools.domove("/usr/imdb/locale", "/usr/share/locale")
+    pisitools.removeDir("/usr/imdb")
     pisitools.remove("%s/%s/INSTALL.txt" % (get.docDIR(), get.srcNAME()) )
 
