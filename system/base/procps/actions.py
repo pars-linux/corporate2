7fdad700a11e1849eba2667612141d157d07a15c
Ozan Çağlayan <ozancaglayan@users.noreply.github.com>
2010-03-30 18:51:14 +0000

    Version bupm

--- system/base/procps/actions.py
+++ system/base/procps/actions.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyright 2005-2008 TUBITAK/UEKAE
+# Copyright 2005-2010 TUBITAK/UEKAE
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
@@ -14,14 +14,8 @@ def build():
                    (get.CC(), get.CXXFLAGS(), get.CFLAGS(), get.LDFLAGS()))
 
 def install():
-    autotools.rawInstall('ln_f="ln -sf" ldconfig="true" lib64=lib DESTDIR=%s' % get.installDIR())
-
-    pisitools.insinto("/usr/include/proc/", "proc/*.h")
-
-    # conflicts with coreutils
-    pisitools.remove("/bin/kill")
-    pisitools.remove("/usr/share/man/man1/kill.1")
+    autotools.rawInstall('ln_f="ln -sf" ldconfig="true" lib64=lib DESTDIR=%s SKIP="/bin/kill /usr/share/man/man1/kill.1"' % get.installDIR())
 
     pisitools.dosym("libproc-%s.so" % get.srcVERSION(), "/lib/libproc.so")
 
-    pisitools.dodoc("sysctl.conf", "BUGS", "NEWS", "TODO", "ps/HACKING")
+    pisitools.dodoc("BUGS", "NEWS", "TODO", "ps/HACKING")
