04019516e175480d00f3a30a78fd283af1697262
ozan.caglayan <ozan.caglayan@420bcd57-4a62-4fd6-832e-5ede16c90cc9>
2010-03-30 17:32:21 +0000

    Version bump

--- corporate2/devel/system/base/dnsmasq/actions.py
+++ corporate2/devel/system/base/dnsmasq/actions.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyright 2009 TUBITAK/UEKAE
+# Copyright 2009-2010 TUBITAK/UEKAE
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
@@ -11,7 +11,11 @@ from pisi.actionsapi import get
 
 def setup():
     pisitools.dosed("src/config.h", "/\* #define HAVE_DBUS \*/", "#define HAVE_DBUS")
-    pisitools.dosed("Makefile", "AWK = nawk", "AWK = /bin/awk")
+
+    for f in ("dnsmasq.conf.example", "man/dnsmasq.8", "man/es/dnsmasq.8", "src/config.h"):
+        pisitools.dosed(f, "/var/lib/misc", "/var/lib/dnsmasq")
+
+
 
 def build():
     autotools.make()
@@ -19,5 +23,11 @@ def build():
 def install():
     autotools.rawInstall("PREFIX=%s/usr" % get.installDIR())
     pisitools.insinto("/etc/dbus-1/system.d", "dbus/dnsmasq.conf", "dnsmasq.conf")
+
+    # Install python binding
+    pisitools.insinto("/usr/lib/%s/site-packages" % get.curPYTHON(), "dnsmasq.py")
+
+    pisitools.dodir("/var/lib/dnsmasq")
+
     pisitools.dodoc("CHANGELOG", "COPYING", "COPYING-v3", "FAQ")
     pisitools.dohtml("doc.html", "setup.html")
