0541081e580454a5b5fc2bdd3e373eee2fb8a8b0
Serdar Dalgıç <sd@serdardalgic.org>
2010-04-05 09:02:39 +0000

    Merge from devel/programming/language/python/pydns:
    rev. 88828, by igungor on 2010-03-19
    First, sorry for the mass commit. i had to do this operation sooner or later. Second, please, dont put a period in Summary.
    
    rev. 89558, by sdalgic on 2010-04-05
    version bump to latest stable version.

--- programming/language/python/pydns/actions.py
+++ programming/language/python/pydns/actions.py
@@ -8,8 +8,12 @@ from pisi.actionsapi import pisitools
 from pisi.actionsapi import pythonmodules
 from pisi.actionsapi import get
 
+def setup():
+    pythonmodules.compile()
+
 def install():
     pythonmodules.install()
 
     pisitools.insinto("%s/%s/tests" % (get.docDIR(), get.srcNAME()), "tests/*")
+    pisitools.insinto("%s/%s/tools" % (get.docDIR(), get.srcNAME()), "tools/*")
     pisitools.dodoc("CREDITS.txt")
