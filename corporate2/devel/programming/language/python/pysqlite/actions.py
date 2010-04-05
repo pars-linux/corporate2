a158dffa3751835db554d861b0bf5442449267e3
sdalgic <sdalgic@420bcd57-4a62-4fd6-832e-5ede16c90cc9>
2010-04-05 11:17:16 +0000

    pysqlite changes from 2009 repository, packages lost the synch what a pity :/

--- corporate2/devel/programming/language/python/pysqlite/actions.py
+++ corporate2/devel/programming/language/python/pysqlite/actions.py
@@ -7,11 +7,22 @@
 
 from pisi.actionsapi import pythonmodules
 from pisi.actionsapi import pisitools
+from pisi.actionsapi import shelltools
 from pisi.actionsapi import get
+
+code = "%s/%s/code" % (get.docDIR(), get.srcNAME())
+
+def build():
+    pisitools.dosed("setup.py", "pysqlite2-doc", "share/doc/pysqlite")
+    pythonmodules.compile("build_docs")
+
+def setup():
+    shelltools.chmod("doc/includes/sqlite3/*", 0644)
 
 def install():
     pythonmodules.install()
 
-    pisitools.remove("usr/pysqlite2-doc/install-source.txt")
-#    needs sphinx package for documentation
+    pisitools.dohtml("build/doc/")
+    pisitools.insinto(code, "doc/includes/sqlite3/*")
+    pisitools.remove("%s/%s/install-source.txt" % (get.docDIR(), get.srcNAME()))
 
