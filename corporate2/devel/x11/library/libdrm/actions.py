18590c4b52849746b267ad961e182381ecf532f7
fatih <fatih@420bcd57-4a62-4fd6-832e-5ede16c90cc9>
2010-04-15 06:15:01 +0000

    Split package and bump to new stable release

--- corporate2/devel/x11/library/libdrm/actions.py
+++ corporate2/devel/x11/library/libdrm/actions.py
@@ -4,20 +4,6 @@
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
 from pisi.actionsapi import autotools
-from pisi.actionsapi import pisitools
-
-# kernel-headers package provides these
-kheaders = ("drm.h",
-            "drm_mode.h",
-            "drm_sarea.h",
-            "i915_drm.h",
-            "mga_drm.h",
-            "nouveau_drm.h",
-            "r128_drm.h",
-            "radeon_drm.h",
-            "savage_drm.h",
-            "sis_drm.h",
-            "via_drm.h")
 
 def setup():
     autotools.autoreconf("-vif")
@@ -29,6 +15,3 @@ def build():
 
 def install():
     autotools.install()
-
-    for f in kheaders:
-        pisitools.remove("/usr/include/drm/%s" % f)
