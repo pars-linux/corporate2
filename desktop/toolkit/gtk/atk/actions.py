2a643046c757ab4b1c69b9746336ec3fe5be1692
burak <burak@420bcd57-4a62-4fd6-832e-5ede16c90cc9>
2010-04-18 14:56:23 +0000

    version bump

--- desktop/toolkit/gtk/atk/actions.py
+++ desktop/toolkit/gtk/atk/actions.py
@@ -7,8 +7,13 @@
 
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+from pisi.actionsapi import shelltools
+
+shelltools.export("HOME", get.workDIR())
 
 def setup():
+    autotools.autoreconf("-fiv")
     autotools.configure("--disable-gtk-doc")
 
 def build():
