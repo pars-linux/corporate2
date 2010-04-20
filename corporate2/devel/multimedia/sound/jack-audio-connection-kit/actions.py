f0dc46db12b8d07776b2f7212f8283374ffa3e0e
igungor <igungor@420bcd57-4a62-4fd6-832e-5ede16c90cc9>
2010-04-20 10:02:02 +0000

    Version bump and compile with ffado support

--- corporate2/devel/multimedia/sound/jack-audio-connection-kit/actions.py
+++ corporate2/devel/multimedia/sound/jack-audio-connection-kit/actions.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 #
-# Copyright 2005-2009 TUBITAK/UEKAE
+# Copyright 2005-2010 TUBITAK/UEKAE
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
@@ -10,12 +10,12 @@ from pisi.actionsapi import get
 
 def setup():
     autotools.autoreconf("-vif")
-
     # for using posix-shm see https://bugzilla.novell.com/show_bug.cgi?id=337972
     autotools.configure("--enable-html-docs \
                          --with-html-dir=/usr/share/doc/%s \
                          --enable-timestamps \
                          --enable-freebob \
+                         --enable-firewire \
                          --enable-resize \
                          --disable-optimize \
                          --disable-oss \
@@ -30,5 +30,4 @@ def build():
 def install():
     autotools.rawInstall("DESTDIR=%s" % get.installDIR())
 
-    pisitools.doman("man/*.1")
     pisitools.dodoc("AUTHORS", "COPYING*", "README", "TODO")
