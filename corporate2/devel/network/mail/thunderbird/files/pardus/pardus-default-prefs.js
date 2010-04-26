1dec53244ccba1e17d037072037a3e8cea8f0a9c
ozan.caglayan <ozan.caglayan@420bcd57-4a62-4fd6-832e-5ede16c90cc9>
2010-04-26 13:29:54 +0000

    Merge 3.0.4 from 2009

--- corporate2/devel/network/mail/thunderbird/files/pardus/pardus-default-prefs.js
+++ corporate2/devel/network/mail/thunderbird/files/pardus/pardus-default-prefs.js
@@ -1,6 +1,6 @@
-pref("network.protocol-handler.app.ftp","firefox");
-pref("network.protocol-handler.app.http","firefox");
-pref("network.protocol-handler.app.https","firefox");
+pref("network.protocol-handler.app.ftp", "LAUNCHER");
+pref("network.protocol-handler.app.http", "LAUNCHER");
+pref("network.protocol-handler.app.https", "LAUNCHER");
 pref("mail.content_disposition_type", 1);
 pref("mailnews.reply_header_type", 3);
 pref("mailnews.reply_header_authorwrote", "%s şöyle yazmıştı:");
@@ -9,13 +9,14 @@ pref("mailnews.reply_header_separator", " ");
 pref("mailnews.reply_header_colon", ":");
 pref("mailnews.default_sort_type", 2);
 pref("mailnews.default_sort_order", 22);
+pref("network.negotiate-auth.gsslib", "GSSAPI");
 
 pref("mail.compose.max_recycled_windows", 1);
 pref("browser.display.show_image_placeholders", false);
 pref("mailnews.customHeaders", "List-Id");
 
-pref("general.useragent.vendor", "Pardus");
-pref("general.useragent.vendorSub", "Corporate2");
+pref("general.useragent.vendor", "DISTRIB_ID");
+pref("general.useragent.vendorSub", "DISTRIB_RELEASE");
 
 pref("font.name.monospace.null", "DejaVu Sans Mono");
 pref("font.name.monospace.tr", "DejaVu Sans Mono");
@@ -45,3 +46,8 @@ pref("font.size.variable.x-unicode", 12);
 pref("font.size.variable.null", 12);
 pref("font.size.variable.x-western", 12);
 pref("font.size.variable.tr", 12);
+pref("print.print_edge_top", 14); // 1/100 of an inch
+pref("print.print_edge_left", 16); // 1/100 of an inch
+pref("print.print_edge_right", 16); // 1/100 of an inch
+pref("print.print_edge_bottom", 14); // 1/100 of an inch
+pref("intl.locale.matchOS", true);
