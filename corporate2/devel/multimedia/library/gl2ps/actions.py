a32f1e7ad2fd3425f9d81c0d7c79ddbf0f525053
igungor <igungor@420bcd57-4a62-4fd6-832e-5ede16c90cc9>
2010-05-04 09:16:04 +0000

    gl2ps is ready for C2

---
+++ corporate2/devel/multimedia/library/gl2ps/actions.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2009 TUBITAK/UEKAE
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/copyleft/gpl.txt.
+
+from pisi.actionsapi import cmaketools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+
+WorkDir = "%s-source" % get.srcDIR()
+
+def setup():
+    cmaketools.configure()
+
+def build():
+    cmaketools.make()
+
+def install():
+    cmaketools.rawInstall("DESTDIR=%s" % get.installDIR())
+
+    pisitools.remove("/usr/lib/libgl2ps.a")
+
+    pisitools.dodoc("README.txt", "TODO.txt")
