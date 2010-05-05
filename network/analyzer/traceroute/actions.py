9011ece29fad20ba865d482c101d1c9a96349a7e
M. Emre Atasever <m.emre.atas@gmail.com>
2010-05-05 08:41:06 +0000

    --use-quilt fixcd

--- network/analyzer/traceroute/actions.py
+++ network/analyzer/traceroute/actions.py
@@ -1,18 +1,16 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyright 2005-2008 TUBITAK/UEKAE
+# Copyright 2005-2010 TUBITAK/UEKAE
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
-from pisi.actionsapi import shelltools
 from pisi.actionsapi import get
 
 def build():
-    autotools.make('CC="%s" CFLAGS="%s"' % (get.CC(),get.CFLAGS()))
-    autotools.make()
+    autotools.make('CC="%s" CFLAGS="%s" SKIPDIRS="patches"' % (get.CC(),get.CFLAGS()))
 
 def install():
     pisitools.doexe("traceroute/traceroute", "/bin/")
