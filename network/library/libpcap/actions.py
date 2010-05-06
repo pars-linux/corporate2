0e8a3920ffb1c8ec6cd7a742fd8c9562c68ec08b
Onur Küçük <zaburt@users.noreply.github.com>
2010-05-06 11:55:18 +0000

    Minor version bump Depend on libnl for wireless support Fix pcap-config script including wrong whitespace, fixes bug #12824 Handle device changes better Prefer sysfs on linux

--- network/library/libpcap/actions.py
+++ network/library/libpcap/actions.py
@@ -1,31 +1,32 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyright 2005-2009 TUBITAK/UEKAE
+# Copyright 2005-2010 TUBITAK/UEKAE
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
+from pisi.actionsapi import shelltools
 from pisi.actionsapi import get
 
 def setup():
-    autotools.autoreconf("-fi")
+    shelltools.export("CFLAGS", "%s -fPIC" % get.CFLAGS())
+    autotools.autoreconf("-vfi")
     autotools.configure("--enable-ipv6 \
                          --enable-bluetooth")
 
 def build():
-    autotools.make("all shared")
+    autotools.make("all")
+    autotools.make("shared")
 
 def install():
-    # rawInstall borks somehow, don't know how to fix it
-    autotools.make('DESTDIR="%s" install install-shared' % get.installDIR())
+    autotools.rawInstall('DESTDIR="%s"' % get.installDIR())
 
     # No static libs
     pisitools.remove("/usr/lib/*.a")
 
-    pisitools.dolib_so("libpcap.so.%s" % get.srcVERSION())
-    pisitools.dosym("libpcap.so.1", "/usr/lib/libpcap.so")
-    pisitools.dosym("libpcap.so.1.0.0", "/usr/lib/libpcap.so.1")
+    # it is needed for ppd etc.
+    pisitools.insinto("/usr/include", "pcap-int.h")
 
     pisitools.dodoc("CHANGES", "CREDITS", "README*", "VERSION", "TODO")
