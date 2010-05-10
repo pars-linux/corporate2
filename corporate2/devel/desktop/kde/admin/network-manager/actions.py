d7a5606beb1320fd18c0972a50ff1b04745e0de4
ozan.caglayan <ozan.caglayan@420bcd57-4a62-4fd6-832e-5ede16c90cc9>
2010-05-10 13:00:32 +0000

    Make NM and applet GNOME NM aware

--- corporate2/devel/desktop/kde/admin/network-manager/actions.py
+++ corporate2/devel/desktop/kde/admin/network-manager/actions.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyright 2005-2009 TUBITAK/UEKAE
+# Copyright 2005-2010 TUBITAK/UEKAE
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 #
@@ -13,6 +13,7 @@ from pisi.actionsapi import get
 
 shelltools.export('HOME', get.workDIR())
 KeepSpecial=["libtool"]
+
 def install():
     pythonmodules.install()
     binpath = "%s/bin/network-manager" % get.kdeDIR()
