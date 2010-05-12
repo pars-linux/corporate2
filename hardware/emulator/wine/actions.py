3fe1bd47fa4d108a267797bd4fb6de07631e058e
Fatih Arslan <ftharsln@gmail.com>
2010-05-12 12:34:50 +0000

    wine is ready for c2

---
+++ hardware/emulator/wine/actions.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+
+def setup():
+    autotools.autoreconf("-vif")
+    autotools.configure("--without-capi \
+                         --with-curses \
+                         --without-esd \
+                         --with-opengl \
+                         --with-pulse")
+
+def build():
+    autotools.make()
+
+def install():
+    autotools.install("UPDATE_DESKTOP_DATABASE=/bin/true")
+
+    # Symlink for KDE3
+    pisitools.dosym("/etc/xdg/menus/applications-merged/wine.menu", "%s/etc/xdg/menus/applications-merged/wine.menu" % get.kdeDIR())
+
+    pisitools.dodoc("ANNOUNCE", "AUTHORS", "COPYING.LIB", "LICENSE*", "README", "documentation/README.*")
