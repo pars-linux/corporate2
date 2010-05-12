d94fae2ff5aed770a1600f2f59c7581ebe854aa6
Ozan Çağlayan <ozancaglayan@users.noreply.github.com>
2010-05-12 17:02:46 +0000

    Bump to 4.2.6_p1, refactor and split package. Write ntpd and ntpdate COMAR services. It's done.

--- server/ntp/actions.py
+++ server/ntp/actions.py
@@ -1,23 +1,23 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyright 2006-2008 TUBITAK/UEKAE
+# Copyright 2006-2010 TUBITAK/UEKAE
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
+from pisi.actionsapi import shelltools
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
-from pisi.actionsapi import shelltools
 from pisi.actionsapi import get
 
-WorkDir="ntp-4.2.4p7"
+WorkDir="ntp-%s" % get.srcVERSION().replace("_", "")
 
 def setup():
-    shelltools.export("CFLAGS","%s -pie -fPIE" % get.CFLAGS())
+    shelltools.export("CFLAGS", "%s -pie -fPIE -fno-strict-aliasing" % get.CFLAGS())
 
     # needed to link with avahi
-    shelltools.export("ac_cv_header_dns_sd_h","yes")
-    shelltools.export("ac_cv_lib_dns_sd_DNSServiceRegister","yes")
+    shelltools.export("ac_cv_header_dns_sd_h", "yes")
+    shelltools.export("ac_cv_lib_dns_sd_DNSServiceRegister", "yes")
 
     autotools.configure("--enable-all-clocks \
                          --enable-parse-clocks \
@@ -28,15 +28,27 @@ def setup():
 def build():
     autotools.make()
 
+    shelltools.cd("html")
+    shelltools.system("../scripts/html2man")
+    shelltools.system("../fix-man-pages")
+
 def install():
     autotools.rawInstall("DESTDIR=%s" % get.installDIR())
 
-    for sbin in ["sntp","ntpdc","ntpd","ntp-keygen","ntp-wait","ntpq",
-                 "ntptime","ntptrace","tickadj"]:
+    # Remove autotools installed man pages
+    pisitools.removeDir("/usr/share/man")
+
+    pisitools.doman("html/man/man5/*.5", "html/man/man8/*.8")
+
+
+    for sbin in ["sntp", "ntpdc", "ntpd", "ntp-keygen", "ntp-wait",
+                 "ntpq", "ntptime", "ntptrace", "tickadj", "ntpsnmpd", "ntpdate"]:
         pisitools.domove("/usr/bin/%s" % sbin, "/usr/sbin")
 
     pisitools.dodir("/var/lib/ntp")
-    pisitools.dodir("/var/lib/ntp/drift")
+
+    pisitools.removeDir("/usr/lib")
+    pisitools.removeDir("/usr/bin")
 
     pisitools.dohtml("html/*")
     pisitools.dodoc("ChangeLog", "NEWS", "README", "TODO", "WHERE-TO-START")
