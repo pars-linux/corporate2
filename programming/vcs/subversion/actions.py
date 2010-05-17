530128dccff0af5faf67ef284a39f3f35b174666
Ozan Çağlayan <ozancaglayan@users.noreply.github.com>
2010-05-17 13:58:35 +0000

    Merge from devel/programming/vcs/subversion/:
    rev. 88088, by sdalgic on 2010-03-06
    Mark packages as unmaintained
    
    rev. 88619, by ozan.caglayan on 2010-03-17
    Take over
    
    rev. 92134, by ozan.caglayan on 2010-05-17
    Bump to 1.6.11
    
    Version bump to 1.6.11,
    Add system-wide configuration file for svn:mime-type auto-setting.

--- programming/vcs/subversion/actions.py
+++ programming/vcs/subversion/actions.py
@@ -1,20 +1,20 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyright 2005-2009 TUBITAK/UEKAE
+# Copyright 2005-2010 TUBITAK/UEKAE
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
+from pisi.actionsapi import get
+from pisi.actionsapi import libtools
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import shelltools
 from pisi.actionsapi import perlmodules
-from pisi.actionsapi import get
 
 def setup():
     # Respect the user LDFLAGS
     shelltools.system("./autogen.sh")
-    #autotools.autoreconf("-fi")
 
     shelltools.export("EXTRA_LDFLAGS", get.LDFLAGS())
 
