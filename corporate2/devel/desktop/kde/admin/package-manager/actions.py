a4c1ba678363bb191471d58667090e0f2a46c71a
gokmen.goksel <gokmen.goksel@420bcd57-4a62-4fd6-832e-5ede16c90cc9>
2010-05-17 09:07:57 +0000

    * New Package Manager landed to Corporate \o/

--- corporate2/devel/desktop/kde/admin/package-manager/actions.py
+++ corporate2/devel/desktop/kde/admin/package-manager/actions.py
@@ -1,22 +1,12 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyright 2005-2009 TUBITAK/UEKAE
+# Copyright 2005-2010 TUBITAK/UEKAE
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
 from pisi.actionsapi import pythonmodules
-from pisi.actionsapi import pisitools
-from pisi.actionsapi import shelltools
-from pisi.actionsapi import get
-
-shelltools.export('HOME', get.workDIR())
-KeepSpecial=["libtool"]
 
 def install():
     pythonmodules.install()
 
-    pisitools.remove("%s/bin/package-manager" % get.kdeDIR())
-    pisitools.remove("%s/bin/pm-install" % get.kdeDIR())
-    pisitools.dosym("%s/share/apps/package-manager/package-manager.py" % get.kdeDIR(), "%s/bin/package-manager" % get.kdeDIR())
-    pisitools.dosym("%s/share/apps/package-manager/pm-install.py" % get.kdeDIR(), "%s/bin/pm-install" % get.kdeDIR())
