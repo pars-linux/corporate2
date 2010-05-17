fb59d1c6379d42c43358dccc349193d8bf9ad33c
ozan.caglayan <ozan.caglayan@420bcd57-4a62-4fd6-832e-5ede16c90cc9>
2010-05-17 13:17:06 +0000

    Merge from devel/x11/terminal/xterm/:
    rev. 90437, by fatih on 2010-04-23
    New stable release (257)

--- corporate2/devel/x11/terminal/xterm/actions.py
+++ corporate2/devel/x11/terminal/xterm/actions.py
@@ -1,18 +1,13 @@
-#!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyright 2009 TUBITAK/UEKAE
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/copyleft/gpl.txt.
 
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
-from pisi.actionsapi import shelltools
 from pisi.actionsapi import get
 
 def setup():
-    shelltools.export("CC", get.CC())
-
     autotools.configure(" \
              --disable-full-tgetent \
              --with-app-defaults=/usr/share/X11/app-defaults \
