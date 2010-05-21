3c25ea07e152926c7080f59054008112da2f71a0
ozan.caglayan <ozan.caglayan@420bcd57-4a62-4fd6-832e-5ede16c90cc9>
2010-05-21 10:34:50 +0000

    Merge from devel/hardware/disk/ntfs_3g/:
    rev. 88088, by sdalgic on 2010-03-06
    Mark packages as unmaintained
    
    rev. 88619, by ozan.caglayan on 2010-03-17
    Take over
    
    rev. 92237, by ozan.caglayan on 2010-05-18
    Version bump, enable library

--- corporate2/devel/hardware/disk/ntfs_3g/actions.py
+++ corporate2/devel/hardware/disk/ntfs_3g/actions.py
@@ -5,6 +5,7 @@
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
+from pisi.actionsapi import shelltools
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
@@ -12,10 +13,12 @@ from pisi.actionsapi import get
 WorkDir="ntfs-3g-%s" % get.srcVERSION()[4:]
 
 def setup():
-    autotools.configure("--prefix=/ \
-                         --exec-prefix=/ \
+    shelltools.export("CFLAGS", "%s -D_FILE_OFFSET_BITS=64" % get.CFLAGS())
+    autotools.configure("--exec-prefix=/ \
+                         --bindir=/bin \
+                         --sbindir=/sbin \
+                         --libdir=/usr/lib \
                          --disable-static \
-                         --disable-library \
                          --disable-ldconfig \
                          --with-fuse=external \
                          --docdir=/usr/share/doc/%s" % get.srcNAME())
@@ -25,5 +28,7 @@ def build():
 
 def install():
     autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+
+    pisitools.insinto("/usr/lib/pkgconfig", "libntfs-3g/*.pc")
 
     pisitools.dodoc("AUTHORS", "ChangeLog", "COPYING", "CREDITS", "NEWS", "README")
