67edb1d156a733aebd7ea99fedf8ac09abcb484f
Onur Küçük <zaburt@users.noreply.github.com>
2010-05-28 21:42:19 +0000

    update snapshot to 23351

--- multimedia/video/ffmpeg/actions.py
+++ multimedia/video/ffmpeg/actions.py
@@ -11,7 +11,7 @@ from pisi.actionsapi import shelltools
 from pisi.actionsapi import get
 
 WorkDir = "ffmpeg"
-version = "22144"
+version = "23351"
 minimumcpu = "" if get.ARCH() == "x86_64" else "--cpu=i686"
 
 
