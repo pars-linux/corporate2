fb7639d8029227b786a250c17d2a1264cf37876f
ozan.caglayan <ozan.caglayan@420bcd57-4a62-4fd6-832e-5ede16c90cc9>
2010-05-31 12:13:56 +0000

    Remove unpackaged directory

--- corporate2/devel/desktop/kde/addon/kaptan/actions.py
+++ corporate2/devel/desktop/kde/addon/kaptan/actions.py
@@ -21,4 +21,4 @@ def install():
     pisitools.remove("%s/bin/kaptan" % get.kdeDIR())
     pisitools.dosym("%s/share/apps/kaptan/kaptan.py" % get.kdeDIR(), "%s/bin/kaptan" % get.kdeDIR())
 
-    pisitools.removeDir("/usr/share/doc/")
+    pisitools.removeDir("/usr/share")
