9c69d41f4be14926ad11c379883d9631147b6df5
Serdar Dalgıç <sd@serdardalgic.org>
2010-05-31 11:03:31 +0000

    Merge from devel/programming/language/perl/perl-ExtUtils-CBuilder:
    rev. 93341, by sdalgic on 2010-05-31
    Remove man pages as they conflict with perl-doc package.

--- programming/language/perl/perl-ExtUtils-CBuilder/actions.py
+++ programming/language/perl/perl-ExtUtils-CBuilder/actions.py
@@ -23,3 +23,6 @@ def check():
 def install():
     perlmodules.install()
     pisitools.removeDir("/usr/lib/perl5/vendor_perl/%s/%s-linux-thread-multi/" % (get.curPERL(), get.ARCH()))
+
+    #conflicts with perl-doc package
+    pisitools.removeDir("/usr/share")
