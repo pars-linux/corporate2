90b527ac65d4a297ed881521882a649c3637d13f
Onur Küçük <zaburt@users.noreply.github.com>
2010-06-01 12:50:25 +0000

    Fixed buffer size dimension in UDFGetPVD

--- multimedia/library/libdvdread/actions.py
+++ multimedia/library/libdvdread/actions.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyright 2005-2009 TUBITAK/UEKAE
+# Copyright 2005-2010 TUBITAK/UEKAE
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 #
@@ -17,7 +17,7 @@ WorkDir = "libdvdread"
 
 def setup():
     libtools.libtoolize("--force --install")
-    autotools.autoreconf("-fi")
+    autotools.autoreconf("-vfi")
 
     autotools.configure("--with-libdvdcss=/usr \
                          --disable-static")
