d9cdec5b9ad0a29fea9a2801973bc2ebfbc1a846
ozan.caglayan <ozan.caglayan@420bcd57-4a62-4fd6-832e-5ede16c90cc9>
2010-06-07 06:33:45 +0000

    Merge from devel/hardware/optical/cdrtools/:
    rev. 93579, by ozan.caglayan on 2010-06-03
    Bump to 3.00

--- corporate2/devel/hardware/optical/cdrtools/actions.py
+++ corporate2/devel/hardware/optical/cdrtools/actions.py
@@ -1,15 +1,13 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyright 2005-2009 TUBITAK/UEKAE
+# Copyright 2005-2010 TUBITAK/UEKAE
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
-
-WorkDir="cdrtools-2.01.01"
 
 def build():
     #parallel compilation of cdda2wav exetuable is broken in this release
