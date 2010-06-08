452347efe19c0130ef2f29b12cd6e2b0b62f0f67
Ozan Çağlayan <ozancaglayan@users.noreply.github.com>
2010-06-08 20:46:09 +0000

    Bump

--- programming/library/gobject-introspection/actions.py
+++ programming/library/gobject-introspection/actions.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyright 2008-2009 TUBITAK/UEKAE
+# Copyright 2008-2010 TUBITAK/UEKAE
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
@@ -13,19 +13,15 @@ from pisi.actionsapi import get
 shelltools.export("HOME", get.workDIR())
 
 def setup():
-    pisitools.dosed("giscanner/Makefile.in", "py_compile = .*", "py_compile = /bin/true")
+    #pisitools.dosed("giscanner/Makefile.in", "py_compile = .*", "py_compile = /bin/true")
     autotools.autoreconf("-fi")
     autotools.configure("--disable-static")
 
 def build():
-    autotools.make("-j1 V=1")
+    autotools.make("V=1")
 
 def install():
     autotools.rawInstall("DESTDIR=%s" % get.installDIR())
-
-    pisitools.dodir("/usr/lib/%s/site-packages" % get.curPYTHON())
-    pisitools.domove("/usr/lib/gobject-introspection/giscanner", "/usr/lib/%s/site-packages" % get.curPYTHON())
-    pisitools.removeDir("/usr/lib/gobject-introspection")
 
     pisitools.dodoc("AUTHORS", "COPYING*", "NEWS", "README")
 
