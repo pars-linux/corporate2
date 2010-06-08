fc3ad157f796c71f39268b5e6f64efcd16163adb
Necdet Yucel <necdetyucel@gmail.com>
2010-06-08 07:27:12 +0000

    Version bump, fix for x86_64

--- util/archive/rar/actions.py
+++ util/archive/rar/actions.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyright 2009 TUBITAK/UEKAE
+# Copyright 2005-2010 TUBITAK/UEKAE
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 
-WorkDir = get.srcNAME()
+WorkDir = "%s/%s" % (get.ARCH(), get.srcNAME())
 NoStrip = ["/"]
 
 def install():
