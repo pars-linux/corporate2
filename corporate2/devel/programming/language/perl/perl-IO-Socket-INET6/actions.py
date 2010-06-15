7b4d7aaca282437a30c7b62d136c28a1f3c3916f
sdalgic <sdalgic@420bcd57-4a62-4fd6-832e-5ede16c90cc9>
2010-06-15 11:19:52 +0000

    Merge from devel/programming/language/perl/perl-IO-Socket-INET6:
    rev. 94226, by sdalgic on 2010-06-15
    * Version bump
    * add perl-Test-Pod + perl-Test-Pod-Coverage packages for the build time.
    * remove useless empty directory.

--- corporate2/devel/programming/language/perl/perl-IO-Socket-INET6/actions.py
+++ corporate2/devel/programming/language/perl/perl-IO-Socket-INET6/actions.py
@@ -6,6 +6,7 @@
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
 from pisi.actionsapi import perlmodules
+from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 
 WorkDir = "%s-%s" % (get.srcNAME()[5:], get.srcVERSION())
@@ -21,3 +22,5 @@ def check():
 
 def install():
     perlmodules.install()
+
+    pisitools.removeDir("/usr/lib/perl5/vendor_perl/%s/%s-linux-thread-multi/" % (get.curPERL(), get.ARCH()))
