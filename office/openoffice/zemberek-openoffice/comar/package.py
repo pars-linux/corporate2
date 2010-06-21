f6522358bc23fee8f4cfcdf5fba9d2bcd7863c4e
Gökçen Eraslan <gokcen.eraslan@gmail.com>
2010-06-21 11:40:56 +0000

    Merge from devel/office/openoffice/zemberek-openoffice/:
    rev. 94599, by gokcen.eraslan on 2010-06-21
    Fix remove part of postinstall and recompile zemberek extension for OO.org 3.2.

--- office/openoffice/zemberek-openoffice/comar/package.py
+++ office/openoffice/zemberek-openoffice/comar/package.py
@@ -4,13 +4,21 @@ import os
 import glob
 
 unopkg = "/opt/OpenOffice.org/bin/unopkg"
+extPath = glob.glob("/usr/share/zemberek/zemberek*.oxt")[0]
+extID = "net.zemberek.ooo.spellchecker"
 
 def postInstall(fromVersion, fromRelease, toVersion, toRelease):
     os.environ["JAVA_HOME"] = "/opt/sun-jre"
-    extPath = glob.glob("/usr/share/zemberek/zemberek*.oxt")[0]
-    os.system("%s add --shared --force %s" % (unopkg, extPath))
+
+    ret = os.system("%s add --shared --force %s" % (unopkg, extPath))
+
+    if ret != 0:
+        raise Exception("Could not install OO.org extension: %s" % extID)
 
 def preRemove():
     os.environ["JAVA_HOME"] = "/opt/sun-jre"
-    extPath = glob.glob("/usr/share/zemberek/zemberek*.oxt")[0]
-    os.system("%s remove --shared %s" % (unopkg, os.path.basename(extPath)))
+
+    ret = os.system("%s remove --shared %s" % (unopkg, extID))
+
+    if ret != 0:
+        raise Exception("Could not remove OO.org extension: %s" % extID)
