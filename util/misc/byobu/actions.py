5a3f98e8478774e6f1970471903545d8c0c13086
fethican <fethican@420bcd57-4a62-4fd6-832e-5ede16c90cc9>
2010-06-21 22:35:34 +0000

    Merge from devel/util/misc/byobu/:
    rev. 94615, by fethican on 2010-06-21
    version bump.

--- util/misc/byobu/actions.py
+++ util/misc/byobu/actions.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyright 2009 TUBITAK/UEKAE
+# Copyright 2010 TUBITAK/UEKAE
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
@@ -14,7 +14,7 @@ def install():
     pisitools.insinto("/", "usr")
 
     for pofile in shelltools.ls("po/*.po"):
-        lang = shelltools.baseName(pofile)[:3]
+        lang = shelltools.baseName(pofile)[:-3]
         pisitools.domo(pofile, lang, "byobu.mo")
 
     pisitools.dodoc("AUTHORS", "README", "COPYING")
