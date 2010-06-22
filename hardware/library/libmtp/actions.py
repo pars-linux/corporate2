a8f49ac92e1ebe57490fe2fe9a1e3265388beeaa
Ozan Çağlayan <ozancaglayan@users.noreply.github.com>
2010-06-22 12:31:24 +0000

    Bump to 1.0.3

--- hardware/library/libmtp/actions.py
+++ hardware/library/libmtp/actions.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyright 2006-2009 TUBITAK/UEKAE
+# Copyright 2006-2010 TUBITAK/UEKAE
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
@@ -10,7 +10,8 @@ from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
 
 def setup():
-    autotools.configure("--disable-static")
+    autotools.configure("--disable-static \
+                         --program-prefix=mtp-")
 
 def build():
     autotools.make()
@@ -18,13 +19,13 @@ def build():
 def install():
     autotools.install()
 
-    #install HAL file for portable audio players
+    # Install HAL file for portable audio players
     pisitools.insinto("/usr/share/hal/fdi/information/10freedesktop", "libmtp.fdi", "10-usb-music-players-libmtp.fdi")
 
     # Regenerate udev rules without MODE= property
     shelltools.system('examples/hotplug -a\'SYMLINK+="libmtp-%k", ENV{ACL_MANAGE}="1"\' > libmtp.rules')
 
-    #install UDEV rules
+    # Install UDEV rules
     pisitools.insinto("/lib/udev/rules.d/", "libmtp.rules", "60-mtp.rules")
 
     pisitools.removeDir("/usr/share/doc/libmtp-*")
