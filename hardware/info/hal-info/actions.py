517dcc25d22c29560d0b298a25647d2964bcf793
Ozan Çağlayan <ozancaglayan@users.noreply.github.com>
2010-06-25 07:47:06 +0000

    Merge from devel/hardware/info/hal-info/:
    rev. 94691, by ozan.caglayan on 2010-06-23
    Version bump

--- hardware/info/hal-info/actions.py
+++ hardware/info/hal-info/actions.py
@@ -1,16 +1,13 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyright 2005-2009 TUBITAK/UEKAE
+# Copyright 2005-2010 TUBITAK/UEKAE
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
-from pisi.actionsapi import shelltools
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
-
-WorkDir = "hal-info-20090716"
 
 def setup():
     autotools.configure("--enable-killswitch-dell-wlan=no \
