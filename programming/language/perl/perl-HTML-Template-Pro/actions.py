4edbed31bf41ed079a65349496734ef585afeec9
Serdar Dalgıç <sd@serdardalgic.org>
2010-06-28 11:25:08 +0000

    Merge from devel/programming/language/perl/perl-HTML-Template-Pro:
    rev. 95016, by sdalgic on 2010-06-28
    Version bump: - bugfix release: segfault for string operetions with undefined variables.

--- programming/language/perl/perl-HTML-Template-Pro/actions.py
+++ programming/language/perl/perl-HTML-Template-Pro/actions.py
@@ -11,7 +11,7 @@ from pisi.actionsapi import shelltools
 from pisi.actionsapi import get
 
 #WorkDir = "%s-%s" % (get.srcNAME()[5:], get.srcVERSION())
-WorkDir = "%s-0.9501" % get.srcNAME()[5:]
+WorkDir = "%s-0.9502" % get.srcNAME()[5:]
 
 def setup():
     perlmodules.configure()
