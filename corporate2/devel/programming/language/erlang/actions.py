fd15b2c2b63bc70ff8700db734f4d13866429680
ozan.caglayan <ozan.caglayan@420bcd57-4a62-4fd6-832e-5ede16c90cc9>
2010-07-06 13:47:51 +0000

    I think it's ready.

--- corporate2/devel/programming/language/erlang/actions.py
+++ corporate2/devel/programming/language/erlang/actions.py
@@ -31,13 +31,16 @@ def setup():
                          --with-ssl")
 
 def build():
-    autotools.make()
+    autotools.make("-j1")
 
     # Building documentation needs escript from erlang package
     shelltools.export("PATH", "%s:%s/bin" % (os.environ.get("PATH"), get.curDIR()))
-    autotools.make("docs")
+    autotools.make("-j1 docs")
 
 def install():
+    # Build dummy PDFs to break fop dependency
+    shelltools.system("./genpdf")
+
     autotools.rawInstall("DESTDIR=%s" % get.installDIR())
     autotools.rawInstall("DESTDIR=%s install-docs" % get.installDIR())
 
@@ -48,8 +51,21 @@ def install():
     for f in ("lib/observer-*/priv/bin/*.bat",
               "lib/os_mon-*/ebin/nteventlog.beam",
               "lib/ssl-*/examples/certs/etc/otpCA/*.txt.old",
-              "lib/webtool-*/priv/bin/start_webtool.bat"):
+              "lib/webtool-*/priv/bin/start_webtool.bat",
+              "lib/*-*/info",
+              "erts-*/info",
+              "man/man3/erl_set_memory_block.*",
+              "man/man3/nteventlog.*",
+              "man/man3/win32reg.*",
+              "man/man1/erlsrv.*",
+              "man/man1/werl.*",
+              "PR.template",
+              ):
         pisitools.remove("/usr/lib/erlang/%s" % f)
+
+    # Move README and COPYRIGHT
+    pisitools.domove("/usr/lib/erlang/README", "/usr/share/doc/%s" % get.srcNAME())
+    pisitools.domove("/usr/lib/erlang/COPYRIGHT", "/usr/share/doc/%s" % get.srcNAME())
 
     for d in ("lib/*/priv/obj",
               "lib/*/c_src",
@@ -57,4 +73,22 @@ def install():
               "erts-5.7.5/doc",
               "erts-5.7.5/man",
               "misc"):
-        pisitools.remove("/usr/lib/erlang/%s" % d)
+        pisitools.removeDir("/usr/lib/erlang/%s" % d)
+
+    # Relocate doc files
+    pisitools.domove("/usr/lib/erlang/doc/*", "/usr/share/doc/%s" % get.srcNAME())
+    pisitools.removeDir("/usr/lib/erlang/doc")
+
+    for module in shelltools.ls("%s/usr/lib/erlang/erts-*/doc" % get.installDIR()):
+        pisitools.domove(os.path.join(module.split(get.installDIR())[-1], "*"),
+                         "/usr/share/doc/%s/%s" % (get.srcNAME(), module.split("/")[-2]))
+
+    for lib in shelltools.ls("%s/usr/lib/erlang/lib/*/doc" % get.installDIR()):
+        path = os.path.join(lib.split(get.installDIR())[-1])
+        pisitools.domove("%s/*" % path, "/usr/share/doc/%s/%s" % (get.srcNAME(), lib.split("/")[-2]))
+        if len(shelltools.ls(path)) == 0:
+            pisitools.removeDir(path)
+
+    # Remove dummy installed pdf files
+    shelltools.system("./genpdf clean %s/usr/share/doc/%s" % (get.installDIR(), get.srcNAME()))
+
