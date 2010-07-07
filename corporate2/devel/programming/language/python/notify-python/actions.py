f8cb6c292dfdb8190c195ffa3582934bbee862ff
fatih <fatih@420bcd57-4a62-4fd6-832e-5ede16c90cc9>
2010-07-07 08:22:51 +0000

    Remove pynotify.c to be regenerated from pynotify.defs
    Merged from 2009/devel.

--- corporate2/devel/programming/language/python/notify-python/actions.py
+++ corporate2/devel/programming/language/python/notify-python/actions.py
@@ -1,15 +1,18 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyright 2005-2009 TUBITAK/UEKAE
+# Copyright 2005-2010 TUBITAK/UEKAE
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
+from pisi.actionsapi import shelltools
 from pisi.actionsapi import autotools
-from pisi.actionsapi import pythonmodules
 from pisi.actionsapi import get
 
 def setup():
+    # Remove .c file to be regenerated from .defs file
+    shelltools.unlink("src/pynotify.c")
+
     autotools.autoreconf("-fi")
     autotools.configure()
 
