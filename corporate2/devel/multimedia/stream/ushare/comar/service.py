07168c3cc281916856559fff5a4e25a8a47193a6
igungor <igungor@420bcd57-4a62-4fd6-832e-5ede16c90cc9>
2010-07-12 11:48:55 +0000

    Merge: Check if the shared directory is a valid one or not

--- corporate2/devel/multimedia/stream/ushare/comar/service.py
+++ corporate2/devel/multimedia/stream/ushare/comar/service.py
@@ -9,8 +9,22 @@ serviceDefault = "off"
 pidfile = "/var/run/ushare.pid"
 command = "/usr/bin/ushare"
 
+def check_ushare_dir():
+    conf = open("/etc/ushare.conf")
+
+    for line in conf.readlines():
+        line = line.strip()
+        if line.startswith("USHARE_DIR"):
+            ushare_dir = line.split("=")[1]
+
+    if not os.path.exists(ushare_dir):
+        fail('uShare directory "%s" doesn\'t exist, please edit your /etc/ushare.conf and set a valid directory.' % ushare_dir)
+
+    conf.close()
+
 @synchronized
 def start():
+    check_ushare_dir()
     startService(command=command,
                  args="--cfg=/etc/ushare.conf -d",
                  pidfile=pidfile,
