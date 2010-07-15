b5f74ef4735995a76450992f3922099dd4507dfc
Bahadır Kandemir <kandemir@gmail.com>
2010-07-15 06:47:00 +0000

    New release, also fixed service script

--- server/openldap/comar/slapd.py
+++ server/openldap/comar/slapd.py
@@ -1,21 +1,17 @@
+from comar.service import *
+
 serviceType = "server"
 serviceDesc = _({"en": "OpenLDAP Server",
                  "tr": "OpenLDAP Sunucusu"})
 serviceConf = "slapd.conf"
-
-from comar.service import *
 
 def start():
     import os
     os.environ["LC_ALL"] = "C"
     os.environ["LANG"] = "C"
 
-    args = ["-u", "ldap", "-g", "ldap", "-h"]
-    args.extend(config.get("SERVE", "ldap://").split())
-    args.extend(config.get("OPTS", "").split())
-
     startService(command="/usr/libexec/slapd",
-                 args=args,
+                 args="-u ldap -g ldap %s" % config.get("OPTS", ""),
                  pidfile="/var/run/openldap/slapd.pid",
                  donotify=True)
 
