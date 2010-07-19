6f800c2114141a9776d7f73ac7b6290a318c8442
Onur Küçük <zaburt@users.noreply.github.com>
2010-07-19 08:22:54 +0000

    fix version

--- multimedia/video/ffmpeg/actions.py
+++ multimedia/video/ffmpeg/actions.py
@@ -11,7 +11,7 @@ from pisi.actionsapi import shelltools
 from pisi.actionsapi import get
 
 WorkDir = "ffmpeg"
-version = "24252"
+version = "24320"
 minimumcpu = "" if get.ARCH() == "x86_64" else "--cpu=i686"
 
 
