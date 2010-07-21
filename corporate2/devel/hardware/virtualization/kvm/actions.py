337ed1e20a86785bd85564c3c85ebf4195c302e4
ozan.caglayan <ozan.caglayan@420bcd57-4a62-4fd6-832e-5ede16c90cc9>
2010-07-21 11:22:10 +0000

    Merge from 2009/devel/hardware/virtualization/kvm/:
    rev. 95900, by ozan.caglayan on 2010-07-15
    Version bump
    
    rev. 95913, by ozan.caglayan on 2010-07-15
    Install kvm tools
    
    rev. 95929, by ozan.caglayan on 2010-07-15
    Bump to 0.12.4
    
    - Install kvm tools.
    - Version bump, add fedora patches,
    - Install kvm tools,
    - Add KSM support.
    
    rev. 96192, by ozan.caglayan on 2010-07-21
    Disable service

--- corporate2/devel/hardware/virtualization/kvm/actions.py
+++ corporate2/devel/hardware/virtualization/kvm/actions.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyright 2007-2009 TUBITAK/UEKAE
+# Copyright 2007-2010 TUBITAK/UEKAE
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
@@ -13,7 +13,6 @@ from pisi.actionsapi import get
 NoStrip=["/usr/share/kvm"]
 WorkDir="qemu-kvm-%s" % get.srcVERSION()
 
-soundDrivers = "alsa pa sdl oss"
 cflags = get.CFLAGS().replace("-fpie", "").replace("-fstack-protector", "")
 
 targetList = "i386-softmmu i386-linux-user"
@@ -24,22 +23,23 @@ def setup():
 
     shelltools.export("CFLAGS", cflags)
     autotools.rawConfigure('--prefix=/usr \
-                            --disable-werror \
-                            --audio-drv-list="%s" \
+                            --audio-drv-list="alsa pa sdl oss" \
                             --cc="%s" \
                             --host-cc="%s" \
                             --enable-system \
                             --enable-linux-user \
+                            --disable-strip \
                             --disable-bsd-user \
-                            --disable-darwin-user' % (soundDrivers, get.CC(), get.CC()))
-                            # --audio-card-list="ac97 es1370 sb16 cs4231a adlib gus" \
-                            # --target-list="%s"' % " ".join(target_list))
-                            # --disable-bluez \
-                            # --kerneldir="/lib/modules/%s/build" \
+                            --disable-darwin-user' % (get.CC(), get.CC()))
+
+    shelltools.cd("kvm/user")
+    autotools.rawConfigure("--prefix=/usr \
+                            --kerneldir=../../kernel")
 
 
 def build():
     autotools.make("V=1")
+    autotools.make("-C kvm/user kvmtrace")
 
 def install():
     autotools.rawInstall("DESTDIR=%s" % get.installDIR())
@@ -47,9 +47,15 @@ def install():
     pisitools.domove("/usr/bin/qemu-system-x86_64", "/usr/bin/", "qemu-kvm")
     pisitools.domove("/usr/share/man/man1/qemu.1", "/usr/share/man/man1/", "qemu-kvm.1")
 
+    # Install kvm-tools
+    pisitools.dobin("kvm/user/kvmtrace")
+    pisitools.dobin("kvm/user/kvmtrace_format")
+    pisitools.dobin("kvm/kvm_stat")
+
     # Use the one qemu provides
     pisitools.remove("/usr/bin/qemu-img")
     pisitools.remove("/usr/share/man/man1/qemu-img.1")
     pisitools.remove("/usr/bin/qemu-nbd")
+
     pisitools.removeDir("/usr/share/man/man8/")
     pisitools.removeDir("/usr/share/doc")
