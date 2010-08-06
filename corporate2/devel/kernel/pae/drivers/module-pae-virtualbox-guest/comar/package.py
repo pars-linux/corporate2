d75af4e68f02ccbf9e22d274bd733cc2b6290a75
fatih <fatih@420bcd57-4a62-4fd6-832e-5ede16c90cc9>
2010-08-06 17:44:52 +0000

    New stable release (3.2.8)

--- corporate2/devel/kernel/pae/drivers/module-pae-virtualbox-guest/comar/package.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-
-def postRemove():
-    import os
-
-    old_module_path = "/lib/modules/2.6.32.14-139-pae/extra/vboxvfs.ko"
-    if os.path.exists(old_module_path):
-        os.remove(old_module_path)
