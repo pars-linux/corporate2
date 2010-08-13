815433bd5ca6c325836de3f51a9947e83caf5a72
Ozan Çağlayan <ozancaglayan@users.noreply.github.com>
2010-08-13 12:24:45 +0000

    Merge from default kernel

--- kernel/pae/kernel-pae/files/scripts/bump-driverdeps.py
+++ kernel/pae/kernel-pae/files/scripts/bump-driverdeps.py
@@ -9,17 +9,15 @@ import pisi
 oldwd = os.getcwd()
 repo_root = "/root/2009/"
 
-kpspec = pisi.specfile.SpecFile('pspec.xml')
-kver = kpspec.history[0].version
 
 if len(sys.argv) == 3:
     repo_root = sys.argv[1]
     krel = sys.argv[2]
 else:
-    krel = kpspec.history[0].release
+    krel = pisi.specfile.SpecFile('pspec.xml').history[0].release
 
 for d in ["devel"]:
-    os.chdir(repo_root+d)
+    os.chdir(os.path.join(repo_root, d))
 
     packages = os.popen("grep --exclude=pisi-index* --exclude-dir=.svn -r '<Dependency release=\".*\">kernel.*' * | gawk -F: '{ print $1 }'").read().strip().split()
 
