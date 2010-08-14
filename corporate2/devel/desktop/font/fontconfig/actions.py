6daa7a600d808ad0a7354f857fa87a52c1b6f173
fatih <fatih@420bcd57-4a62-4fd6-832e-5ede16c90cc9>
2010-08-14 20:28:52 +0000

    Add Fedora patch to sleep less in fc-cache

--- corporate2/devel/desktop/font/fontconfig/actions.py
+++ corporate2/devel/desktop/font/fontconfig/actions.py
@@ -1,15 +1,17 @@
-#!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyright 2005-2008 TUBITAK/UEKAE
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
+from pisi.actionsapi import shelltools
 from pisi.actionsapi import get
 
 def setup():
+    # Do not rebuild docs
+    shelltools.export("HASDOCBOOK", "no")
+
     autotools.autoreconf("-vif")
     autotools.configure("--disable-static \
                          --disable-docs \
