a675a41bf2f7d8c119f7fef3d88581584cd381ce
fatih <fatih@420bcd57-4a62-4fd6-832e-5ede16c90cc9>
2010-08-14 12:24:02 +0000

    New stable release (1.0.6)

--- corporate2/devel/x11/library/libXvMC/actions.py
+++ corporate2/devel/x11/library/libXvMC/actions.py
@@ -1,10 +1,10 @@
-#!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
 from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
 
 def setup():
     autotools.autoreconf("-vif")
@@ -15,3 +15,5 @@ def build():
 
 def install():
     autotools.install()
+
+    pisitools.dodoc("ChangeLog", "COPYING", "README")
