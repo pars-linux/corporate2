1f266a4475de4af4a891455d4aca25f95ccb0d27
Fatih Arslan <ftharsln@gmail.com>
2010-08-16 20:49:40 +0000

    New stable release (1.4.2)

--- x11/library/libXfont/actions.py
+++ x11/library/libXfont/actions.py
@@ -4,6 +4,7 @@
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
 from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 
 def setup():
@@ -15,3 +16,5 @@ def build():
 
 def install():
     autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+
+    pisitools.dodoc("AUTHORS", "ChangeLog", "COPYING", "README")
