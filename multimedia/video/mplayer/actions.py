190cce04c3ddfa9764936f251e50f4f1fe0ca44a
Onur Küçük <zaburt@users.noreply.github.com>
2010-08-19 19:41:59 +0000

    Bump snapshot to 31982, ffmpeg 24839 Fix segfaults with flv files Fix cache2 segfault, fixes seeking in some filetypes like flv Revert flash completion as it breaks empty space completion Fix versioning

--- multimedia/video/mplayer/actions.py
+++ multimedia/video/mplayer/actions.py
@@ -14,8 +14,9 @@ from pisi.actionsapi import get
 import os
 
 WorkDir = "mplayer"
-mp_version = "31750"
-ff_version = "24320"
+gcc_version = "4.3.5"
+mp_version = "31982"
+ff_version = "24839"
 
 dllloader = "" if get.ARCH() == "x86_64" else "--enable-win32dll \
                                                --codecsdir=/usr/lib/%(esdir)s" % {"esdir": "essential"}
@@ -31,13 +32,10 @@ def setup():
     fixPermissions("DOCS")
     #shelltools.export("LINGUAS", "tr")
 
-#define VERSION "SVN-r31743-4.3.3"
-#define MP_TITLE "%s "VERSION" (C) 2000-2010 MPlayer Team\n"
-
     # to keep the source tarball small and avoid sandbox violation from subversion we remove .svn folders
     shelltools.unlink("version.sh")
-    shelltools.echo("version.sh", '#!/bin/bash\necho "#define VERSION \\\"dev-SVN-r%s\\\"" > version.h' % mp_version)
-    shelltools.echo("version.sh", 'echo "#define MP_TITLE \\\"MPlayer dev-SVN-r%s (C) 2000-2010 MPlayer Team\\\" " >> version.h' % mp_version)
+    shelltools.echo("version.sh", '#!/bin/bash\necho "#define VERSION \\\"SVN-r%s-%s\\\"" > version.h' % (mp_version, gcc_version))
+    shelltools.echo("version.sh", 'echo "#define MP_TITLE \\\"%s \\\"VERSION\\\" (C) 2000-2010 MPlayer Team\\n\\\"" >> version.h')
     shelltools.chmod("version.sh", 0755)
 
     shelltools.export("CFLAGS", "%s -O3 -ffast-math -fomit-frame-pointer" % get.CFLAGS())
