11050d86262a92da77f425d4b18693e6404a41cc
Onur Küçük <zaburt@users.noreply.github.com>
2010-08-19 17:30:46 +0000

    Update snapshot
    fix playing of some wma files

--- multimedia/video/ffmpeg/actions.py
+++ multimedia/video/ffmpeg/actions.py
@@ -11,7 +11,7 @@ from pisi.actionsapi import shelltools
 from pisi.actionsapi import get
 
 WorkDir = "ffmpeg"
-version = "24320"
+version = "24839"
 minimumcpu = "" if get.ARCH() == "x86_64" else "--cpu=i686"
 
 
