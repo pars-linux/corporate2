40b5f87cb798127a93bb27f45fee1637d6c46035
H. İbrahim Güngör <igungor@users.noreply.github.com>
2010-08-19 13:28:18 +0000

    Switch to Jack2, enable RT mode by adding jackuser group and increasing the rtprio of members of jackuser group.

--- multimedia/sound/jack-audio-connection-kit/actions.py
+++ multimedia/sound/jack-audio-connection-kit/actions.py
@@ -1,33 +1,33 @@
+#!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyright 2005-2010 TUBITAK/UEKAE
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
-from pisi.actionsapi import autotools
+from pisi.actionsapi import shelltools
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
+import os
+
+WorkDir = "jack-%s" % get.srcVERSION()
+del(os.environ["JOBS"])
 
 def setup():
-    autotools.autoreconf("-vif")
-    # for using posix-shm see https://bugzilla.novell.com/show_bug.cgi?id=337972
-    autotools.configure("--enable-html-docs \
-                         --with-html-dir=/usr/share/doc/%s \
-                         --enable-timestamps \
-                         --enable-freebob \
-                         --enable-firewire \
-                         --enable-resize \
-                         --disable-optimize \
-                         --disable-oss \
-                         --disable-portaudio \
-                         --disable-ensure-mlock \
-                         --disable-dependency-tracking \
-                         --with-default-tmpdir=/dev/shm" % get.srcNAME())
+    shelltools.system("./waf configure \
+                        --prefix=/usr \
+                        --doxygen \
+                        --dbus \
+                        --classic")
 
 def build():
-    autotools.make()
+    shelltools.system("./waf build -v")
 
 def install():
-    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+    shelltools.system("./waf --destdir=%s install" % get.installDIR())
 
-    pisitools.dodoc("AUTHORS", "COPYING*", "README", "TODO")
+    # be compatible with the former jackaudio
+    pisitools.rename("/usr/bin/jack_rec", "jackrec")
+
+    shelltools.chmod("%s/usr/lib/jack/*.so*" % get.installDIR(), 0755)
+
+    pisitools.dodoc("ChangeLog", "README*", "TODO")
