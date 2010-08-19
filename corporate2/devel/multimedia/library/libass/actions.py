e7b98bc9b2eb82ba59e5f9e2d19703ffba7336ff
onur <onur@420bcd57-4a62-4fd6-832e-5ede16c90cc9>
2010-08-19 18:33:31 +0000

    Version bump Fix serious memory leaks Reduce frame/drawing initialization overhead Basic (incorrect, but working) support for @font vertical text layout Fix multiple faces per font attachment charmap selection fixes Add ass_flush_events API function Improve fullname font matching Better PAR correction if text transforms are used Calculate drawing bounding box like VSFilter Performance improvements Cache vector clip masks Avoid unnecessary glyph copies Various rendering fixes

--- corporate2/devel/multimedia/library/libass/actions.py
+++ corporate2/devel/multimedia/library/libass/actions.py
@@ -9,7 +9,7 @@ from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 
-WorkDir = "libass"
+#WorkDir = "libass"
 
 def setup():
     autotools.autoreconf("-vfi")
