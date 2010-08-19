cb15d630c0e7825a329f3f8b9031f1c14117dd1b
onur <onur@420bcd57-4a62-4fd6-832e-5ede16c90cc9>
2010-08-19 19:02:29 +0000

    Version bump

--- corporate2/devel/multimedia/library/opencore-amr/actions.py
+++ corporate2/devel/multimedia/library/opencore-amr/actions.py
@@ -1,19 +1,24 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyright 2009 TUBITAK/UEKAE
+# Copyright 2009-2010 TUBITAK/UEKAE
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/copyleft/gpl.txt.
 
 from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 
-WorkDir = "opencore-amr"
+
+def setup():
+    autotools.autoreconf("-vfi")
+    autotools.configure("--disable-dependency-tracking \
+                         --disable-static")
 
 def build():
     autotools.make()
 
 def install():
-    autotools.rawInstall('PREFIX=/usr \
-                          DESTDIR="%s"' % get.installDIR())
+    autotools.rawInstall('DESTDIR="%s"' % get.installDIR())
 
+    pisitools.dodoc("AUTHORS", "ChangeLog", "README")
