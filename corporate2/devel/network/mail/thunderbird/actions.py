92e9cbd8cc3b14a982649a744ca737d6f452ffdc
ozan.caglayan <ozan.caglayan@420bcd57-4a62-4fd6-832e-5ede16c90cc9>
2010-09-20 12:02:27 +0000

    Merge from 2009/devel/network/mail/thunderbird/:
    rev. 96316, by ozan.caglayan on 2010-07-23
    Bump to 3.1.1
    
    rev. 97090, by ozan.caglayan on 2010-08-15
    Let's ship enigmail too
    
    rev. 100837, by ozan.caglayan on 2010-09-20
    Version bump to 3.1.4 to fix several vulnerabilities
    
    rev. 100844, by ozan.caglayan on 2010-09-20
    disable enigmail

--- corporate2/devel/network/mail/thunderbird/actions.py
+++ corporate2/devel/network/mail/thunderbird/actions.py
@@ -36,8 +36,44 @@ def build():
         pisitools.copy("mozilla/dist/xpi-stage/locale-%s/chrome/%s.jar" % (locale, locale), "mozilla/dist/bin/chrome/")
         pisitools.copy("mozilla/dist/xpi-stage/locale-%s/chrome/%s.manifest" % (locale, locale), "mozilla/dist/bin/chrome/")
 
+    # Build enigmail
+    """
+    shelltools.cd("mailnews/extensions/enigmail")
+    shelltools.system("./makemake -r")
+    autotools.make()
+    autotools.make("xpi")
+    """
+
+def install_enigmail():
+    # Install enigmail
+    pisitools.insinto(MOZAPPDIR, "mozilla/dist/bin/enigmail-*.xpi")
+
+    enigmail_dir = "mozilla/extensions/{3550f703-e582-4d05-9a08-453d09bdfdc6}/{847b3a00-7ab1-11d4-8f02-006008948af5}"
+    pisitools.dodir("%s/%s" % (MOZAPPDIR, enigmail_dir))
+
+    old_dir = get.curDIR()
+
+    shelltools.cd("%s/%s/%s" % (get.installDIR(), MOZAPPDIR, enigmail_dir))
+    shelltools.system("unzip %s/%s/enigmail-*.xpi" % (get.installDIR(), MOZAPPDIR))
+    shelltools.cd(old_dir)
+
+    # Remove unwanted build artifacts from enigmail
+    for f in ("enigmail.jar", "enigmail-locale.jar", "enigmail-en-US.jar", "enigmail-skin.jar", "installed-chrome.txt", "enigmime.jar"):
+        pisitools.remove("%s/chrome/%s" % (MOZAPPDIR, f))
+
+    for f in ("libenigmime.so", "ipc.xpt", "enig*"):
+        pisitools.remove("%s/components/%s" % (MOZAPPDIR, f))
+
+    pisitools.removeDir("%s/defaults/preferences" % MOZAPPDIR)
+    pisitools.removeDir("%s/platform" % MOZAPPDIR)
+    pisitools.removeDir("%s/wrappers" % MOZAPPDIR)
+    pisitools.removeDir("%s/enigmail*.xpi" % MOZAPPDIR)
+
+
 def install():
     pisitools.insinto("/usr/lib/", "mozilla/dist/bin", "MozillaThunderbird", sym=False)
+
+    #install_enigmail()
 
     # Install default-prefs.js
     pisitools.insinto("%s/greprefs" % MOZAPPDIR, ".pardus-default-prefs.js", "all-pardus.js")
@@ -51,7 +87,7 @@ def install():
     pisitools.insinto("/usr/share/pixmaps", "other-licenses/branding/thunderbird/mailicon256.png", "thunderbird.png")
     pisitools.insinto("%s/icons" % MOZAPPDIR, "other-licenses/branding/thunderbird/mailicon16.png")
 
-    for s in (16,22,24,32,48,256):
+    for s in (16, 22, 24, 32, 48, 256):
         pisitools.insinto("/usr/share/icons/hicolor/%dx%d/apps" % (s,s), "other-licenses/branding/thunderbird/mailicon%d.png" % s)
 
     # Install docs
