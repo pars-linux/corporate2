1a7bc0d8129c0f936c4228954c9421f8f0781bb6
Ozan Çağlayan <ozancaglayan@users.noreply.github.com>
2010-09-22 10:59:03 +0000

    Include CMS profiles

--- hardware/printer/splix/actions.py
+++ hardware/printer/splix/actions.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyright 2008 TUBITAK/UEKAE
+# Copyright 2008-2010 TUBITAK/UEKAE
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
@@ -11,8 +11,6 @@ from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 
 def build():
-    shelltools.export("CC", get.CC())
-    shelltools.export("CXX", get.CXX())
     shelltools.export("OS_CFLAGS", get.CFLAGS())
     shelltools.export("OS_LDFLAGS", get.LDFLAGS())
     shelltools.export("OS_CXXFLAGS", get.CXXFLAGS())
@@ -20,9 +18,8 @@ def build():
     autotools.make()
 
 def install():
-    pisitools.dodir("/usr/lib/cups/filter")
-    pisitools.dodir("/usr/share/cups/model")
-
     autotools.install("DESTDIR=%s" % get.installDIR())
+
+    pisitools.insinto("/usr/share/cups/model/samsung/cms", "cms/*")
 
     pisitools.dodoc("AUTHORS", "ChangeLog", "COPYING", "README", "THANKS", "TODO")
