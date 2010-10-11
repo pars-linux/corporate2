402595ccdc8533f97fe381c4c6a0c1e6eb792c27
bahadir.kandemir <bahadir.kandemir@420bcd57-4a62-4fd6-832e-5ede16c90cc9>
2010-10-11 12:16:59 +0000

    Moved to Corporate 2
    BUG:COMMENT:14626

---
+++ corporate2/devel/util/admin/ahenk-agent/comar/service.py
@@ -0,0 +1,24 @@
+serviceType = "server"
+serviceDesc = _({"en": "Ahenk Agent",
+                 "tr": "Ahenk Ajan"})
+serviceConf = "ahenk_agent"
+
+from comar.service import *
+
+@synchronized
+def start():
+    import os
+    os.environ["LC_ALL"] = "C"
+    os.environ["LANG"] = "C"
+    startService(command="/usr/bin/ahenk_agent",
+                 args="%s -d" % config.get("OPTIONS", ""),
+                 pidfile="/var/run/ahenk-agent.pid",
+                 donotify=True)
+
+@synchronized
+def stop():
+    stopService(pidfile="/var/run/ahenk-agent.pid",
+                donotify=True)
+
+def status():
+    return isServiceRunning("/var/run/ahenk-agent.pid")
