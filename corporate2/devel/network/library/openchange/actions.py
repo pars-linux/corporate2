d2465e2913f4f3c0f460c2b434c3f51e41f14295
igungor <igungor@420bcd57-4a62-4fd6-832e-5ede16c90cc9>
2010-10-12 08:40:10 +0000

    Move openchange to C2 repository. Thanks for reviews.
    BUG:FIXED:13665

---
+++ corporate2/devel/network/library/openchange/actions.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 2
+# See the file http://www.gnu.org/copyleft/gpl.txt
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+
+WorkDir = "%s-COCHRANE" % get.srcDIR()
+
+def setup():
+    autotools.configure()
+
+def build():
+    autotools.make()
+
+def install():
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+
+    libs = ["libmapi", "libmapiadmin", "libmapiproxy", "libmapiserver", "libmapistore", "libocpf"]
+
+    for i in libs:
+        pisitools.dosym("%s.so.0.9" % i, "/usr/lib/%s.so.0" % i)
+
+    pisitools.removeDir("/usr/modules")
+
+    pisitools.dodoc("CHANGELOG", "ChangeLog", "COPYING", "README")
