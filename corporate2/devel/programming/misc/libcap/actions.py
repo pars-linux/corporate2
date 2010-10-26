23e4dcb5ec1b55b866dc569847b4567ca85d85b9
ozan.caglayan <ozan.caglayan@420bcd57-4a62-4fd6-832e-5ede16c90cc9>
2010-10-26 17:27:08 +0000

    Add in misc

---
+++ corporate2/devel/programming/misc/libcap/actions.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2005-2008 TUBITAK/UEKAE
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+
+def setup():
+    pisitools.dosed("Make.Rules", "pardusCFLAGS", get.CFLAGS())
+    pisitools.dosed("Make.Rules", "pardusLDFLAGS", get.LDFLAGS())
+
+def build():
+    autotools.make('CC="%s"' % get.CC())
+
+def install():
+    autotools.rawInstall("FAKEROOT=%s" % get.installDIR())
+
+    pisitools.insinto("/etc/security", "pam_cap/capability.conf")
+
+    # we should not need this static
+    pisitools.remove("/lib/libcap.a")
+    pisitools.dodoc("CHANGELOG", "README", "doc/capability.notes")
