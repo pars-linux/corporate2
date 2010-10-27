e3ddcfe05941f1d04e9df1f4de0f724376605380
fatih <fatih@420bcd57-4a62-4fd6-832e-5ede16c90cc9>
2010-10-27 13:10:30 +0000

    New snapshot from suspend-utils repo

--- corporate2/devel/hardware/powermanagement/suspend/actions.py
+++ corporate2/devel/hardware/powermanagement/suspend/actions.py
@@ -1,7 +1,5 @@
-#!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyright 2006-2009 TUBITAK/UEKAE
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
@@ -10,19 +8,22 @@ from pisi.actionsapi import pisitools
 from pisi.actionsapi import shelltools
 from pisi.actionsapi import get
 
-WorkDir = "suspend-0.8.20081103"
+WorkDir = "suspend-%s" % get.srcVERSION()
 
 def setup():
+    autotools.autoreconf("-vif")
     autotools.configure("--enable-static \
                          --enable-compress \
                          --disable-encrypt \
                          --with-initramfsdir=/usr/sbin")
 
 def build():
-    autotools.make()
+    autotools.make("-j1")
 
 def install():
     autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+
+    pisitools.removeDir("/dev")
 
     # will be created by postInstall script
     pisitools.remove("/etc/suspend.conf")
