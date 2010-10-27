11e1694a815ecdb6c03cae1502b3b0ae7d3fce7f
onur <onur@420bcd57-4a62-4fd6-832e-5ede16c90cc9>
2010-10-27 16:55:33 +0000

    fix build with latest autofoo

--- corporate2/devel/x11/util/xvattr/actions.py
+++ corporate2/devel/x11/util/xvattr/actions.py
@@ -10,7 +10,7 @@ from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 
 def setup():
-    autotools.autoreconf("-fi -I m4")
+    autotools.autoreconf("-vfi")
     autotools.configure("--disable-static")
 
 def build():
