e31272a9d2d48af7b08993995afbcd14f16650c1
Fatih Arslan <ftharsln@gmail.com>
2010-11-01 07:36:49 +0000

    Ready for C2

--- tex/addon/latex-xcolor/actions.py
+++ tex/addon/latex-xcolor/actions.py
@@ -15,23 +15,17 @@ WorkDir= "xcolor"
 def build():
     shelltools.system("latex --interaction=batchmode ./xcolor.ins")
 
-    texdocs = shelltools.ls("*.tex")
-    texdocs.extend(shelltools.ls("*.dtx"))
-    for texdoc in texdocs:
-        print texdoc
-        shelltools.system("texi2dvi -q -c --language=latex ./%s" % texdoc)
-
 def install():
-    dvidocs = []
-    for i in ["dvi", "pdf"]:
-        dvidocs.extend(shelltools.ls("*.%s" % i))
-    for dvidoc in dvidocs:
-        print dvidoc
-        pisitools.dodoc(dvidoc)
-        pisitools.dosym("/usr/share/doc/%s/%s" % (get.srcTAG(), dvidoc), "/usr/share/texmf/doc/latex/%s/%s" % (WorkDir, dvidoc))
 
-    for stylesdoc in ["sty", "def"]:
-        print stylesdoc
-        pisitools.insinto("/usr/share/texmf/tex/latex/%s/" % WorkDir, "*.%s" %stylesdoc)
+    for srcfile in shelltools.ls("."):
+        if srcfile.endswith(("dvi", "ps", "pdf")):
+            pisitools.dodoc(srcfile)
+            pisitools.dosym("/usr/share/doc/%s/%s" % (get.srcTAG(), srcfile), "/usr/share/texmf/doc/latex/%s/%s" % (WorkDir, srcfile))
+        elif srcfile.endswith(("sty", "cls", "fd", "clo", "def", "cfg")):
+            pisitools.insinto("/usr/share/texmf/tex/latex/%s/" % WorkDir, srcfile)
+
+    for srcfile in shelltools.ls("."):
+        if srcfile.endswith("tex"):
+            shelltools.system("texi2dvi -q -c --language=latex %s" % srcfile)
 
     pisitools.dodoc("ChangeLog", "README")
