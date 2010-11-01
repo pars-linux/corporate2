5ef4d2380f6de2d7eb18d862654e4e88eceed6de
ozan.caglayan <ozan.caglayan@420bcd57-4a62-4fd6-832e-5ede16c90cc9>
2010-11-01 07:16:09 +0000

    Fix build on 32-bit

--- corporate2/devel/server/web/webalizer/actions.py
+++ corporate2/devel/server/web/webalizer/actions.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyright 2007 TUBITAK/UEKAE
+# Copyright 2007-2010 TUBITAK/UEKAE
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
+from pisi.actionsapi import shelltools
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
@@ -12,11 +13,10 @@ from pisi.actionsapi import get
 WorkDir="webalizer-2.21-02"
 
 def setup():
-    autotools.autoreconf()
-    autotools.configure("--enable-dns \
-                         --with-language=turkish \
-                         --with-dblib=/usr/lib/libdb.so \
-                         --enable-geoip")
+    shelltools.export("CFLAGS", "%s -D_GNU_SOURCE" % get.CFLAGS())
+    autotools.configure("--enable-bz2 \
+                         --enable-geoip \
+                         --with-dblib=/usr/lib/libdb.so")
 def build():
     autotools.make()
 
