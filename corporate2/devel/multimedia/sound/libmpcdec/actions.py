7822f3f4c8211be98067eb73a2eb3c833b94bd48
sdalgic <sdalgic@420bcd57-4a62-4fd6-832e-5ede16c90cc9>
2010-11-01 14:41:35 +0000

    FSU libmpcdec...

--- corporate2/devel/multimedia/sound/libmpcdec/actions.py
+++ corporate2/devel/multimedia/sound/libmpcdec/actions.py
@@ -1,13 +1,17 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyright 2005-2008 TUBITAK/UEKAE
+# Copyright 2005-2010 TUBITAK/UEKAE
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
 from pisi.actionsapi import autotools
+import os
 
 def setup():
+    # This is a very important workaround / fix
+    os.system('touch -d "15 March 1980" configure.ac')
+
     autotools.configure("--disable-static")
 
 def build():
