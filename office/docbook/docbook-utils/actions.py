1922d15788d2ae2a3ad18bf5a1b76ac101ee16ef
Ozan Çağlayan <ozancaglayan@users.noreply.github.com>
2010-11-02 22:15:02 +0000

    Refactor package, fix buggy postinstall script.

--- office/docbook/docbook-utils/actions.py
+++ office/docbook/docbook-utils/actions.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyright 2007-2009 TUBITAK/UEKAE
+# Copyright 2007-2010 TUBITAK/UEKAE
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
@@ -19,5 +19,10 @@ def build():
 
 def install():
     autotools.rawInstall("DESTDIR=%s htmldir=/usr/share/doc/%s/html" % (get.installDIR(), get.srcNAME()))
+
+    for util in ("dvi", "html", "pdf", "ps", "rtf"):
+        pisitools.dosym("docbook2%s" % util, "/usr/bin/db2%s" % util)
+        pisitools.dosym("jw.1", "/usr/share/man/man1/db2%s.1" % util)
+    pisitools.dosym("jw.1", "/usr/share/man/man1/docbook2txt.1")
 
     pisitools.dodoc("AUTHORS", "ChangeLog", "NEWS", "README", "TODO")
