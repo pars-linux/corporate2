7945576d5b6361290eea1d4369f7a3cd4a3bd444
Ozan Çağlayan <ozancaglayan@users.noreply.github.com>
2010-11-02 21:12:44 +0000

    Refactor package, fix buggy postinstall.

--- office/docbook/docbook-dsssl/actions.py
+++ office/docbook/docbook-dsssl/actions.py
@@ -5,11 +5,18 @@
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
 from pisi.actionsapi import autotools
-from pisi.actionsapi import get
 from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+
+def setup():
+    pass
+
+def build():
+    pass
 
 def install():
-    autotools.rawInstall("BINDIR=%s/usr/bin DESTDIR=%s/usr/share/sgml/docbook/dsssl-stylesheets-%s install"
-                         % (get.installDIR(),get.installDIR(),get.srcVERSION()))
+    autotools.rawInstall("BINDIR=%s/usr/bin DESTDIR=%s/usr/share/sgml/docbook/dsssl-stylesheets-%s MANDIR=%s/%s install" % (get.installDIR(), get.installDIR(), get.srcVERSION(), get.installDIR(), get.manDIR()))
 
-    pisitools.rename("/usr/bin/collateindex.pl", "collateindex")
+    pisitools.dosym("dsssl-stylesheets-%s" % get.srcVERSION(), "/usr/share/sgml/docbook/dsssl-stylesheets")
+
+    #pisitools.rename("/usr/bin/collateindex.pl", "collateindex")
--- office/docbook/docbook-dsssl/comar/package.py
+++ office/docbook/docbook-dsssl/comar/package.py
@@ -1,19 +1,20 @@
 #!/usr/bin/python
 
 import os
+import glob
+
+def manage_catalogs(action, filename, ver):
+    catalog_dir = "/usr/share/sgml/docbook/dsssl-stylesheets-%s/catalog" % ver
+    cmd = "/usr/bin/install-catalog --%s %s %s" % (action, filename, catalog_dir)
+    print "**** %s" % cmd
+    os.system(cmd)
 
 def postInstall(fromVersion, fromRelease, toVersion, toRelease):
-    os.system("/usr/bin/install-catalog --add /etc/sgml/dsssl-docbook-stylesheets.cat \
-               /usr/share/sgml/docbook/dsssl-stylesheets-1.79/catalog")
-    os.system("/usr/bin/install-catalog --add /etc/sgml/dsssl-docbook-stylesheets.cat \
-               /usr/share/sgml/docbook/dsssl-stylesheets-1.79/common/catalog")
-    os.system("/usr/bin/install-catalog --add /etc/sgml/sgml-docbook.cat \
-               /etc/sgml/dsssl-docbook-stylesheets.cat")
+    for centralized in glob.glob("/etc/sgml/*-docbook-*.cat"):
+        manage_catalogs("add", centralized, toVersion)
 
 def preRemove():
-        os.system("/usr/bin/install-catalog --remove /etc/sgml/dsssl-docbook-stylesheets.cat \
-                   /usr/share/sgml/docbook/dsssl-stylesheets-1.79/catalog")
-        os.system("/usr/bin/install-catalog --remove /etc/sgml/dsssl-docbook-stylesheets.cat \
-                   /usr/share/sgml/docbook/dsssl-stylesheets-1.79/common/catalog")
-        os.system("/usr/bin/install-catalog --remove /etc/sgml/sgml-docbook.cat \
-                   /etc/sgml/dsssl-docbook-stylesheets.cat")
+    #FIXME: version is static because preRemove doesn't pass that info
+    ver = "1.79"
+    for centralized in glob.glob("/etc/sgml/*-docbook-*.cat"):
+        manage_catalogs("remove", centralized, ver)
