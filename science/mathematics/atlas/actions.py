a0baa65e0d0b0220fc4eaad9432b50e06d8f6872
Ozan Çağlayan <ozancaglayan@users.noreply.github.com>
2010-11-03 09:36:03 +0000

    Fix architecture detection

--- science/mathematics/atlas/actions.py
+++ science/mathematics/atlas/actions.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyright 2006-2009 TUBITAK/UEKAE
+# Copyright 2006-2010 TUBITAK/UEKAE
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
+from pisi.actionsapi import shelltools
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
-from pisi.actionsapi import shelltools
 from pisi.actionsapi import get
 
 WorkDir="ATLAS"
@@ -25,13 +25,20 @@ def setup():
                        -b %s \
                        --with-netlib-lapack=/usr/lib/liblapack.a" % bits[get.ARCH()])
 
+    # Fix architecture detection
+    if get.ARCH() == "x86_64":
+        pisitools.dosed("Make.inc", "ARCH =.*", "ARCH = HAMMER64SSE2")
+        pisitools.dosed("Make.inc", "-DATL_SSE3", "")
+        pisitools.dosed("Make.inc", "-msse3", "-msse2")
+    elif get.ARCH() == "i686":
+        pisitools.dosed("Make.inc", "ARCH =.*", "ARCH = PIII32SSE1")
+        pisitools.dosed("Make.inc", "-DATL_SSE3 -DATL_SSE2", "")
+        pisitools.dosed("Make.inc", "-msse3", "-msse")
+
+
 def build():
-    shelltools.cd("build")
-
-    autotools.make("-j1")
-
-    shelltools.cd("lib")
-    autotools.make("shared")
+    autotools.make("-C build -j1")
+    autotools.make("-C lib shared")
 
 def install():
     for lib in ["atlas","cblas","f77blas"]:
