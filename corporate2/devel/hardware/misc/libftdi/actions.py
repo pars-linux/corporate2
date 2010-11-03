b78412af18ca048ced0c574a3db8c9c23eb632e8
ozan.caglayan <ozan.caglayan@420bcd57-4a62-4fd6-832e-5ede16c90cc9>
2010-11-03 06:38:11 +0000

    Version bump

--- corporate2/devel/hardware/misc/libftdi/actions.py
+++ corporate2/devel/hardware/misc/libftdi/actions.py
@@ -12,11 +12,12 @@ def setup():
     # I haven't enabled non-C bindings, cause they aren't very useful
     # at the moment. In case if you need them later:
     # C++ binding: remove --without-boost and depend on boost
-    # Python binding: add --enable-python-binding and depend on swig
-    # Also I haven't decided that if they would be worth separating
-    # into their own binary packages.
     autotools.configure("--without-boost \
+                         --enable-python-binding \
                          --disable-static")
+
+    # kernel doesn't provide usb_device anymore
+    pisitools.dosed("packages/99-libftdi.rules", "usb_device", "usb")
 
 def build():
     autotools.make()
@@ -28,7 +29,15 @@ def install():
     pisitools.remove("/usr/bin/bitbang*")
     pisitools.remove("/usr/bin/find*")
     pisitools.remove("/usr/bin/simple")
+    pisitools.remove("/usr/bin/baud_test")
+    pisitools.remove("/usr/bin/serial_read")
+
     # Their source can be useful though
     pisitools.dodoc("examples/*.c", destDir="%s/examples" % get.srcNAME())
+
+    # Install udev rule
+    pisitools.insinto("/lib/udev/rules.d", "packages/99-libftdi.rules")
+
+    pisitools.doman("doc/man/man3/*.3")
 
     pisitools.dodoc("AUTHORS", "COPYING.LIB", "ChangeLog", "LICENSE", "README")
