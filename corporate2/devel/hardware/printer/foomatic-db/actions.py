7ec3e468d8a4c11a6149abdf8415f6e642f99885
ozan.caglayan <ozan.caglayan@420bcd57-4a62-4fd6-832e-5ede16c90cc9>
2010-11-03 09:55:32 +0000

    Version bump

--- corporate2/devel/hardware/printer/foomatic-db/actions.py
+++ corporate2/devel/hardware/printer/foomatic-db/actions.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyright 2005-2009 TUBITAK/UEKAE
+# Copyright 2005-2010 TUBITAK/UEKAE
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
 from pisi.actionsapi import shelltools
 from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 
 import os
@@ -15,9 +16,10 @@ WorkDir = "foomatic-db-%s" % get.srcVERSION().split("_", 1)[1]
 NoStrip = "/"
 
 def setup():
-    # Disable gzip compression for better lzma compression
-    autotools.configure("--disable-gzip-ppds \
-                         --with-drivers=NOOBSOLETES,NOEMPTYCMDLINE")
+    # For gutenprint printers, use gutenprint-ijs-simplified.5.2
+    pisitools.dosed("db/source/printer/*.xml", ">gutenprint<", ">gutenprint-ijs-simplified.5.2<")
+
+    autotools.configure()
 
     # Cleanup conflicts
     shelltools.cd("db/source")
