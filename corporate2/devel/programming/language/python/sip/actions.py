7b4a1fe5cfdeaa8c2b0299811a0cb1b0831d39a8
ozan.caglayan <ozan.caglayan@420bcd57-4a62-4fd6-832e-5ede16c90cc9>
2010-11-04 10:39:45 +0000

    Bump to 4.11.2

--- corporate2/devel/programming/language/python/sip/actions.py
+++ corporate2/devel/programming/language/python/sip/actions.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyright 2009 TUBITAK/UEKAE
+# Copyright 2009-2010 TUBITAK/UEKAE
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
@@ -9,6 +9,8 @@ from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import pythonmodules
 from pisi.actionsapi import get
+
+#WorkDir = "%s-4.11.2-snapshot-c93f5da3d4e4" % get.srcNAME()
 
 def setup():
     pythonmodules.run('configure.py \
