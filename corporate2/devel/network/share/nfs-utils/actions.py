c00b8cf983fae53a04d12588edd85792c91b0aa0
igungor <igungor@420bcd57-4a62-4fd6-832e-5ede16c90cc9>
2010-11-26 14:42:24 +0000

    nfs-utils seems ready.

--- corporate2/devel/network/share/nfs-utils/actions.py
+++ corporate2/devel/network/share/nfs-utils/actions.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyright 2005-2007 TUBITAK/UEKAE
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
@@ -10,15 +9,18 @@ from pisi.actionsapi import pisitools
 from pisi.actionsapi import shelltools
 from pisi.actionsapi import get
 
-def setup():
-    shelltools.export("CFLAGS","%s -fPIE" % get.CFLAGS())
-    shelltools.export("LDFLAGS","%s -pie" % get.LDFLAGS())
+shelltools.export("CFLAGS","%s -fpie -D_FILE_OFFSET_BITS=64" % get.CFLAGS())
+shelltools.export("LDFLAGS","%s -pie" % get.LDFLAGS())
 
-    autotools.configure("--with-statedir=/var/lib/nfs \
-                         --disable-gss \
-                         --enable-nfsv4 \
+def setup():
+    autotools.autoreconf("-vfi")
+    autotools.configure("--enable-mountconfig \
+                         --enable-ipv6 \
                          --enable-nfsv3 \
-                         --disable-static")
+                         --enable-nfsv4 \
+                         --enable-gss \
+                         --with-krb5=/usr \
+                         --with-statedir=/var/lib/nfs")
 
 def build():
     autotools.make()
@@ -26,18 +28,12 @@ def build():
 def install():
     autotools.rawInstall("DESTDIR=%s" % get.installDIR())
 
-    pisitools.dodir("/var/lib/nfs")
-    pisitools.dodir("/var/lib/nfs/statd")
-    pisitools.dodir("/var/lib/nfs/sm")
-    pisitools.dodir("/var/lib/nfs/sm.bak")
-    pisitools.dodir("/var/lib/nfs/rpc_pipefs")
-    pisitools.dodir("/var/lib/nfs/rpc_pipefs/nfs")
+    pisitools.dodir("/var/lib/nfs/statd/sm")
+    pisitools.dodir("/var/lib/nfs/statd/sm.bak")
     pisitools.dodir("/var/lib/nfs/v4recovery")
+    pisitools.dodir("/var/lib/nfs/rpc_pipefs")
 
-    shelltools.touch("%s/var/lib/nfs/state" % get.installDIR())
-    shelltools.touch("%s/var/lib/nfs/xtab" % get.installDIR())
-    shelltools.touch("%s/var/lib/nfs/etab" % get.installDIR())
-    shelltools.touch("%s/var/lib/nfs/rmtab" % get.installDIR())
+    pisitools.insinto("/etc", "utils/mount/nfsmount.conf")
 
     pisitools.domove("/usr/sbin/rpc.statd", "/sbin/")
 
