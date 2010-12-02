7365e8c450fca2cd7d32a4ffefa305fade746b86
ozan.caglayan <ozan.caglayan@420bcd57-4a62-4fd6-832e-5ede16c90cc9>
2010-12-02 13:13:45 +0000

    Bump to 0.8.2

--- corporate2/devel/network/connection/NetworkManager/actions.py
+++ corporate2/devel/network/connection/NetworkManager/actions.py
@@ -9,14 +9,11 @@ from pisi.actionsapi import pisitools
 from pisi.actionsapi import autotools
 from pisi.actionsapi import get
 
-#WorkDir = "%s-0.8" % get.srcNAME()
-
 def setup():
     autotools.autoreconf("-fi")
     autotools.configure("--disable-static \
                          --disable-dependency-tracking \
                          --disable-more-warnings \
-                         --localstatedir=/var \
                          --with-crypto=nss \
                          --with-distro=pardus \
                          --with-resolvconf=/etc/resolv.conf \
@@ -32,8 +29,6 @@ def check():
 def install():
     autotools.rawInstall("DESTDIR=%s" % get.installDIR())
 
-    pisitools.dodir("/var/lib/NetworkManager")
     pisitools.dodir("/etc/NetworkManager/VPN")
-    pisitools.dodir("/etc/NetworkManager/system-connections")
 
     pisitools.dodoc("README", "COPYING")
