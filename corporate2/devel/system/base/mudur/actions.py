1dda94677e1d04c3b7fd3e8918262ba9be1e2d86
ozan.caglayan <ozan.caglayan@420bcd57-4a62-4fd6-832e-5ede16c90cc9>
2010-12-08 11:29:14 +0000

    Bump to 4.1.3

--- corporate2/devel/system/base/mudur/actions.py
+++ corporate2/devel/system/base/mudur/actions.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyright 2006-2009 TUBITAK/UEKAE
+# Copyright 2006-2010 TUBITAK/UEKAE
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
@@ -12,9 +12,6 @@ from pisi.actionsapi import get
 def install():
     shelltools.system("./setup.py install %s" % get.installDIR())
 
-    pisitools.dodir("/etc/network")
     pisitools.dodir("/etc/mudur/services/enabled")
     pisitools.dodir("/etc/mudur/services/disabled")
     pisitools.dodir("/etc/mudur/services/conditional")
-
-    pisitools.remove("/bin/network")
