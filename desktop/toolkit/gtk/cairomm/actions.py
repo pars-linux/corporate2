fbb8d4061dfceb038ac085b3bc2a85493a99ed20
Ozan Çağlayan <ozancaglayan@users.noreply.github.com>
2010-12-14 13:12:32 +0000

    Version bump

--- desktop/toolkit/gtk/cairomm/actions.py
+++ desktop/toolkit/gtk/cairomm/actions.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyright 2006-2009 TUBITAK/UEKAE
+# Copyright 2006-2010 TUBITAK/UEKAE
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
@@ -11,14 +11,17 @@ from pisi.actionsapi import get
 
 def setup():
     autotools.configure("--disable-static")
+
     pisitools.dosed("libtool"," -shared ", " -Wl,--as-needed -shared ")
 
 def build():
-    autotools.make("-j1")
+    autotools.make()
 
 def install():
     autotools.rawInstall("DESTDIR=%s" % get.installDIR())
 
+    pisitools.domove("/usr/share/doc/cairomm-1.0/*", "/usr/share/doc/cairomm")
     pisitools.removeDir("/usr/share/devhelp")
+    pisitools.removeDir("/usr/share/doc/cairomm-*")
 
     pisitools.dodoc("AUTHORS", "README", "COPYING", "ChangeLog", "NEWS")
