6096c2a85b9654fd39360ec5210201ec3169d70e
ozan.caglayan <ozan.caglayan@420bcd57-4a62-4fd6-832e-5ede16c90cc9>
2010-12-14 14:42:41 +0000

    Version bump

--- corporate2/devel/desktop/toolkit/gtk/gtk2/actions.py
+++ corporate2/devel/desktop/toolkit/gtk/gtk2/actions.py
@@ -1,29 +1,29 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyright 2005-2008 TUBITAK/UEKAE
+# Copyright 2005-2010 TUBITAK/UEKAE
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
+from pisi.actionsapi import get
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
-from pisi.actionsapi import get
 from pisi.actionsapi import shelltools
 
 WorkDir = "gtk+-%s" % get.srcVERSION()
+shelltools.export("HOME", get.workDIR())
 
 def setup():
-    autotools.configure("--with-libjpeg \
-                         --with-libtiff \
-                         --with-libjasper\
-                         --with-libpng \
-                         --with-gdktarget=x11 \
+    shelltools.export("CFLAGS", get.CFLAGS().replace("-fomit-frame-pointer",""))
+    autotools.automake()
+    autotools.configure("--with-gdktarget=x11 \
                          --enable-xinerama \
                          --with-xinput=yes \
                          --enable-xkb \
                          --enable-shm \
-                         --disable-introspection \
-                         --with-included-loaders=png")
+                         --enable-silent-rules \
+                         --enable-introspection \
+                         --disable-papi")
     pisitools.dosed("libtool"," -shared ", " -Wl,--as-needed -shared ")
 
 def build():
