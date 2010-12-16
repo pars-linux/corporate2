df80f747c1c24e4fb883eeaaa81a6cae25150e0f
igungor <igungor@420bcd57-4a62-4fd6-832e-5ede16c90cc9>
2010-12-16 08:52:53 +0000

    Version bump to 0.6 major release.

--- corporate2/devel/multimedia/sound/clementine/actions.py
+++ corporate2/devel/multimedia/sound/clementine/actions.py
@@ -9,25 +9,34 @@ from pisi.actionsapi import pisitools
 from pisi.actionsapi import shelltools
 from pisi.actionsapi import get
 
+shelltools.export("HOME", get.workDIR())
+
 def setup():
-    for i in ["libprojectm", "qxt", "qtiocompressor"]:
+    #for i in ["libprojectm", "qxt", "qtiocompressor"]:
+    for i in ["libprojectm", "qxt"]:
         shelltools.unlinkDir("3rdparty/%s" % (i))
 
     # Upstream supports only gstreamer engine, other engines are unstable and lacking features.
-    # QTSINGLEAPPLICATION is builtin since we need to patch Qt just for this package and  Gökçen has given OK
+    # QTSINGLEAPPLICATION is builtin since we need to patch Qt just for this package and Gökçen has given OK
     # for using builtin qtsingleapplication.
     cmaketools.configure("-DHAVE_LIBNOTIFY=ON \
                           -DENGINE_GSTREAMER_ENABLED=ON \
                           -DENGINE_QT_PHONON_ENABLED=OFF \
                           -DENGINE_LIBVLC_ENABLED=OFF \
                           -DENGINE_LIBXINE_ENABLED=OFF \
-                          -DSTATIC_SQLITE=OFF \
-                          -DUSE_SYSTEM_QTSINGLEAPPLICATION=OFF \
                           -DUSE_SYSTEM_QXT=ON \
+                          -DUSE_SYSTEM_QTSINGLEAPPLICATION=OFF \
+                          -DSTATIC_SQLITE=OFF \
                           -DUSE_SYSTEM_PROJECTM=ON \
+                          -DENABLE_WIIMOTEDEV=ON \
+                          -DENABLE_LIBGPOD=ON \
+                          -DENABLE_IMOBILEDEVICE=ON \
                           -DENABLE_LIBMTP=ON \
-                          -DENABLE_VISUALISATIONS=ON \
                           -DENABLE_GIO=ON \
+                          -DENABLE_VISUALISATIONS=ON \
+                          -DQXTCORE_INCLUDE_DIRS=/usr/qt/4/include/QxtCore \
+                          -DQXTGUI_INCLUDE_DIRS=/usr/qt/4/include/QxtGui \
+                          -DQXT_LIBRARIES=/usr/qt/4/lib/libQxtGui.so \
                           -DBUNDLE_PROJECTM_PRESETS=OFF", sourceDir=".")
 
 def build():
