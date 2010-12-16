03211321c8fe0e410604e9d9806d038720a4f6cc
Ozan Çağlayan <ozancaglayan@users.noreply.github.com>
2010-12-16 14:51:50 +0000

    Version bump

--- hardware/disk/encfs/actions.py
+++ hardware/disk/encfs/actions.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyright 2007-2009 TUBITAK/UEKAE
+# Copyright 2007-2010 TUBITAK/UEKAE
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
@@ -9,8 +9,6 @@ from pisi.actionsapi import shelltools
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
-
-WorkDir = "encfs-1.5"
 
 def setup():
     shelltools.export("AUTOPOINT", "true")
