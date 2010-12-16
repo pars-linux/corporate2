04682d82ec75b47aca4673f14c08f23559c02ce3
H. İbrahim Güngör <igungor@users.noreply.github.com>
2010-12-16 12:36:12 +0000

    split configure parameters for better readability

--- desktop/toolkit/gtk/gtk-doc/actions.py
+++ desktop/toolkit/gtk/gtk-doc/actions.py
@@ -8,7 +8,8 @@ from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
 
 def setup():
-    autotools.configure("--disable-static --disable-scrollkeeper")
+    autotools.configure("--disable-static \
+                         --disable-scrollkeeper")
 
 def build():
     autotools.make()
