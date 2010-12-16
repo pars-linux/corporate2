08a9fac8fe33f66fe35fbcd694411a30f6b5df54
gokcen.eraslan <gokcen.eraslan@420bcd57-4a62-4fd6-832e-5ede16c90cc9>
2010-12-16 09:35:52 +0000

    Use internal libwp family and make extension install failures non-fatal

--- corporate2/devel/office/openoffice/openoffice/comar/package-presenter.py
+++ corporate2/devel/office/openoffice/openoffice/comar/package-presenter.py
@@ -12,11 +12,11 @@ def postInstall(fromVersion, fromRelease, toVersion, toRelease):
     ret = os.system("%s add --shared --force %s/%s.oxt" % (unopkg, extPath, extName))
 
     if ret != 0:
-        raise Exception("Could not install OO.org extension: %s" % extName)
+        print "Could not install OO.org extension: %s" % extName
 
 def preRemove():
     os.environ["JAVA_HOME"] = "/opt/sun-jre"
     ret = os.system("%s remove --shared %s" % (unopkg, extID))
 
     if ret != 0:
-        raise Exception("Could not remove OO.org extension: %s" % extName)
+        print "Could not remove OO.org extension: %s" % extName
