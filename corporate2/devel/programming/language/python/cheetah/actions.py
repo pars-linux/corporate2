fe516bc7fc6c65c32cd3977f3600515d531ea9e3
fethican <fethican@420bcd57-4a62-4fd6-832e-5ede16c90cc9>
2010-12-27 16:59:35 +0000

    Version bump, add app:console IsA

--- corporate2/devel/programming/language/python/cheetah/actions.py
+++ corporate2/devel/programming/language/python/cheetah/actions.py
@@ -8,9 +8,12 @@
 from pisi.actionsapi import pythonmodules
 from pisi.actionsapi import shelltools
 from pisi.actionsapi import get
+from pisi.actionsapi import pisitools
 
 WorkDir = "Cheetah-%s" % get.srcVERSION()
 
 def install():
     shelltools.export("CHEETAH_USE_SETUPTOOLS", "")
     pythonmodules.install()
+
+    pisitools.dodoc("CHANGES", "TODO")
