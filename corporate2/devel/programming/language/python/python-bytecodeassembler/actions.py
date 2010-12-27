70e8f1b61e3f4e15692df3392674028044e74675
fethican <fethican@420bcd57-4a62-4fd6-832e-5ede16c90cc9>
2010-12-27 21:12:05 +0000

    Version bump, license is changed to Apache-2.0

--- corporate2/devel/programming/language/python/python-bytecodeassembler/actions.py
+++ corporate2/devel/programming/language/python/python-bytecodeassembler/actions.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyright 2009 TUBITAK/UEKAE
+# Copyright 2010 TUBITAK/UEKAE
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
@@ -12,6 +12,9 @@ WorkDir = "BytecodeAssembler-%s" % get.srcVERSION()
 
 def setup():
     pythonmodules.compile()
+
+def check():
+    pythonmodules.compile("test")
 
 def install():
     pythonmodules.install()
