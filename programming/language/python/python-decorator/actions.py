50c7f3994711e613a4482ad482cff49ec865ffbd
fethican <fethican@420bcd57-4a62-4fd6-832e-5ede16c90cc9>
2010-12-27 21:54:49 +0000

    Version bump

--- programming/language/python/python-decorator/actions.py
+++ programming/language/python/python-decorator/actions.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyright 2009 TUBITAK/UEKAE
+# Copyright 2010 TUBITAK/UEKAE
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
@@ -18,4 +18,4 @@ def check():
 def install():
     pythonmodules.install()
 
-    pisitools.dodoc("README.txt", "documentation.html", "documentation.py")
+    pisitools.dodoc("README.txt", "documentation.py")
