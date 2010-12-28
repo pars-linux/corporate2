d5a850a01d1e35a1eaa34ed3dcbcb3bfc06768a8
Ozan Çağlayan <ozancaglayan@users.noreply.github.com>
2010-12-28 09:58:08 +0000

    Bump, use vimfiles directory

--- programming/language/vala/actions.py
+++ programming/language/vala/actions.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyright © 2010 TUBITAK/UEKAE
+# Copyright (C) 2010 TUBITAK/UEKAE
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
 
 def setup():
     autotools.configure("--enable-vapigen")
@@ -18,6 +19,6 @@ def check():
     autotools.make("check")
 
 def install():
-    autotools.install()
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
 
     pisitools.dodoc("NEWS", "README", "AUTHORS", "COPYING")
