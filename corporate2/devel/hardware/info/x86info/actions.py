4e6aa7d86a6d8eecbed94a422df3ab0670995ba7
ozan.caglayan <ozan.caglayan@420bcd57-4a62-4fd6-832e-5ede16c90cc9>
2010-12-28 12:05:21 +0000

    Version bump

--- corporate2/devel/hardware/info/x86info/actions.py
+++ corporate2/devel/hardware/info/x86info/actions.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyright 2009 TUBITAK/UEKAE
+# Copyright 2010 TUBITAK/UEKAE
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
@@ -10,7 +10,9 @@ from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 
 def build():
-    autotools.make("-j1")
+    autotools.make("CC=%s CFLAGS='%s' LDFLAGS='%s'" % (get.CC(),
+                                                       get.CFLAGS(),
+                                                       get.LDFLAGS()))
 
 def install():
     pisitools.dosbin("lsmsr")
