e7b851a69265d918fa204cb053930d23a1908706
ozan.caglayan <ozan.caglayan@420bcd57-4a62-4fd6-832e-5ede16c90cc9>
2010-12-28 09:06:47 +0000

    Version bump

--- corporate2/devel/hardware/printer/libpaper/actions.py
+++ corporate2/devel/hardware/printer/libpaper/actions.py
@@ -4,6 +4,7 @@
 # Licensed under the GNU General Public License, version 3.
 # See the file http://www.gnu.org/licenses/gpl.txt
 
+from pisi.actionsapi import shelltools
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
 
@@ -14,11 +15,13 @@ def setup():
     pisitools.dosed("libtool", "^hardcode_libdir_flag_spec=.*", "hardcode_libdir_flag_spec=\"\"")
     pisitools.dosed("libtool", "^runpath_var=LD_RUN_PATH", "runpath_var=DIE_RPATH_DIE")
 
-
 def build():
     autotools.make()
 
 def install():
     autotools.install()
+
+    for lang in shelltools.ls("debian/po/*.po"):
+        pisitools.domo(lang, shelltools.baseName(lang).replace(".po", ""), "libpaper.mo")
 
     pisitools.dodoc("README", "ChangeLog")
