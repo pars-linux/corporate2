89a99f7fb7092ca78f034d027cd563f82f47a81b
Ozan Çağlayan <ozancaglayan@users.noreply.github.com>
2011-01-04 08:13:05 +0000

    Bump to 4.1 to add LZO support

--- hardware/disk/squashfs-tools/actions.py
+++ hardware/disk/squashfs-tools/actions.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyright 2005-2009 TUBITAK/UEKAE
+# Copyright 2005-2010 TUBITAK/UEKAE
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
@@ -10,11 +10,9 @@ from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 
-WorkDir = "squashfs-tools-%s-with-lzma465" % get.srcVERSION()
-
 def build():
-    autotools.make('CC="%s"' % get.CC())
+    autotools.make('-C squashfs-tools CC="%s"' % get.CC())
 
 def install():
-    pisitools.dobin("mksquashfs")
-    pisitools.dobin("unsquashfs")
+    pisitools.dobin("squashfs-tools/mksquashfs")
+    pisitools.dobin("squashfs-tools/unsquashfs")
