ac463f5989f7e963a5379e83ecb6ab18ceab8b82
onur <onur@420bcd57-4a62-4fd6-832e-5ede16c90cc9>
2011-01-05 13:38:48 +0000

    Version bump Fix stride segfaults Fix x86_64 cpuid function

--- corporate2/devel/multimedia/misc/liboil/actions.py
+++ corporate2/devel/multimedia/misc/liboil/actions.py
@@ -7,9 +7,11 @@
 
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
+from pisi.actionsapi import shelltools
 from pisi.actionsapi import get
 
 def setup():
+    shelltools.export("CFLAGS", "%s -O2" % get.CFLAGS())
     autotools.configure("--disable-static \
                          --disable-gtk-doc")
 
