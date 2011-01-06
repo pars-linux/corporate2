7c9a25634bab97c4cdc05ee2fd83f549ea864980
ozan.caglayan <ozan.caglayan@420bcd57-4a62-4fd6-832e-5ede16c90cc9>
2011-01-06 12:11:14 +0000

    Sync with 2011

--- corporate2/devel/multimedia/sound/sox/actions.py
+++ corporate2/devel/multimedia/sound/sox/actions.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyright 2005-2009 TUBITAK/UEKAE
+# Copyright 2005-2010 TUBITAK/UEKAE
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
@@ -11,29 +11,12 @@ from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 
 def setup():
-    shelltools.export("CFLAGS","%s -fsigned-char" % get.CFLAGS())
-
-    #autotools.autoreconf("-fi")
+    shelltools.export("CFLAGS", "%s -fsigned-char -D_FILE_OFFSET_BITS=64" % get.CFLAGS())
+    autotools.autoreconf("-fi")
 
     autotools.configure("--disable-static \
-                         --disable-largefile \
-                         --disable-dependency-tracking \
-                         --enable-alsa \
-                         --enable-libao \
-                         --enable-oss \
-                         --with-sndfile \
-                         --with-ogg \
-                         --with-flac \
-                         --with-ffmpeg \
-                         --with-mad \
-                         --with-id3tag \
-                         --with-lame \
-                         --with-samplerate \
-                         --with-ladspa")
-
-    # autoreconf fails, lets fix the internal libtool file for correct as-needed usage
-    replace = (r"(\\\$deplibs) (\\\$compiler_flags)", r"\2 \1")
-    pisitools.dosed("libtool", *replace)
+                         --with-gsm \
+                         --with-dyn-default")
 
 def build():
     autotools.make()
