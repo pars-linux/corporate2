7a7e4cebc6e0b3a14c77996e29b2faed5e9992f5
fatih.arslan <fatih.arslan@420bcd57-4a62-4fd6-832e-5ede16c90cc9>
2011-01-06 13:18:05 +0000

    * Version bump to 1.1.0-rc1 * xdg-mime: correct install text to use type/subtype. * xdg-open: declare file variable as local. (BR32487) * xdg-open: decode file:// urls * xdg-screensaver: support X server blanking

--- corporate2/devel/desktop/misc/xdg-utils/actions.py
+++ corporate2/devel/desktop/misc/xdg-utils/actions.py
@@ -6,9 +6,12 @@
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
 from pisi.actionsapi import autotools
+from pisi.actionsapi import shelltools
 from pisi.actionsapi import get
 
 def setup():
+    # For fixing links' sandbox violations
+    shelltools.export("HOME", get.workDIR())
     autotools.configure()
 
 def build():
