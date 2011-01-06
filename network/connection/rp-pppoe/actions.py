b8b4bfe2e84917a086116ba9b8c5ebde95591a89
Ozan Çağlayan <ozancaglayan@users.noreply.github.com>
2011-01-06 09:10:59 +0000

    Fix broken plugin (#16065).
    BUG:FIXED:16065

--- network/connection/rp-pppoe/actions.py
+++ network/connection/rp-pppoe/actions.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyright 2005-2009 TUBITAK/UEKAE
+# Copyright 2005-2010 TUBITAK/UEKAE
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
@@ -13,7 +13,7 @@ from pisi.actionsapi import get
 def setup():
     shelltools.cd("src")
     autotools.autoreconf("-fi")
-    autotools.configure("--enable-plugin=../ppp-2.4.4")
+    autotools.configure()
 
 def build():
     shelltools.cd("src")
@@ -21,11 +21,7 @@ def build():
 
 def install():
     shelltools.cd("src")
-    autotools.rawInstall("DESTDIR=\"%s\" docdir=/usr/share/doc/%s" % (get.installDIR(), get.srcNAME()))
-
-    # Don't use compiled rp-pppoe plugin; use it from the current version of pppd
-    pisitools.remove("/etc/ppp/plugins/rp-pppoe.so")
-    pisitools.dosym("/usr/lib/pppd/2.4.4/rp-pppoe.so", "/etc/ppp/plugins/rp-pppoe.so")
+    autotools.rawInstall("DESTDIR=\"%s\" docdir=/%s/%s" % (get.installDIR(), get.docDIR(), get.srcNAME()))
 
     # Install symnlinks for easier usage
     for f in ["connect", "relay", "server", "setup", "sniff", "start", "status", "stop"]:
