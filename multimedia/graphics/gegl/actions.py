98fe4ba61c1bac14b8d0d07ab7fde5417428f8a5
Ozan Çağlayan <ozancaglayan@users.noreply.github.com>
2011-01-07 14:08:42 +0000

    Bump

--- multimedia/graphics/gegl/actions.py
+++ multimedia/graphics/gegl/actions.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyright 2008 TUBITAK/UEKAE
+# Copyright 2008-2010 TUBITAK/UEKAE
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
-from pisi.actionsapi import autotools
-from pisi.actionsapi import libtools
-from pisi.actionsapi import pisitools
 from pisi.actionsapi import shelltools
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 
 def setup():
@@ -18,7 +17,8 @@ def setup():
                          --enable-sse \
                          --with-cairo \
                          --with-graphviz \
-                         --with-gtk --with-gdk-pixbuf \
+                         --with-gtk \
+                         --with-gdk-pixbuf \
                          --with-libjpeg \
                          --with-libopenraw \
                          --with-libpng \
@@ -27,9 +27,11 @@ def setup():
                          --with-libv4l \
                          --with-lua \
                          --with-openexr \
-                         --with-pango --with-pangocairo \
+                         --with-pango \
+                         --with-pangocairo \
                          --with-sdl \
                          --disable-static \
+                         --disable-gtk-doc \
                          --enable-workshop")
 
     # libtool fixes for underlinking problems
@@ -40,17 +42,11 @@ def setup():
 def build():
     autotools.make()
 
+def check():
+    autotools.make("check")
+
 def install():
     autotools.install()
-
-    pisitools.dodir("/usr/include/gegl-0.0/gegl/buffer")
-    pisitools.dodir("/usr/include/gegl-0.0/gegl/module")
-
-    for header in [h for h in shelltools.ls("gegl/buffer") if h.endswith(".h")]:
-        pisitools.insinto("/usr/include/gegl-0.0/gegl/buffer", "gegl/buffer/%s" % header)
-
-    for header in [h for h in shelltools.ls("gegl/module") if h.endswith(".h")]:
-        pisitools.insinto("/usr/include/gegl-0.0/gegl/module", "gegl/module/%s" % header)
 
     pisitools.dodoc("AUTHORS", "ChangeLog", "COPYING", "COPYING.LESSER", "NEWS", "README")
 
