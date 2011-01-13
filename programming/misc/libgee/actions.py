49098994566e18ae4e524987bdece63676eb9d27
H. İbrahim Güngör <igungor@users.noreply.github.com>
2011-01-13 07:24:28 +0000

    Version bump.

--- programming/misc/libgee/actions.py
+++ programming/misc/libgee/actions.py
@@ -7,14 +7,16 @@
 
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
 
 def setup():
+    autotools.autoreconf("-vfi")
     autotools.configure("--disable-static")
 
 def build():
     autotools.make()
 
 def install():
-    autotools.install()
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
 
     pisitools.dodoc("NEWS", "README", "AUTHORS", "COPYING")
