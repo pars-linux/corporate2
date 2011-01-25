0881ca76693c6e6bef99780406dccbb1e4a6a5c8
Ozan Çağlayan <ozancaglayan@users.noreply.github.com>
2011-01-25 08:47:52 +0000

    Version bump to support SMBIOS 2.7 specification.

--- hardware/info/dmidecode/actions.py
+++ hardware/info/dmidecode/actions.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyright 2005-2009 TUBITAK/UEKAE
+# Copyright 2005-2010 TUBITAK/UEKAE
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
@@ -10,7 +10,7 @@ from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 
 def build():
-    autotools.make()
+    autotools.make("CC=%s CFLAGS='%s'" % (get.CC(), get.CFLAGS()))
 
 def install():
     autotools.rawInstall("DESTDIR=%s prefix=/%s install-bin install-man" % (get.installDIR(), get.defaultprefixDIR()))
