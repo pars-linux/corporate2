28d5db3fc1a8fefd8ad0647bc8e4bd44641e5989
ozan.caglayan <ozan.caglayan@420bcd57-4a62-4fd6-832e-5ede16c90cc9>
2011-01-26 07:22:24 +0000

    Add upstream patches from fedora

--- corporate2/devel/network/connection/pptpclient/actions.py
+++ corporate2/devel/network/connection/pptpclient/actions.py
@@ -1,25 +1,24 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyright 2006-2010 TUBITAK/UEKAE
+# Copyright 2006-2011 TUBITAK/UEKAE
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 #
 
+from pisi.actionsapi import shelltools
 from pisi.actionsapi import autotools
-from pisi.actionsapi import get
 from pisi.actionsapi import pisitools
-
-WorkDir="pptp-1.7.2"
-
-def setup():
-    pisitools.dosed("routing.c", "/bin/ip", "/sbin/ip")
+from pisi.actionsapi import get
 
 def build():
-    autotools.make("CC=%s CFLAGS='%s' DESTDIR=%s" % (get.CC(), get.CFLAGS(), get.installDIR()))
+    autotools.make("CC=%s CFLAGS='%s' IP=/sbin/ip" % (get.CC(), get.CFLAGS()))
 
 def install():
     autotools.install("DESTDIR=%s" % get.installDIR())
+
+    pisitools.dodir("/var/run/pptp")
+    shelltools.chmod("%s/var/run/pptp" % get.installDIR(), 0750)
     pisitools.dodoc("AUTHORS", "COPYING", "ChangeLog", "DEVELOPERS",
         "NEWS", "README", "TODO", "USING")
 
