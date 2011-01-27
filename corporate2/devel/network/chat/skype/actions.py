ce03a4277060f89c9d6405e2577e0c5549860bb2
ozan.caglayan <ozan.caglayan@420bcd57-4a62-4fd6-832e-5ede16c90cc9>
2011-01-27 12:54:55 +0000

    Merge

--- corporate2/devel/network/chat/skype/actions.py
+++ corporate2/devel/network/chat/skype/actions.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyright 2005-2008 TUBITAK/UEKAE
+# Copyright 2005-2011 TUBITAK/UEKAE
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
@@ -9,8 +9,8 @@ from pisi.actionsapi import pisitools
 NoStrip = ["/"]
 
 def install():
-    for data in ["avatars","lang","sounds"]:
-        pisitools.insinto("/usr/share/skype",data)
+    for data in ["avatars", "lang", "sounds"]:
+        pisitools.insinto("/usr/share/skype", data)
 
     pisitools.dobin("skype")
     pisitools.rename("/usr/bin/skype", "skype.bin")
@@ -18,8 +18,9 @@ def install():
     # Dbus config
     pisitools.insinto("/etc/dbus-1/system.d", "skype.conf")
 
-    pisitools.insinto("/usr/share/icons/hicolor/16x16/apps", "icons/SkypeBlue_16x16.png", "skype.png")
-    pisitools.insinto("/usr/share/icons/hicolor/32x32/apps", "icons/SkypeBlue_32x32.png", "skype.png")
-    pisitools.insinto("/usr/share/icons/hicolor/48x48/apps", "icons/SkypeBlue_48x48.png", "skype.png")
+    for size in ("16", "32", "48"):
+        pisitools.insinto("/usr/share/icons/hicolor/%sx%s/apps" % (size, size),
+                          "icons/SkypeBlue_%sx%s.png" % (size, size),
+                          "skype.png")
 
     pisitools.dodoc("README", "LICENSE")
