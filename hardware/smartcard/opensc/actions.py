6b12faf07cd2b826547be4ac0658aedabc2a9b23
Ozan Çağlayan <ozancaglayan@users.noreply.github.com>
2011-01-29 21:16:21 +0000

    Three stack-based buffer overflows, when processing crafted serial numbers of certain cards (CVE-2010-4523) (#16005)
    BUG:FIXED:16005

--- hardware/smartcard/opensc/actions.py
+++ hardware/smartcard/opensc/actions.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyright 2007-2010 TUBITAK/UEKAE
+# Copyright 2007-2011 TUBITAK/UEKAE
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
@@ -12,28 +12,20 @@ from pisi.actionsapi import get
 
 def setup():
     pisitools.dosed("Makefile.am", "win32 ", "")
-    shelltools.unlink("m4/libassuan.m4")
     autotools.autoreconf("-fi")
 
     autotools.configure("--disable-static \
+                         --disable-assert \
                          --enable-pcsc \
-                         --enable-openct \
-                         --enable-nsplugin \
-                         --with-pinentry=/usr/bin/pinentry \
-                         --with-pcsc-provider=libpcsclite.so.1 \
-                         --with-plugindir=/usr/lib/nsbrowser/plugins/")
+                         --with-pcsc-provider=libpcsclite.so.1")
 
 def build():
     autotools.make()
 
 def install():
     autotools.rawInstall("DESTDIR=%s" % get.installDIR())
-    pisitools.removeDir("/usr/share/doc/opensc")
-
-    pisitools.remove("/usr/lib/nsbrowser/plugins/opensc-signer.so")
-    pisitools.domove("/usr/lib/opensc-signer.so", "/usr/lib/nsbrowser/plugins")
+    pisitools.removeDir("/usr/sbin")
 
     pisitools.insinto("/etc", "etc/opensc.conf")
 
-    pisitools.dohtml("doc/html.out/*.html")
     pisitools.dodoc("README")
