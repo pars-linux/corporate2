b44825ac5f15f9fd1275ae75def323381c4b55d0
Gökçen Eraslan <gokcen.eraslan@gmail.com>
2011-02-01 14:46:35 +0000

    Merge from devel/programming/language/perl/perl-JSON:
    rev. 104826, by sdalgic on 2010-11-10
    Version bump and perl-JSON is ready for Pardus 2011.

--- programming/language/perl/perl-JSON/actions.py
+++ programming/language/perl/perl-JSON/actions.py
@@ -23,5 +23,4 @@ def check():
 def install():
     perlmodules.install()
 
-    pisitools.removeDir("/usr/lib/perl5/vendor_perl/%s/%s-linux-thread-multi/" % (get.curPERL(), get.ARCH()))
     pisitools.dodoc("Changes", "README")
