679b4cdee4bc327523cfd12b47a6a2350601d4c6
ozan.caglayan <ozan.caglayan@420bcd57-4a62-4fd6-832e-5ede16c90cc9>
2011-02-02 09:07:21 +0000

    Bump, separate architecture packages.

--- corporate2/devel/util/shell/command-not-found/actions.py
+++ corporate2/devel/util/shell/command-not-found/actions.py
@@ -1,17 +1,22 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyright 2008-2009 TUBITAK/UEKAE
+# Copyright 2008-2011 TUBITAK/UEKAE
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
 from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+
+WorkDir = "%s-%s-%s" % (get.srcNAME(),
+                        get.srcVERSION(),
+                        get.ARCH())
 
 def install():
     pisitools.dobin("src/command-not-found")
     pisitools.insinto("/var/db/command-not-found", "data/packages.db")
 
-    for lang in ["de", "es", "fr", "nl", "tr", "sv", "pl"]:
+    for lang in ["da", "de", "es", "fr", "hu", "it", "nl", "ru", "sv", "tr"]:
         pisitools.domo("po/%s.po" % lang, lang, "command-not-found.mo")
 
     pisitools.dodoc("AUTHORS", "COPYING", "README")
