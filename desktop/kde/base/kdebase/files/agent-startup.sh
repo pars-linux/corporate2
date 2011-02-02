07b723b78c630e90a5fdf09c6235af7f2be6d1aa
Fatih Arslan <ftharsln@gmail.com>
2011-02-02 14:49:03 +0000

    Do not start gpg/ssh agents if they are already running

--- desktop/kde/base/kdebase/files/agent-startup.sh
+++ desktop/kde/base/kdebase/files/agent-startup.sh
@@ -2,10 +2,10 @@
 #
 # Customized agents startup file
 
-if [ -x /usr/bin/gpg-agent ]; then
+if [ -x /usr/bin/gpg-agent -a -z "$GPG_AGENT_INFO" ]; then
   eval "$(/usr/bin/gpg-agent --daemon)"
 fi
 
-if [ -x /usr/bin/ssh-agent ]; then
+if [ -x /usr/bin/ssh-agent -a -z "$SSH_AGENT_PID" ]; then
   eval "$(/usr/bin/ssh-agent -s)"
 fi
