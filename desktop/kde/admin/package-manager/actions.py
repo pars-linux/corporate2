049a2617bd2f8b5b27e739c437e0687529600510
Gokmen Goksel <gokmen@goksel.me>
2011-02-03 10:25:41 +0000

    - New release  - Switched to 2011 Package Manager Branch
    BUG:FIXED:15999

--- desktop/kde/admin/package-manager/actions.py
+++ desktop/kde/admin/package-manager/actions.py
@@ -5,8 +5,12 @@
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
+from pisi.actionsapi import pisitools
 from pisi.actionsapi import pythonmodules
 
 def install():
     pythonmodules.install()
+
+    # In C2 we dont need these desktop files 
+    pisitools.remove("/usr/share/applications/package-manager-*.desktop")
 
