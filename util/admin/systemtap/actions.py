48643e1f28559c357ea13875f252048870b7610e
Ozan Çağlayan <ozancaglayan@users.noreply.github.com>
2011-02-04 12:24:49 +0000

    Version bump to fix CVE-2010-4170, CVE-2010-4171 (#15544).
    BUG:FIXED:15544

--- util/admin/systemtap/actions.py
+++ util/admin/systemtap/actions.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyright 2006-2010 TUBITAK/UEKAE
+# Copyright 2006-2011 TUBITAK/UEKAE
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
@@ -11,13 +11,16 @@ from pisi.actionsapi import autotools
 from pisi.actionsapi import get
 
 def setup():
+    pisitools.dosed("configure.ac", "-Werror")
+    pisitools.dosed("Makefile.am", "-Werror")
     autotools.autoreconf("-vfi")
+    # --enable-publican --with-publican-brand=common
     autotools.configure("--enable-grapher \
                          --enable-sqlite \
                          --enable-pie \
-                         --disable-server \
-                         --disable-crash \
                          --disable-docs \
+                         --disable-publican \
+                         --disable-crash \
                          --disable-silent-rules \
                          --docdir=/%s/%s \
                          --without-rpm" % (get.docDIR(), get.srcNAME()))
@@ -30,6 +33,21 @@ def install():
 
     pisitools.dodir("/var/cache/systemtap")
     pisitools.dodir("/var/run/systemtap")
+    pisitools.dodir("/var/log/stap-server")
+    pisitools.dodir("/etc/logrotate.d")
+    pisitools.dodir("/etc/stap-server/conf.d")
+    pisitools.dodir("/etc/systemtap/conf.d")
+    pisitools.dodir("/etc/systemtap/script.d")
+
+    pisitools.dobin("stap-prep")
+
+    shelltools.copytree("testsuite", "%s/usr/share/systemtap" % get.installDIR())
+
+    pisitools.insinto("/etc/logrotate.d", "initscript/logrotate.stap-server", "stap-server")
+    pisitools.insinto("/etc/conf.d", "initscript/config.stap-server", "stap-server")
+    pisitools.insinto("/etc/systemtap", "initscript/config.systemtap", "config")
 
     # Clean uprobes directory
     autotools.make("-C %s/usr/share/systemtap/runtime/uprobes clean" % get.installDIR())
+
+    pisitools.dodoc("COPYING", "HACKING", "INTERNALS", "initscript/README.*")
