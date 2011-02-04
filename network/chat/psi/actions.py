14dd6d4d75dd082da4c15a5ab7af1d8855792e7d
Serdar Dalgıç <sd@serdardalgic.org>
2011-02-04 10:43:15 +0000

    psi is ready for C2.

---
+++ network/chat/psi/actions.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 3.
+# See the file http://www.gnu.org/copyleft/gpl.txt.
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+
+def setup():
+    autotools.rawConfigure("--prefix=/usr \
+                            --qtdir=/usr/qt/4 \
+                            --disable-bundled-qca")
+
+def build():
+    autotools.make()
+
+def install():
+    autotools.rawInstall('INSTALL_ROOT="%s"'  % get.installDIR())
+
+    pisitools.domove("/usr/share/psi/README","/usr/share/doc/psi")
+    pisitools.domove("/usr/share/psi/COPYING","/usr/share/doc/psi")
