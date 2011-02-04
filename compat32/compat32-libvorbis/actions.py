df8374765324e18792a0efb6a461188c1754ee0f
Onur Küçük <zaburt@users.noreply.github.com>
2011-02-04 13:54:40 +0000

    compat32-libvorbis is ready

---
+++ compat32/compat32-libvorbis/actions.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2005-2009 TUBITAK/UEKAE
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import shelltools
+from pisi.actionsapi import get
+
+shelltools.export("CFLAGS", "%s -m32" % get.CFLAGS())
+shelltools.export("CXXFLAGS", "%s -m32" % get.CXXFLAGS())
+shelltools.export("LDFLAGS", "%s -m32" % get.LDFLAGS())
+shelltools.export("PKG_CONFIG_LIBDIR", "/usr/lib32/pkgconfig")
+
+def setup():
+    # sanitize flags
+    pisitools.dosed("configure", "-mno-ieee-fp")
+    pisitools.dosed("configure", "-mfused-madd")
+    pisitools.dosed("configure", "-mcpu=750")
+    pisitools.dosed("configure", "-O20")
+
+    autotools.configure("--disable-static \
+                         --libdir=/usr/lib32")
+
+def build():
+    autotools.make()
+
+def install():
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+
+    for i in ["share", "include"]:
+        pisitools.removeDir("/usr/%s" % i)
+
