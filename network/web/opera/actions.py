cc6414bff3ae54db6de79500fe7ff18c6ea087f9
Serdar Dalgıç <sd@serdardalgic.org>
2011-02-04 08:42:23 +0000

    Add Opera to Corporate2.

---
+++ network/web/opera/actions.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2006-2009 TUBITAK/UEKAE
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import autotools
+from pisi.actionsapi import shelltools
+from pisi.actionsapi import get
+
+arch = "i386" if get.ARCH() == "i686" else "x86_64"
+
+WorkDir = "%s-%s-1190.%s.linux" % (get.srcNAME(), get.srcVERSION(), arch)
+
+def build():
+    # Flashplugin hack for Opera, see pardus #13989
+    autotools.compile("-shared -fPIC -L/opt/netscape/plugins/ -lflashplayer \
+                       -o libflashplayer.so -Wl,-rpath \
+                       /opt/netscape/plugins/ opera-flash-workaround.c")
+
+def install():
+    shelltools.system("./install --prefix /usr --repackage %s/usr" % get.installDIR())
+
+    pisitools.insinto("/usr/lib/opera/plugins/", "libflashplayer.so")
