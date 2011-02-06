8732cc3f98074fe9c4941e85a2c4639292df7cec
M. Emre Atasever <m.emre.atas@gmail.com>
2011-02-06 17:37:03 +0000

    fixes and takeover

--- programming/misc/iksemel/actions.py
+++ programming/misc/iksemel/actions.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyright 2005-2009 TUBITAK/UEKAE
+# Copyright 2005-2011 TUBITAK/UEKAE
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
@@ -10,8 +10,9 @@ from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 
 def setup():
-    autotools.autoreconf("-fi")
-    autotools.configure("--disable-static")
+    autotools.autoreconf("-fvi")
+    autotools.configure("--disable-static \
+                         --disable-rpath")
 
 def build():
     autotools.make()
