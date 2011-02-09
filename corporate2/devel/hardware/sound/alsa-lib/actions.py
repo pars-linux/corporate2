cb670288ee8ede31a57a626ea149d2960ac565c5
ozan.caglayan <ozan.caglayan@420bcd57-4a62-4fd6-832e-5ede16c90cc9>
2011-02-09 08:34:56 +0000

    Version bump

--- corporate2/devel/hardware/sound/alsa-lib/actions.py
+++ corporate2/devel/hardware/sound/alsa-lib/actions.py
@@ -9,8 +9,6 @@ from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 
-WorkDir = "alsa-lib"
-
 def setup():
     autotools.autoreconf("-fi")
     autotools.configure("--disable-aload")
