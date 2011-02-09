a5c25f772da3c289ae31b36fdfb706624152b471
ozan.caglayan <ozan.caglayan@420bcd57-4a62-4fd6-832e-5ede16c90cc9>
2011-02-09 08:20:52 +0000

    Version bump

--- corporate2/devel/hardware/firmware/alsa-firmware/actions.py
+++ corporate2/devel/hardware/firmware/alsa-firmware/actions.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyright 2006-2009 TUBITAK/UEKAE
+# Copyright 2006-2011 TUBITAK/UEKAE
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
@@ -11,9 +11,6 @@ from pisi.actionsapi import shelltools
 from pisi.actionsapi import get
 
 NoStrip = ["/"]
-
-if "_" in get.srcVERSION():
-    WorkDir = get.srcNAME()
 
 def setup():
     pisitools.dosed("configure.in", "multisound/Makefile", "")
