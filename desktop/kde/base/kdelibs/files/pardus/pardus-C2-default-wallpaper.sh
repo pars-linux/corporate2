486048724223dc87e0073548bee245548f3d7834
Ozan Çağlayan <ozancaglayan@users.noreply.github.com>
2011-02-11 12:15:11 +0000

    Cleanup upd files, add a default wallpaper.

---
+++ desktop/kde/base/kdelibs/files/pardus/pardus-C2-default-wallpaper.sh
@@ -0,0 +1,4 @@
+if [ -f /usr/kde/3.5/share/wallpapers/default_wallpaper.png ]; then
+    echo 'Wallpaper[$e]=/usr/kde/3.5/share/wallpapers/default_wallpaper.png'
+    echo 'WallpaperMode=ScaleAndCrop'
+fi
