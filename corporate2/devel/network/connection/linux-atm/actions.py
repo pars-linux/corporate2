0e3461f33ca5d1c28440d895a31bc94ab0733e9d
ozan.caglayan <ozan.caglayan@420bcd57-4a62-4fd6-832e-5ede16c90cc9>
2011-02-14 06:58:20 +0000

    Version bump, replace br2684ctl

--- corporate2/devel/network/connection/linux-atm/actions.py
+++ corporate2/devel/network/connection/linux-atm/actions.py
@@ -1,22 +1,19 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyright 2005-2009 TUBITAK/UEKAE
+# Copyright 2005-2010 TUBITAK/UEKAE
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
-from pisi.actionsapi import shelltools
 from pisi.actionsapi import get
 
 def setup():
-    shelltools.export("CFLAGS", "%s -fno-strict-aliasing" % get.CFLAGS())
-    pisitools.dosed("src/Makefile.in", "br2684", "")
-    autotools.configure()
+    autotools.configure("--disable-static")
 
 def build():
-    autotools.make()
+    autotools.make("-j1")
 
 def install():
     autotools.rawInstall("DESTDIR=%s man_prefix=/usr/share/man" % get.installDIR())
