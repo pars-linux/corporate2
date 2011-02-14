5df296a09a76ba71eba7063ee2dfce0cb7147e44
Ozan Çağlayan <ozancaglayan@users.noreply.github.com>
2011-02-14 07:19:49 +0000

    Merge bump to 1.0.21

--- hardware/scanner/sane-backends/comar/package.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python
-
-import shutil
-
-def postInstall(fromVersion, fromRelease, toVersion, toRelease):
-    try:
-        shutil.rmtree("/var/lib/lock/sane")
-    except OSError:
-        pass
