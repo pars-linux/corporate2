d038ccde7448240ba1de7697240f404a0ab07cf2
Gökçen Eraslan <gokcen.eraslan@gmail.com>
2011-02-18 13:23:23 +0000

    Version bump to 0.9.6.6 and switch to cmake.
    BUG:FIXED:17006

--- programming/language/falcon/falcon/actions.py
+++ programming/language/falcon/falcon/actions.py
@@ -1,22 +1,21 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyright 2009 TUBITAK/UEKAE
+# Copyright 2009-2010 TUBITAK/UEKAE
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
-from pisi.actionsapi import pisitools
+from pisi.actionsapi import cmaketools
 from pisi.actionsapi import shelltools
 from pisi.actionsapi import get
 
-import os
-
 WorkDir = "Falcon-%s" % get.srcVERSION()
 
+def setup():
+    cmaketools.configure()
+
 def build():
-    shelltools.system("./build.sh -p %s -f %s -l %s" % (os.path.join(get.installDIR(), "usr"), "/usr/", "/lib/"))
+    cmaketools.make()
 
 def install():
-    shelltools.system("./build.sh -i")
-
-    pisitools.dodoc("AUTHORS", "ChangeLog", "copyright", "LICENSE*", "README", "RELNOTES", "versioninfo")
+    cmaketools.install()
