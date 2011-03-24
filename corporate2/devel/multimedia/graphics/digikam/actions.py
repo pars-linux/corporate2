d7187a2042a82305668f6e8123b04f603a5132fa
ozan.caglayan <ozan.caglayan@420bcd57-4a62-4fd6-832e-5ede16c90cc9>
2011-03-24 06:11:33 +0000

    Avoid sandbox violation (pb#17474).
    BUG:FIXED:17474

--- corporate2/devel/multimedia/graphics/digikam/actions.py
+++ corporate2/devel/multimedia/graphics/digikam/actions.py
@@ -1,14 +1,18 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyright 2005-2010 TUBITAK/UEKAE
+# Copyright 2005-2011 TUBITAK/UEKAE
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
 from pisi.actionsapi import kde
+from pisi.actionsapi import get
 from pisi.actionsapi import pisitools
+from pisi.actionsapi import shelltools
 
 KeepSpecial = ["libtool"]
+
+shelltools.export("HOME", get.workDIR())
 
 def setup():
     kde.configure("--enable-shared \
