1b34e2ecea36ffffe4326829fb28b8b4bda36e78
Kaan Özdinçer <kaanozdincer@gmail.com>
2011-05-13 06:59:17 +0000

    Gone to C2. Thanks for reviews.
    BUG:FIXED:17998

---
+++ programming/language/perl/perl-Convert-BinHex/actions.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import perlmodules
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+
+def setup():
+    perlmodules.configure()
+
+def build():
+    perlmodules.make()
+
+def install():
+    perlmodules.install()
+
+    pisitools.dodoc("COPYING", "README*")
+
+    pisitools.removeDir("/usr/lib/perl5/vendor_perl/%s/%s-linux-thread-multi/" % (get.curPERL(), get.ARCH()))
