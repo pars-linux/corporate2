9f0293b84ac4b7cc4d22d6a8736bbb0e8baeaae5
ebayer <ebayer@420bcd57-4a62-4fd6-832e-5ede16c90cc9>
2011-05-25 18:41:54 +0000

    Version bump and major changes:
    * Version Bump
    * Add config file to autostart selected containers
    * Add uludag/trunk/scripts/lxc-pardus
    * Add service script to autostart/stop selected containers
    * Add bridge-utils as dependency
    * Delete unnecessary patches

--- hardware/virtualization/lxc/comar/service.py
+++ hardware/virtualization/lxc/comar/service.py
@@ -2,13 +2,38 @@ from comar.service import *
 
 serviceType = "local"
 serviceDefault = "off"
-serviceDesc = _({"en": "LXC Management Service",
-                 "tr": "LXC Yönetim Hizmeti"})
+serviceDesc = _({"en": "Linux Containers Management Service",
+                 "tr": "Linux Kapları Yönetim Hizmeti"})
+serviceConf = "lxc"
+
+LOCKFILE="/var/lock/subsys/lxc"
+
+import os
 
 @synchronized
 def start():
+    # Start all containers listed in /etc/conf.d/lxc
+    open(LOCKFILE, 'w').close()
+    os.system("/usr/bin/logger -t LXC -- Service is starting...")
+    for container in config.get("AUTOSTART", "").split():
+        os.system("/usr/bin/logger -t LXC -- Starting container %s" % container)
+        os.system("/usr/bin/screen -dmS init-%s /usr/bin/lxc-start -n %s" % (container, container))
+
+    os.system("/usr/bin/logger -t LXC -- All containers listed in /etc/conf.d/lxc started.")
 
 @synchronized
 def stop():
+    # Currently there is no way to gracefully shutdown a container.
+    # Mudur is not stopping services running inside a container when
+    # it recieves the TERM signal. So we must kill init of the container.
+    os.system("/usr/bin/logger -t LXC -- Service is stopping...")
+    for container in config.get("AUTOSTART", "").split():
+        os.system("/usr/bin/logger -t LXC -- Stopping container %s" % container)
+        os.system("/usr/bin/lxc-stop -n %s" % container)
+
+    os.system("/usr/bin/logger -t LXC -- All containers listed in /etc/conf.d/lxc stopped.")
+    os.remove(LOCKFILE)
 
 def status():
+    return os.path.isfile(LOCKFILE)
+
