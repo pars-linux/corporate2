b6d9f10bf297182dcb2f2ffbaa0620cb56d1f8e1
fatih.arslan <fatih.arslan@420bcd57-4a62-4fd6-832e-5ede16c90cc9>
2011-05-27 06:03:05 +0000

    Fix typo

--- corporate2/devel/kernel/default/drivers/module-virtualbox-guest/files/vbox-guest.sh
+++ corporate2/devel/kernel/default/drivers/module-virtualbox-guest/files/vbox-guest.sh
@@ -5,7 +5,7 @@
 case $ACTION in
     add)
         if [ "x${LOAD_VBOXVFS_MODULE-true}" == "xtrue" ]; then
-            /sbin/modprobe -s vboxfs
+            /sbin/modprobe -s vboxsf
         fi
         if [ -x /usr/sbin/VBoxService ]; then
             /usr/sbin/VBoxService $VBOXSERVICE_OPTS
