a6833e98073b3fc3ad8ebb8435eee65322b58873
ozan.caglayan <ozan.caglayan@420bcd57-4a62-4fd6-832e-5ede16c90cc9>
2011-06-17 07:38:16 +0000

    Bump to our own 3.5.11 tarball

--- corporate2/devel/desktop/kde/l10n/kde-i18n-tr/actions.py
+++ corporate2/devel/desktop/kde/l10n/kde-i18n-tr/actions.py
@@ -1,19 +1,20 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyright 2005-2007 TUBITAK/UEKAE
+# Copyright 2005-2011 TUBITAK/UEKAE
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
-from pisi.actionsapi import kde
-from pisi.actionsapi import pisitools
 from pisi.actionsapi import shelltools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import kde
 from pisi.actionsapi import get
 
 shelltools.export("HOME", get.workDIR())
 
 def setup():
-    kde.configure("--without-arts")
+    kde.make("-f admin/Makefile.common")
+    kde.configure()
 
 def build():
     kde.make()
