45b7048a3d4a23d75df918ba47bed482edc4f915
Ozan Çağlayan <ozancaglayan@users.noreply.github.com>
2011-06-24 06:28:37 +0000

    - Refactor paths, - Fix CA path, - Use 2048 bits RSA keys instead of 1024.

--- system/base/openssl/actions.py
+++ system/base/openssl/actions.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyright 2005-2008 TUBITAK/UEKAE
+# Copyright 2005-2011 TUBITAK/UEKAE
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
@@ -14,6 +14,7 @@ def setup():
     shelltools.system("./config \
                        --prefix=/usr \
                        --openssldir=/etc/ssl \
+                       --enginesdir=/usr/lib/openssl/engines \
                        zlib enable-camellia enable-seed enable-tlsext enable-rfc3779 \
                        threads shared -Wa,--noexecstack")
 
@@ -26,6 +27,9 @@ def build():
     autotools.make("rehash")
 
 def check():
+    #FIXME: Some tests write into /etc/pki directory which violates
+    # sandbox rules. It is not important for now. However, we will
+    # need to fix it later. (08/17/2010 --Eren)
     homeDir = "%s/test-home" % get.workDIR()
     shelltools.export("HOME", homeDir)
     shelltools.makedirs(homeDir)
@@ -35,6 +39,10 @@ def check():
 def install():
     autotools.rawInstall("INSTALL_PREFIX=%s MANDIR=/usr/share/man" % get.installDIR())
 
+    # Move engines to /usr/lib/openssl/engines
+    pisitools.dodir("/usr/lib/openssl")
+    pisitools.domove("/usr/lib/engines", "/usr/lib/openssl")
+
     # Certificate stuff
     pisitools.dobin("tools/c_rehash")
     pisitools.dosym("/etc/ssl/certs/ca-bundle.crt","/etc/ssl/cert.pem")
@@ -43,6 +51,10 @@ def install():
     pisitools.rename("/usr/share/man/man1/passwd.1", "ssl-passwd.1")
     pisitools.rename("/usr/share/man/man3/rand.3", "ssl-rand.3")
     pisitools.rename("/usr/share/man/man3/err.3", "ssl-err.3")
+
+    # Create CA dirs
+    for cadir in ["CA", "CA/private", "CA/certs", "CA/crl", "CA/newcerts"]:
+        pisitools.dodir("/etc/ssl/%s" % cadir)
 
     # No static libs
     pisitools.remove("/usr/lib/*.a")
