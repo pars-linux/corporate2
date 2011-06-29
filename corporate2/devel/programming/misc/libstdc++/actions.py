31bd695799daf4e5ba493e60c889102d48ae8a6b
onur <onur@420bcd57-4a62-4fd6-832e-5ede16c90cc9>
2011-06-29 13:53:56 +0000

    Adapt to multilib under x86_64, fixes pb#17082
    BUG:FIXED:17082

--- corporate2/devel/programming/misc/libstdc++/actions.py
+++ corporate2/devel/programming/misc/libstdc++/actions.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyright 2005 TUBITAK/UEKAE
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
@@ -10,16 +9,25 @@ from pisi.actionsapi import pisitools
 from pisi.actionsapi import shelltools
 from pisi.actionsapi import get
 
-WorkDir = "gcc-3.3.6"
+version = get.srcVERSION()
+WorkDir = "gcc-%s" % version
 
 # Global Path Variables
-BINPATH = "/usr/%s/gcc-bin/3.3.6" % get.HOST()
-LIBPATH = "/usr/lib/gcc-lib/%s/3.3.6" % get.HOST()
-DATAPATH = "/usr/share/gcc-data/%s/3.3.6" % get.HOST()
-STDCXX_INCDIR = "/usr/lib/gcc-lib/%s/3.3.6/include/g++-v3" % get.HOST()
+BINPATH = "/usr/%s/gcc-bin/%s" % (get.HOST(), version)
+LIBPATH = "/usr/lib/gcc-lib/%s/%s" % (get.HOST(), version)
+DATAPATH = "/usr/share/gcc-data/%s/%s" % (get.HOST(), version)
+STDCXX_INCDIR = "/usr/lib/gcc-lib/%s/%s/include/g++-v3" % (get.HOST(), version)
 
 cflags = get.CFLAGS().replace("mtune", "mcpu").replace("-fstack-protector", "").replace("-D_FORTIFY_SOURCE=2", "").replace("-mcpu=generic", "-mcpu=%s" % get.ARCH().replace("_", "-"))
 cxxflags =  get.CXXFLAGS().replace("mtune", "mcpu").replace("-fstack-protector", "").replace("-D_FORTIFY_SOURCE=2", "").replace("-mcpu=generic", "-mcpu=%s" % get.ARCH().replace("_", "-"))
+
+multilib = (get.ARCH() == "x86_64")
+opt_multilib = "--enable-multilib" if multilib else ""
+
+ldconfig = {
+            "system": ["/etc/ld.so.conf.d/99-libstdc++.conf", "/usr/lib/libstdc++-v3"],
+            "multilib": ["/etc/ld.so.conf.d/99-libstdc++-32bit.conf", "/usr/lib32/libstdc++-v3"]
+           }
 
 def setup():
     shelltools.export("CFLAGS", cflags)
@@ -32,7 +40,7 @@ def setup():
 
     conf = "--enable-nls \
             --without-included-gettext \
-            --disable-multilib \
+            %s \
             --prefix=/usr \
             --bindir=%s \
             --includedir=%s/include \
@@ -51,50 +59,50 @@ def setup():
             --enable-__cxa_atexit \
             --enable-version-specific-runtime-libs \
             --with-gxx-include-dir=%s \
-            --with-local-prefix=/usr/local" %(BINPATH, LIBPATH, DATAPATH, DATAPATH, DATAPATH, get.HOST(), get.HOST(), STDCXX_INCDIR)
+            --with-local-prefix=/usr/local" % (opt_multilib, BINPATH, LIBPATH, DATAPATH, DATAPATH, DATAPATH, get.HOST(), get.HOST(), STDCXX_INCDIR)
 
     # Build in a separate build tree
-    shelltools.makedirs("%s/build" % get.workDIR())
-    shelltools.cd("%s/build" % get.workDIR())
-    shelltools.system("%s/%s/configure %s" % (get.workDIR(), WorkDir, conf))
+    shelltools.makedirs("build")
+    shelltools.cd("build")
+    shelltools.system("../configure %s" % conf)
 
     shelltools.touch("gcc/c-gperf.h")
 
 def build():
-    shelltools.cd("%s/build" % get.workDIR())
-    autotools.make("all-target-libstdc++-v3 \
-                    LIBPATH=\"%s\" \
-                    BOOT_CFLAGS=\"%s\" STAGE1_CFLAGS=\"-O\"" % (LIBPATH, get.CFLAGS()))
+    shelltools.cd("build")
+    autotools.make('all-target-libstdc++-v3 \
+                    LIBPATH="%s" \
+                    BOOT_CFLAGS="%s" STAGE1_CFLAGS="-O"' % (LIBPATH, cflags))
 
 def install():
-    shelltools.cd("%s/build" % get.workDIR())
-
+    shelltools.cd("build")
 
     # Do the 'make install' from the build directory
-    autotools.rawInstall("prefix=/usr \
-                        bindir=%s \
-                        includedir=%s/include \
-                        datadir=%s \
-                        mandir=%s/man \
-                        infodir=%s/info \
-                        DESTDIR=\"%s\" \
-                        LIBPATH=\"%s\"" % (BINPATH, LIBPATH, DATAPATH, DATAPATH, DATAPATH, get.installDIR(), LIBPATH),
-                        "install-target-libstdc++-v3")
+    autotools.rawInstall('prefix=/usr \
+                          bindir="%s" \
+                          includedir="%s/include" \
+                          datadir="%s" \
+                          mandir="%s/man" \
+                          infodir="%s/info" \
+                          DESTDIR="%s" \
+                          LIBPATH="%s"' % (BINPATH, LIBPATH, DATAPATH, DATAPATH, DATAPATH, get.installDIR(), LIBPATH),
+                          "install-target-libstdc++-v3")
+
+    confdirbase = ldconfig["system"][0].rsplit("/", 1)[0]
+    pisitools.dodir(confdirbase)
 
     # we'll move this into a directory we can put at the end of ld.so.conf
     # other than the normal versioned directory, so that it doesnt conflict
     # with gcc 3.3.3
-    pisitools.domove("/%s/lib*" % LIBPATH, "/usr/lib/libstdc++-v3/")
+    pisitools.domove("/%s/lib*.so.*" % LIBPATH, "/usr/lib/libstdc++-v3/")
+    shelltools.echo("%s/%s" % (get.installDIR(), ldconfig["system"][0]), ldconfig["system"][1])
 
-    # we dont want the headers...
-    pisitools.removeDir("/usr/lib/gcc*")
-    # or locales...
-    if shelltools.isDirectory("%s/usr/share" % get.installDIR()):
-        pisitools.removeDir("/usr/share")
-    # or anything other than the .so files, really.
-    pisitools.remove("/usr/lib/libstdc++-v3/*.la")
-    pisitools.remove("/usr/lib/libstdc++-v3/*.a")
+    if multilib:
+        pisitools.domove("/%s/32/lib*.so.*" % LIBPATH, "/usr/lib32/libstdc++-v3/")
+        shelltools.echo("%s/%s" % (get.installDIR(), ldconfig["multilib"][0]), ldconfig["multilib"][1])
 
-    # we dont even want the un-versioned .so symlink, as it confuses some
-    # apps and also causes others to link against the old libstdc++...
-    pisitools.remove("/usr/lib/libstdc++-v3/libstdc++.so")
+
+    for i in ["/usr/lib/gcc-lib", "/usr/share/gcc-data"]:
+        if shelltools.isDirectory("%s/%s" % (get.installDIR(), i)):
+            pisitools.removeDir(i)
+
