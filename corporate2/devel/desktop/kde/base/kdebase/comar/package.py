f1d12501c959329e51038dd1ef1c1b635fb81618
ozan.caglayan <ozan.caglayan@420bcd57-4a62-4fd6-832e-5ede16c90cc9>
2011-07-13 10:40:07 +0000

    Change default face icon

--- corporate2/devel/desktop/kde/base/kdebase/comar/package.py
+++ corporate2/devel/desktop/kde/base/kdebase/comar/package.py
@@ -4,11 +4,5 @@ import os
 import shutil
 
 def postInstall(fromVersion, fromRelease, toVersion, toRelease):
-    # set the default kdm face icon if it's not already set by the system admin
-    if not os.path.exists("/usr/kde/3.5/share/apps/kdm/faces/.default.face.icon"):
-        os.makedirs("/usr/kde/3.5/share/apps/kdm/faces")
-        shutil.copyfile("/usr/kde/3.5/share/apps/kdm/pics/users/default1.png",
-                        "/usr/kde/3.5/share/apps/kdm/faces/.default.face.icon")
-
     if not os.path.exists("/usr/kde/3.5/share/templates/.source/emptydir"):
         os.makedirs("/usr/kde/3.5/share/templates/.source/emptydir")
