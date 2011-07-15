8170cb1ff60a1ee859da45916301469a469dda5e
ozan.caglayan <ozan.caglayan@420bcd57-4a62-4fd6-832e-5ede16c90cc9>
2011-07-15 06:06:28 +0000

    Version bump to Ludovic Rousseau's GIT snapshot for smartcard related fixes.

--- corporate2/devel/hardware/misc/libusb1/actions.py
+++ corporate2/devel/hardware/misc/libusb1/actions.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyright 2005-2007 TUBITAK/UEKAE
+# Copyright 2005-2011 TUBITAK/UEKAE
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
@@ -19,4 +19,5 @@ def build():
 
 def install():
     autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+
     pisitools.dodoc("AUTHORS", "NEWS", "README")
