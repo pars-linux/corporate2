ef3c152a3ffcd806b8b358379b788df852823544
Ozan Çağlayan <ozancaglayan@users.noreply.github.com>
2011-08-12 05:35:12 +0000

    Version bump to 0.8.5.91

--- network/connection/network-manager-applet/actions.py
+++ network/connection/network-manager-applet/actions.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyright 2010 TUBITAK/UEKAE
+# Copyright 2010-2011 TUBITAK/UEKAE
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
@@ -20,8 +20,5 @@ def build():
 
 def install():
     autotools.rawInstall("DESTDIR=%s" % get.installDIR())
-
-    # Remove empty gnome-bluetooth directory
-    pisitools.removeDir("/usr/lib")
 
     pisitools.dodoc("README", "COPYING")
