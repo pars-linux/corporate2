7b4496db3dfdcbb3d156a7468728c54dc6a80d24
fatih.arslan <fatih.arslan@420bcd57-4a62-4fd6-832e-5ede16c90cc9>
2011-09-26 07:24:53 +0000

    * New stable release 14.0.835.163 * Added new Web Audio Api * Actually this build contains the final NACL code, but the Linux tarball build is broken currently * Several security and bug fixes: * [49377] High CVE-2011-2835: Race condition in the certificate cache. * [51464] Low CVE-2011-2836: Infobar the Windows Media Player plug-in to avoid click-free access to the system Flash. * [57908] Low CVE-2011-2837: Use PIC / pie compiler flags. * [75070] Low CVE-2011-2838: Treat MIME type more authoritatively when loading plug-ins. * [76771] High CVE-2011-2839: Crash in v8 script object wrappers. * [78427] [83031] Low CVE-2011-2840: Possible URL bar spoofs with unusual user interaction. * [78639] High CVE-2011-2841: Garbage collection error in PDF. * [80680] Low CVE-2011-2842: Insecure lock file handling in the Mac installer. * [82438] Medium CVE-2011-2843: Out-of-bounds read with media buffers. * [85041] Medium CVE-2011-2844: Out-of-bounds read with mp3 files. * [89219] High CVE-2011-2846: Use-after-free in unload event handling. * [89330] High CVE-2011-2847: Use-after-free in document loader. * [89564] Medium CVE-2011-2848: URL bar spoof with forward button. * [89795] Low CVE-2011-2849: Browser NULL pointer crash with WebSockets. * [89991] Medium CVE-2011-3234: Out-of-bounds read in box handling. * [90134] Medium CVE-2011-2850: Out-of-bounds read with Khmer characters. * [90173] Medium CVE-2011-2851: Out-of-bounds read in video handling. * [91120] High CVE-2011-2852: Off-by-one in v8. * [91197] High CVE-2011-2853: Use-after-free in plug-in handling. * [92651] [94800] High CVE-2011-2854: Use-after-free in ruby / table style handing. * [92959] High CVE-2011-2855: Stale node in stylesheet handling. * [93416] High CVE-2011-2856: Cross-origin bypass in v8. * [93420] High CVE-2011-2857: Use-after-free in focus controller. * [93472] High CVE-2011-2834: Double free in libxml XPath handling. * [93497] Medium CVE-2011-2859: Incorrect permissions assigned to non-gallery pages. * [93587] High CVE-2011-2860: Use-after-free in table style handling. * [93596] Medium CVE-2011-2861: Bad string read in PDF. * [93906] High CVE-2011-2862: Unintended access to v8 built-in objects. * [95563] Medium CVE-2011-2864: Out-of-bounds read with Tibetan characters. * [95625] Medium CVE-2011-2858: Out-of-bounds read with triangle arrays. * [95917] Low CVE-2011-2874: Failure to pin a self-signed cert for a session. * [95920] High CVE-2011-2875: Type confusion in v8 object sealing.

--- corporate2/devel/network/web/chromium-browser/actions.py
+++ corporate2/devel/network/web/chromium-browser/actions.py
@@ -20,6 +20,7 @@ def setup():
     #TODO use_system_ssl is disabled -->  https://bugzilla.mozilla.org/show_bug.cgi?id=547312
     #TODO use_system_hunspell has build problems, upstream changes needed
     #TODO use_system_sqlite has build problems, upstream changes needed
+    #TODO use_system_ffmpeg has build problems, system libraries might be outdated
     shelltools.system("build/gyp_chromium -f make build/all.gyp --depth=. \
                         -Dgcc_version=45 \
                         -Dno_strict_aliasing=1 \
@@ -35,15 +36,16 @@ def setup():
                         -Duse_system_libjpeg=1 \
                         -Duse_system_libxslt=1 \
                         -Duse_system_zlib=1 \
-                        -Duse_system_ffmpeg=1 \
                         -Duse_system_flac=1 \
                         -Duse_system_libxml=1 \
-                        -Duse_system_vpx=1 \
+                        -Duse_system_vpx=0 \
                         -Duse_system_xdg_utils=1 \
                         -Duse_system_yasm=1 \
                         -Duse_system_ssl=0 \
                         -Ddisable_sse2=1 \
+                        -Ddisable_nacl=1 \
                         -Dtarget_arch=%s" % ARCH)
+
 
 def build():
     autotools.make("chrome chrome_sandbox BUILDTYPE=Release V=1")
@@ -60,14 +62,19 @@ def install():
     pisitools.insinto("/usr/lib/chromium-browser", "locales")
     pisitools.insinto("/usr/lib/chromium-browser", "resources")
 
-    pisitools.newman("chrome.1", "chromium-browser.1")
+    # Internal ffmpeg libraries
+    pisitools.insinto("/usr/lib/chromium-browser", "libffmpegsumo.so")
+    pisitools.insinto("/usr/lib/chromium-browser", "ffmpegsumo_nolink")
 
-    pisitools.dosym("/usr/lib/nsbrowser/plugins", "/usr/lib/chromium-browser/plugins")
+    # Nacl plugin
+    # pisitools.insinto("/usr/lib/chromium-browser", "libppGoogleNaClPluginChrome.so")
+
+    pisitools.newman("chrome.1", "chromium-browser.1")
 
     # Chromium looks for these in its folder
     # See media_posix.cc and base_paths_linux.cc
-    for lib in ["libavcodec.so.52" , "libavformat.so.52", "libavutil.so.50"]:
-        shelltools.sym("/usr/lib/%s" % lib, "%s/usr/lib/chromium-browser/%s" % (get.installDIR(), lib))
+    #for lib in ["libavcodec.so.52" , "libavformat.so.52", "libavutil.so.50"]:
+    #    shelltools.sym("/usr/lib/%s" % lib, "%s/usr/lib/chromium-browser/%s" % (get.installDIR(), lib))
 
 
     shelltools.cd("../..")
