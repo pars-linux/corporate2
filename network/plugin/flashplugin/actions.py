87faea6e44cffdd302e8e6ef6a5de79f674f5afe
Fatih Arslan <ftharsln@gmail.com>
2011-10-10 06:38:06 +0000

    Update to latest version 11.0.1.152. This includes several enanchement along with security fixes

--- network/plugin/flashplugin/actions.py
+++ network/plugin/flashplugin/actions.py
@@ -1,17 +1,19 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyright 2005-2009 TUBITAK/UEKAE
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 
+ARCH = "i386" if get.ARCH() == "i686" else "x86_64"
 WorkDir = get.ARCH()
 NoStrip = "/"
 
 def install():
-    pisitools.doexe("libflashplayer.so", "/opt/netscape/plugins")
+    distdir = "%s-%s-%s" % (get.srcNAME(), get.srcVERSION(), ARCH)
+
+    pisitools.doexe("%s/libflashplayer.so" % distdir, "/opt/netscape/plugins")
     pisitools.dodir("/usr/lib/nsbrowser/plugins")
     pisitools.dosym("/opt/netscape/plugins/libflashplayer.so", "/usr/lib/nsbrowser/plugins/libflashplayer.so")
