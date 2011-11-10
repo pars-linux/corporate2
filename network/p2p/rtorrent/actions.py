20d4faa715bba6bf17be84a3e069b328f68594b4
Gökhan Özbulak <gozbulak@pardus.org.tr>
2011-11-10 08:34:17 +0000

    * Enable xmlrpc support to provide web-based gui * Version bump to make enabling xmlrpc possible * Add a patch to set scgi port in rc file for web access * Update strict dependency
    BUG:COMMENT:19470

--- network/p2p/rtorrent/actions.py
+++ network/p2p/rtorrent/actions.py
@@ -8,7 +8,7 @@ from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
 
 def setup():
-    autotools.configure()
+    autotools.configure("--with-xmlrpc-c")
 
 def build():
     autotools.make()
